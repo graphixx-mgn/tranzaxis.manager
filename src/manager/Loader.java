fa6acbdaca041fbce770e74423557f86bca2b9f2
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-07-11 22:54:29 +0500

    Создание проекта

---
+++ src/manager/Loader.java
@@ -0,0 +1,13 @@
+package manager;
+
+public class Loader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
