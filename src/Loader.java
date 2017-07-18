5ac78bbdce6a95b199c4010ebaa6443029d2a1e1
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-07-18 21:43:47 +0500

    Удаление тестового кода, старт класса менеджера.

--- src/Loader.java
+++ src/Loader.java
@@ -1,18 +1,10 @@
 
-
-import codex.log.Logger;
-import org.apache.log4j.Level;
-
 public class Loader {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Logger logger = Logger.getLogger();
-        logger.setLevel(Level.ALL);
-        
+        Manager manager = new Manager();
     }
     
 }
