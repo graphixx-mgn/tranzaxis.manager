7cdf13581f050c75067c974a08ebd5f62993de0a
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-07-20 02:30:58 +0500

    Тестирование PropertyHolder и AbstractType.

--- src/Manager.java
+++ src/Manager.java
@@ -1,6 +1,10 @@
 
 import codex.log.Logger;
 import codex.model.AbstractModel;
+import codex.model.Access;
+import codex.property.PropertyHolder;
+import codex.type.StringList;
+import java.util.Arrays;
 import org.apache.log4j.Level;
 
 public class Manager {
@@ -11,11 +15,26 @@ public class Manager {
     }
     
     public Manager() {
-        logger.info("Manager started");
-        
-        AbstractModel model = new AbstractModel();
-        
-        
+        try {
+            AbstractModel model = new AbstractModel("Demo");
+            model.addProperty(new PropertyHolder(Access.class, "access", "Access level", Access.Edit, true), Access.Select);
+            model.addProperty(new PropertyHolder(String.class, "test", "TEST", null, true), Access.Select);
+            model.addProperty(new PropertyHolder(StringList.class, "month", "Month", new StringList(
+                    Arrays.asList("January", "February", "March", "April", "May"), null
+            ), true), Access.Select);
+            
+            model.getProperty("access").setValue(Access.Select);
+            model.getProperty("test").setValue("TEST");
+            model.getProperty("test").setValue("TEST TEST");
+            model.getProperty("month").setValue("April");
+            
+            
+            for (PropertyHolder propHolder : model.getProperties(Access.Any)) {
+                logger.info("Property ''{0}'' = ''{1}''", propHolder.getName(), propHolder.toString());
+            }
+        } catch (Exception | Error e) {
+            logger.error("Unable to init model", e);
+        }
     }
     
 }
