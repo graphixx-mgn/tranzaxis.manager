7b549b4b311936703db264abb923b14eddd31663
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-07-20 05:49:01 +0500

    Тестирование упаковки примитивов.

--- src/Manager.java
+++ src/Manager.java
@@ -16,21 +16,30 @@ public class Manager {
     
     public Manager() {
         try {
+            
             AbstractModel model = new AbstractModel("Demo");
-            model.addProperty(new PropertyHolder(Access.class, "access", "Access level", Access.Edit, true), Access.Select);
-            model.addProperty(new PropertyHolder(String.class, "test", "TEST", null, true), Access.Select);
+            
+            model.addProperty(new PropertyHolder(Access.class, "enum", "Test enum", Access.Edit, true), Access.Select);
+            model.getProperty("enum").setValue(Access.Select);
+            //model.getProperty("enum").setValue(null);
+            
+            model.addProperty(new PropertyHolder(String.class, "string", "Test string", "TEST 1", true), Access.Select);
+            model.getProperty("string").setValue("TEST 2");
+//            model.getProperty("string").setValue(null);
+            
+            
             model.addProperty(new PropertyHolder(StringList.class, "month", "Month", new StringList(
-                    Arrays.asList("January", "February", "March", "April", "May"), null
+                    Arrays.asList("January", "February", "March", "April", "May", "June", "July"), null
             ), true), Access.Select);
-            
-            model.getProperty("access").setValue(Access.Select);
-            model.getProperty("test").setValue("TEST");
-            model.getProperty("test").setValue("TEST TEST");
             model.getProperty("month").setValue("April");
+            model.getProperty("month").setValue("July");
             
-            
+            model.getProperty("month").setValue(new StringList(
+                    Arrays.asList("July", "August", "September", "October", "November", "December"), "July"
+            ));
+
             for (PropertyHolder propHolder : model.getProperties(Access.Any)) {
-                logger.info("Property ''{0}'' = ''{1}''", propHolder.getName(), propHolder.toString());
+                logger.info("Property {0}: value={1}, class={2}", propHolder.getName(), propHolder.toString(), propHolder.getValue().getClass().getCanonicalName());
             }
         } catch (Exception | Error e) {
             logger.error("Unable to init model", e);
