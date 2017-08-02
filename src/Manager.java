0c681da95bebdf8e0e6175da378f645be83eae84
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-08-02 21:27:39 +0500

    Тестирование PropertyHolder и AbstractType.

--- src/Manager.java
+++ src/Manager.java
@@ -4,6 +4,8 @@ import codex.model.AbstractModel;
 import codex.model.Access;
 import codex.property.PropertyHolder;
 import codex.type.StringList;
+import java.io.File;
+import java.nio.file.Path;
 import java.util.Arrays;
 import org.apache.log4j.Level;
 
@@ -16,30 +18,35 @@ public class Manager {
     
     public Manager() {
         try {
-            
             AbstractModel model = new AbstractModel("Demo");
             
             model.addProperty(new PropertyHolder(Access.class, "enum", "Test enum", Access.Edit, true), Access.Select);
             model.getProperty("enum").setValue(Access.Select);
-            //model.getProperty("enum").setValue(null);
-            
+//            model.getProperty("enum").setValue(null);
+
             model.addProperty(new PropertyHolder(String.class, "string", "Test string", "TEST 1", true), Access.Select);
             model.getProperty("string").setValue("TEST 2");
 //            model.getProperty("string").setValue(null);
             
+            model.addProperty(new PropertyHolder(Integer.class, "integer", "Test integer", 1000, true), Access.Select);
+            model.getProperty("integer").setValue(1500);
+//            model.getProperty("integer").setValue(null);
             
+            model.addProperty(new PropertyHolder(Path.class, "path", "Test path", new File("C:\\temp").toPath(), true), Access.Select);
+            model.getProperty("path").setValue(new File("C:\\temp\\temp").toPath());
+//            model.getProperty("path").setValue(null);
+
             model.addProperty(new PropertyHolder(StringList.class, "month", "Month", new StringList(
                     Arrays.asList("January", "February", "March", "April", "May", "June", "July"), null
             ), true), Access.Select);
             model.getProperty("month").setValue("April");
             model.getProperty("month").setValue("July");
-            
             model.getProperty("month").setValue(new StringList(
                     Arrays.asList("July", "August", "September", "October", "November", "December"), "July"
             ));
 
-            for (PropertyHolder propHolder : model.getProperties(Access.Any)) {
-                logger.info("Property {0}: value={1}, class={2}", propHolder.getName(), propHolder.toString(), propHolder.getValue().getClass().getCanonicalName());
+            for (PropertyHolder propHolder : model.getProperties(Access.Edit)) {
+                logger.info("Property {0}: [{1}]", propHolder.getName(), propHolder.toString());
             }
         } catch (Exception | Error e) {
             logger.error("Unable to init model", e);
