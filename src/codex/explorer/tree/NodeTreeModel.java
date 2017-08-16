bc762fcac9eec8225d1490d9fd2f2a46dc7efac8
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-08-17 03:52:55 +0500

    Реализация модели дерева проводника.

---
+++ src/codex/explorer/tree/NodeTreeModel.java
@@ -0,0 +1,72 @@
+package codex.explorer.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+
+
+public final class NodeTreeModel implements TreeModel {
+    
+    private final TreeNode root;
+    private final List<TreeModelListener> listeners = new LinkedList<>();
+
+    public NodeTreeModel(TreeNode root) {
+        this.root = root;
+    }
+    
+    @Override
+    public Object getRoot() {
+        return root;
+    }
+
+    @Override
+    public Object getChild(Object parent, int index) {
+        if (parent == null) {
+            throw new IllegalArgumentException("Argument is null");
+        }
+        return ((TreeNode) parent).getChildAt(index);
+    }
+
+    @Override
+    public int getChildCount(Object parent) {
+        if (parent == null) {
+            throw new IllegalArgumentException("Argument is null");
+        }
+        return ((TreeNode) parent).getChildCount();
+    }
+
+    @Override
+    public boolean isLeaf(Object node) {
+        if (node == null) {
+            throw new IllegalArgumentException("Argument is null");
+        }
+        return ((TreeNode) node).isLeaf();
+    }
+
+    @Override
+    public void valueForPathChanged(TreePath path, Object newValue) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getIndexOfChild(Object parent, Object child) {
+        if (parent == null) {
+            throw new IllegalArgumentException("Argument is null");
+        }
+        return ((TreeNode) parent).getIndex((TreeNode) child);
+    }
+
+    @Override
+    public void addTreeModelListener(TreeModelListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void removeTreeModelListener(TreeModelListener listener) {
+        listeners.remove(listener);
+    }
+    
+}
