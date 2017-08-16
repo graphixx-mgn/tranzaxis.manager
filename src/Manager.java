35fa05425a0fd900b78af90b9e143763aef5c9d9
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-08-17 00:22:48 +0500

    Запуск окна.

--- src/Manager.java
+++ src/Manager.java
@@ -1,13 +1,9 @@
 
+import codex.log.LogUnit;
 import codex.log.Logger;
-import codex.model.AbstractModel;
-import codex.model.Access;
-import codex.property.PropertyHolder;
-import codex.type.StringList;
-import java.io.File;
-import java.nio.file.Path;
-import java.util.Arrays;
+import codex.utils.ImageUtils;
 import org.apache.log4j.Level;
+import ui.Window;
 
 public class Manager {
     
@@ -16,41 +12,20 @@ public class Manager {
         logger.setLevel(Level.ALL);
     }
     
+    private final Window window;
+    
     public Manager() {
-        try {
-            AbstractModel model = new AbstractModel("Demo");
-            
-            model.addProperty(new PropertyHolder(Access.class, "enum", "Test enum", Access.Edit, true), Access.Select);
-            model.getProperty("enum").setValue(Access.Select);
-//            model.getProperty("enum").setValue(null);
-
-            model.addProperty(new PropertyHolder(String.class, "string", "Test string", "TEST 1", true), Access.Select);
-            model.getProperty("string").setValue("TEST 2");
-//            model.getProperty("string").setValue(null);
-            
-            model.addProperty(new PropertyHolder(Integer.class, "integer", "Test integer", 1000, true), Access.Select);
-            model.getProperty("integer").setValue(1500);
-//            model.getProperty("integer").setValue(null);
-            
-            model.addProperty(new PropertyHolder(Path.class, "path", "Test path", new File("C:\\temp").toPath(), true), Access.Select);
-            model.getProperty("path").setValue(new File("C:\\temp\\temp").toPath());
-//            model.getProperty("path").setValue(null);
-
-            model.addProperty(new PropertyHolder(StringList.class, "month", "Month", new StringList(
-                    Arrays.asList("January", "February", "March", "April", "May", "June", "July"), null
-            ), true), Access.Select);
-            model.getProperty("month").setValue("April");
-            model.getProperty("month").setValue("July");
-            model.getProperty("month").setValue(new StringList(
-                    Arrays.asList("July", "August", "September", "October", "November", "December"), "July"
-            ));
-
-            for (PropertyHolder propHolder : model.getProperties(Access.Edit)) {
-                logger.info("Property {0}: [{1}]", propHolder.getName(), propHolder.toString());
-            }
-        } catch (Exception | Error e) {
-            logger.error("Unable to init model", e);
-        }
+        window = new Window("TranzAxis Manager", ImageUtils.getByPath("/images/project.png"));
+        
+        window.addUnit(new LogUnit(), window.loggingPanel);
+        //window.addUnit(new UpdateUnit(), window.upgradePanel);
+        
+        window.setVisible(true);
+        logger.debug("TESD");
+        logger.info("TESD");
+        logger.warn("TESD");
+        logger.error("Some error", new Error("This is an exception"));
+        logger.info("TESD");
     }
     
 }
