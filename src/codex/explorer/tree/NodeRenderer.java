204fb7ab1cda0ca9da35ad3e34c1b88879074127
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-08-17 03:52:04 +0500

    Реализация рендера узла дерева проводника.

---
+++ src/codex/explorer/tree/NodeRenderer.java
@@ -0,0 +1,33 @@
+package codex.explorer.tree;
+
+import codex.utils.ImageUtils;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+public class NodeRenderer extends DefaultTreeCellRenderer {
+
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+        Node node = (Node) value;
+        Component component = super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+        
+        int iconSize = tree.getRowHeight()-2;
+        ImageIcon icon = ImageUtils.resize(node.icon, iconSize, iconSize);
+        setDisabledIcon(ImageUtils.grayscale(icon));
+        setIcon(icon);
+        
+        setTextSelectionColor(Color.WHITE);
+        setBackgroundSelectionColor(Color.decode("#3399FF"));
+        setBorderSelectionColor(Color.GRAY);
+        
+        setEnabled((node.mode & Node.MODE_ENABLED) == Node.MODE_ENABLED);
+        return component;
+        
+    }
+    
+    
+    
+}
