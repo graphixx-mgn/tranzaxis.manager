3cb591b56a17fc63b030e962cb0e63c9810599a4
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-08-17 00:17:57 +0500

    Абстрактная кнопка.

---
+++ src/codex/component/button/Button.java
@@ -0,0 +1,17 @@
+package codex.component.button;
+
+import java.awt.Color;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+public interface Button {
+    
+    public final static Border EMPTY_BORDER = new EmptyBorder(1, 1, 1, 1);
+    public final static Border HOVER_BORDER = new LineBorder(Color.decode("#C0DCF3"), 1);
+    public final static Border PRESS_BORDER = new LineBorder(Color.decode("#90C8F6"), 1);
+    
+    public final static Color  HOVER_COLOR  = Color.decode("#D8E6F2");
+    public final static Color  PRESS_COLOR  = Color.decode("#C0DCF3");
+    
+}
