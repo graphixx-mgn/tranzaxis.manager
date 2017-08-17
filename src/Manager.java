71ef1c4c9fec42e56bc02d4536d1af321f7013db
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-08-17 13:58:26 +0500

    Создание модели дерева и инициализация модуля проводника.

--- src/Manager.java
+++ src/Manager.java
@@ -1,8 +1,13 @@
 
 import codex.explorer.ExplorerUnit;
+import codex.explorer.tree.NodeTreeModel;
 import codex.log.LogUnit;
 import codex.log.Logger;
 import codex.utils.ImageUtils;
+import node.CommonRoot;
+import node.DatabaseRoot;
+import node.RepositoryRoot;
+import node.SystemRoot;
 import org.apache.log4j.Level;
 import ui.Window;
 
@@ -18,8 +23,16 @@ public class Manager {
     public Manager() {
         window = new Window("TranzAxis Manager", ImageUtils.getByPath("/images/project.png"));
         
+        CommonRoot root = new CommonRoot();
+        RepositoryRoot repos = new RepositoryRoot();
+        DatabaseRoot   bases = new DatabaseRoot();
+        SystemRoot   systems = new SystemRoot();
+        root.insert(repos);
+        root.insert(bases);
+        root.insert(systems);
+        
         window.addUnit(new LogUnit(), window.loggingPanel);
-        window.addUnit(new ExplorerUnit(), window.explorePanel);
+        window.addUnit(new ExplorerUnit(new NodeTreeModel(root)), window.explorePanel);
         
         window.setVisible(true);
     }
