27eba6cd276f49bcc6096bdcb6c5bed1f9161025
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-08-22 22:15:16 +0500

    Расширение интерфейса.

--- src/codex/explorer/tree/Node.java
+++ src/codex/explorer/tree/Node.java
@@ -1,5 +1,7 @@
 package codex.explorer.tree;
 
+import codex.presentation.EditorPresentation;
+import codex.presentation.SelectorPresentation;
 import javax.swing.tree.TreeNode;
 
 public interface Node extends TreeNode {
@@ -7,5 +9,9 @@ public interface Node extends TreeNode {
     public static final int MODE_NONE       = 0;
     public static final int MODE_ENABLED    = 1;
     public static final int MODE_SELECTABLE = 2;
+    
+    public EditorPresentation getEditorPresentation();
+    public SelectorPresentation getSelectorPresentation();
+    public Class getChildClass();
     
 }
