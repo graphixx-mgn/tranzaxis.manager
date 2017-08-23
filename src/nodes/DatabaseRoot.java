067848d6eba8750f88415f19708acf110ddb52f4
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-08-23 07:37:33 +0500

    Настройки выделения памяти под запуск приложений для наследования в подключениях.

--- src/nodes/DatabaseRoot.java
+++ src/nodes/DatabaseRoot.java
@@ -1,6 +1,8 @@
 package nodes;
 
 import codex.explorer.tree.AbstractNode;
+import codex.model.Access;
+import codex.property.PropertyHolder;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
 
@@ -8,6 +10,9 @@ public class DatabaseRoot extends AbstractNode {
 
     public DatabaseRoot() {
         super(ImageUtils.getByPath("/images/database.png"), Language.get("title"), Language.get("desc"));
+        model.addPersistProperty(new PropertyHolder(String.class,  "memServer",  null, true), Access.Select);
+        model.addPersistProperty(new PropertyHolder(String.class,  "memExplorer",  null, true), Access.Select);
+        model.addPersistProperty(new PropertyHolder(String.class,  "memDesigner",  null, true), Access.Select);
     }
     
 }
