a7317d87e8b2f2fe43a54e7c9b8055232912d253
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-08-23 18:56:11 +0500

    Перенос ошибочно заведенных настроек памяти в раздел "Системы".

--- src/nodes/SystemRoot.java
+++ src/nodes/SystemRoot.java
@@ -1,6 +1,8 @@
 package nodes;
 
 import codex.explorer.tree.AbstractNode;
+import codex.model.Access;
+import codex.property.PropertyHolder;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
 
@@ -8,6 +10,9 @@ public class SystemRoot extends AbstractNode {
     
     public SystemRoot() {
         super(ImageUtils.getByPath("/images/system.png"), Language.get("title"), Language.get("desc"));
+        model.addPersistProperty(new PropertyHolder(String.class,  "memServer",    "-Xmx2G -XX:MaxPermSize=500M", true), Access.Select);
+        model.addPersistProperty(new PropertyHolder(String.class,  "memExplorer",  "-Xmx1G -XX:MaxPermSize=500M", true), Access.Select);
+        model.addPersistProperty(new PropertyHolder(String.class,  "memDesigner",  "-J-Xmx4G -J-Xms500M", true), Access.Select);
     }
     
 }
