cc81036c55c0578a7d49cef72f51198f532b2ca1
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-08-31 20:12:13 +0500

    Добавлено свойство "guiLang".

--- src/nodes/CommonRoot.java
+++ src/nodes/CommonRoot.java
@@ -2,19 +2,21 @@ package nodes;
 
 
 import codex.explorer.tree.AbstractNode;
+import codex.log.Level;
 import codex.model.Access;
 import codex.property.PropertyHolder;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
 import java.nio.file.Path;
-import org.apache.log4j.Level;
+import type.Locale;
 
 public class CommonRoot extends AbstractNode {
 
     public CommonRoot() {
         super(ImageUtils.getByPath("/images/settings.png"), Language.get("title"), Language.get("desc"));
         model.addPersistProperty(new PropertyHolder(Path.class,  "workDir",  null, true), Access.Select);
-        model.addPersistProperty(new PropertyHolder(Level.class, "logLevel", Level.DEBUG, false), Access.Select);
+        model.addPersistProperty(new PropertyHolder(Level.class, "logLevel", Level.Debug, false), Access.Select);
+        model.addPersistProperty(new PropertyHolder(Locale.class, "guiLang", Locale.Russian, false), Access.Select);
     }
     
 }
