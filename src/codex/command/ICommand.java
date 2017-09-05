511fcb37d11f1f902895db39520d19a27d905423
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-09-05 07:34:06 +0500

    Рефакторинг.

--- src/codex/command/ICommand.java
+++ src/codex/command/ICommand.java
@@ -1,12 +1,12 @@
 package codex.command;
 
 import codex.component.button.IButton;
-import javax.swing.ImageIcon;
 
-public interface ICommand {
+public interface ICommand<T> {
     
-    public ImageIcon getIcon();
-    public void      bindButton(IButton button);
-    public void      execute();
+    public IButton   getButton();
+    public void      setContext(T... context);
+    public T[]       getContext();
+    public void      execute(T context);
     
 }
