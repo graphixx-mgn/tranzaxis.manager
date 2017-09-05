1db919afd26c788478462254e6ca3e114c05c594
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-09-05 07:33:22 +0500

    Кнопка создается автоматически сразу.

--- src/codex/command/PropertyCommand.java
+++ src/codex/command/PropertyCommand.java
@@ -1,43 +1,41 @@
 package codex.command;
 
+import codex.component.button.CommandButton;
 import codex.component.button.IButton;
 import codex.property.PropertyHolder;
+import codex.utils.ImageUtils;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import javax.swing.ImageIcon;
 
 public abstract class PropertyCommand implements ICommand<PropertyHolder>, ActionListener {
     
-    private final ImageIcon icon;
-    private List<PropertyHolder> context;
+    protected PropertyHolder  context;
+    protected IButton         button;
     
     public PropertyCommand(ImageIcon icon) {
-        this.icon = icon;
+        this.button = new CommandButton(ImageUtils.resize(icon, 20, 20));
+        this.button.addActionListener(this);
     }
     
     @Override
-    public ImageIcon getIcon() {
-        return icon;
-    }
-
-    @Override
-    public void bindButton(IButton button) {
-        button.addActionListener(this);
+    public IButton getButton() {
+        return button;
     }
 
     @Override
     public void setContext(PropertyHolder... context) {
-        this.context = Arrays.asList(context);
+        this.context = context[0];
     }
     
     @Override
     public void actionPerformed(ActionEvent event) {
-        for (PropertyHolder propHolder : new LinkedList<>(context)) {
-            execute(propHolder);
-        }
+        execute(context);
+    }
+
+    @Override
+    public PropertyHolder[] getContext() {
+        return new PropertyHolder[]{ context };
     }
     
 }
