60da979f80232a3b09e4a9e143248c3776439747
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-09-22 06:39:20 +0500

    Абстрактная реализация задачи.

--- src/codex/task/AbstractTask.java
+++ src/codex/task/AbstractTask.java
@@ -1,70 +1,119 @@
 package codex.task;
 
 import codex.log.Logger;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public abstract class AbstractTask<T> implements ITask<T> {
+ 
+    private final String title;
+    private Status  status;
+    private Integer percent = 0;
+    private final FutureTask<T> future;
+    private List<ITaskListener> listeners = new LinkedList<>();
     
-    final String  title;
-    private State state;
-    FutureTask<T> future;
-    
-    public AbstractTask(String title) {
-        this.title = title;
-        state  = State.PENDING;
-        future = new FutureTask<T>(this) {
-            
+//05:55:57,648 [DEBUG] Task 'Value incrementator starts from 10' state changed: Pending -> Started
+//05:56:00,900 [DEBUG] Task 'Value incrementator starts from 10' state changed: Started -> Cancelled
+//05:56:00,900 [DEBUG] Task 'Value incrementator starts from 10' state changed: Started -> Failed
+
+
+    public AbstractTask(final String title) {
+        future = new FutureTask<T>((Callable<T>) () -> {
+            setStatus(Status.STARTED);
+            try {
+                finished(execute());
+            } catch (InterruptedException e) {
+                setStatus(Status.FAILED);
+            } catch (Throwable e) {
+                setStatus(Status.FAILED);
+                setProgress(percent, Status.FAILED.getDescription()+e.getLocalizedMessage());
+                Logger.getLogger().error("Error on task execution", e);
+            }
+            return null;
+        }) {       
             @Override
-            public void run() {
-                setState(State.STARTED);
-                T result = execute();
-                setState(State.FINISHED);
-                finished(result);
+            protected void done() {
+                if (status != Status.FAILED) {
+                    setStatus(isCancelled() ? Status.CANCELLED : Status.FINISHED);
+                }
             }
         };
-    }
-    
-    public String getTitle() {
-        return title;
+        this.status = Status.PENDING;
+        this.title = title;
     }
 
     @Override
-    public abstract T execute();
+    public abstract T execute() throws Exception;
 
     @Override
     public abstract void finished(T result);
 
-    
     @Override
-    public T call() throws Exception {
-        try {
-            if (state != State.PENDING) {
-                throw new IllegalStateException("Wrong task state");
-            }
-            future.run();
-            return future.get();
-        }  catch (IllegalStateException | InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public final boolean cancel(boolean mayInterruptIfRunning) {
+        return future.cancel(mayInterruptIfRunning);
     }
-    
+
     @Override
-    public final void cancel() {
-        future.cancel(true);
-    };
-    
-    private void setState(State state) {
-        this.state = state;
-        //Logger.getLogger().debug("new state: {0}", state);
+    public final boolean isCancelled() {
+        return future.isCancelled();
     }
-    
+
+    @Override
+    public final boolean isDone() {
+        return future.isDone();
+    }
+
+    @Override
+    public final T get() throws InterruptedException, ExecutionException {
+        return future.get();
+    }
+
+    @Override
+    public final T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return future.get(timeout, unit);
+    }
+
+    @Override
+    public final String getTitle() {
+        return title;
+    }
+
     public final void setProgress(int percent, String description) {
-        if (percent > 100) {
-            throw new IllegalStateException("Wrong progress state");
+        if (percent < 0 || percent > 100) {
+            throw new IllegalArgumentException("Progress value should be from 0 to 100");
         }
-        Logger.getLogger().info("Progress: {0}", percent);
+        this.percent = percent;
+        new LinkedList<>(listeners).forEach((listener) -> {
+            listener.progressChanged(percent, description);
+        });
     }
+
+    private void setStatus(Status state) {
+        Logger.getLogger().debug("Task ''{0}'' state changed: {1} -> {2}", getTitle(), this.status, state);
+        this.status = state;
+        new LinkedList<>(listeners).forEach((listener) -> {
+            listener.statusChanged(status);
+        });
+    }
+    
+    @Override
+    public Status getStatus() {
+        return status;
+    }
+
+    @Override
+    public final void run() {
+        future.run();
+    }
+
+    @Override
+    public final void addListener(ITaskListener listener) {
+        listeners.add(listener);
+    };
     
 }
