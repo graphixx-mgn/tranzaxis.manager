50db1f89c1f82bd34f85e6e75793ff398c5543ef
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-05 23:52:03 +0500

    Документирование.

--- src/codex/command/ICommand.java
+++ src/codex/command/ICommand.java
@@ -2,11 +2,34 @@ package codex.command;
 
 import codex.component.button.IButton;
 
+/**
+ * Интерфейс команд над объектами. Вызывается при нажатии на кнопку, которая создается 
+ * в классе конкретной реализации метода {@link #getButton()} интерфейса.
+ * @param <T> Тип объекта для которого вызывается команда.
+ */
 public interface ICommand<T> {
     
-    public IButton   getButton();
-    public void      setContext(T... context);
-    public T[]       getContext();
-    public void      execute(T context);
+    /**
+     * @return Экземпляр кнопки, вызывающей исполнение команды над объектом.
+     */
+    public IButton getButton();
+    
+    /**
+     * Установка контекста исполнения команды. При запуске, команды последовательно 
+     * исполняется для каждого объекта из набора.
+     * @param context Группа из одного или более объектов.
+     */
+    public void setContext(T... context);
+    
+    /**
+     * @return Набор объектов, установленных в качестве контекста команды.
+     */
+    public T[] getContext();
+    
+    /**
+     * Метод команды.
+     * @param context Элемент набора объектов, установленных в качестве контекста команды.
+     */
+    public void execute(T context);
     
 }
