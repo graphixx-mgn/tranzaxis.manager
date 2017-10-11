6071815a146618ab77f491ded80c2d2d01e8696a
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-11 20:04:23 +0500

    Расширен прототип методов.

--- src/codex/task/ITaskListener.java
+++ src/codex/task/ITaskListener.java
@@ -2,7 +2,7 @@ package codex.task;
 
 public interface ITaskListener {
     
-    public void statusChanged(Status status);
-    public void progressChanged(int percent, String description);
+    public void statusChanged(ITask task, Status status);
+    public void progressChanged(ITask task,  int percent, String description);
     
 }
