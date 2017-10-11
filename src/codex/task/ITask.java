e74678dc9b8ad5aedfdc24db6061f85cdb982a6d
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-11 19:54:37 +0500

    Добавлены методы getProgress и getDescription.

--- src/codex/task/ITask.java
+++ src/codex/task/ITask.java
@@ -4,12 +4,12 @@ import java.util.concurrent.RunnableFuture;
 
 public interface ITask<T> extends RunnableFuture<T> {
 
-    String getTitle();
-    Status getStatus();
-    
+    String  getTitle();
+    Status  getStatus();
+    Integer getProgress();
+    String  getDescription();
     T execute() throws Exception;
     void finished(T result);
-    
     void addListener(ITaskListener listener);
     
 }
