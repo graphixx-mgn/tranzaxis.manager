1b2739045cf089167150a2b5dac430ebf0ee7759
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-11 22:15:17 +0500

    Реализация типа-обертки для интерфейса List<String>.

--- src/codex/type/StringList.java
+++ src/codex/type/StringList.java
@@ -1,70 +1,52 @@
 package codex.type;
 
-import java.text.MessageFormat;
-import java.util.LinkedHashMap;
+import codex.editor.IEditorFactory;
+import codex.editor.StringListEditor;
+import codex.property.PropertyHolder;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * Implements new property type: string value from list of possible values.
- * @see IComplexType
- * @author Gredyaev Ivan
+ * Тип-обертка {@link IComplexType} для интерфейса {@literal List<String>}.
  */
-public class StringList implements IComplexType {
+public class StringList implements IComplexType<List<String>> {
     
-    private final LinkedHashMap<String, Boolean> values = new LinkedHashMap<>();
+    private final static IEditorFactory EDITOR_FACTORY = (PropertyHolder propHolder) -> {
+        return new StringListEditor(propHolder);
+    };
+    
+    private List<String> value = null;
     
     /**
-     * Creates new instance of StringList
-     * @param range List of possible values
-     * @param value 
+     * Конструктор типа.
+     * @param values Список параметров-строк (vararg) произвольной длины или массив.
      */
-    public StringList(List<String> range, String value) {
-        for (String rangeItem : range) {
-            if (rangeItem == null) {
-                throw new IllegalStateException(
-                        "Range must not contain NULL value"
-                );
-            }
-            if (values.containsKey(rangeItem)) {
-                throw new IllegalStateException(
-                        MessageFormat.format("Range already has item ''{0}''", rangeItem)
-                );
-            }
-            this.values.put(rangeItem, rangeItem.equals(value));
-        }
+    public StringList(String... values) {
+        this(Arrays.asList(IComplexType.coalesce(values, new String[]{})));
+    }
+    
+    /**
+     * Конструктор типа.
+     * @param value Список строк.
+     */
+    public StringList(List<String> value) {
+        // Может быть передан immutable List (Arrays.asList(...)) 
+        this.value = new ArrayList<>(value);
     }
 
     @Override
-    public String getValue() {
-        for (String key : values.keySet()) {
-            if (values.get(key)) {
-                return key;
-            }
-        }
-        return null;
+    public List<String> getValue() {
+        return value;
     }
 
     @Override
-    public void setValue(Object value) {
-        values.replaceAll((k, v) -> Boolean.FALSE);
-        if (value != null) {
-            if (values.containsKey(value.toString())) {
-                values.replace(value.toString(), Boolean.TRUE);
-            } else {
-                throw new IllegalStateException(
-                        MessageFormat.format("Value ''{0}'' is out of range", value)
-                );
-            }
-        }
+    public void setValue(List<String> value) {
+        this.value = value;
     }
-
+    
     @Override
-    public String toString() {
-        String value = getValue();
-        if (value == null) {
-            return "";
-        } else {
-            return value;
-        }
+    public IEditorFactory editorFactory() {
+        return EDITOR_FACTORY;
     }
 }
