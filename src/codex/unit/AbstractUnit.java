0603e4dba151e3f6f865f4bcd54c47c3e270a980
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-18 00:50:05 +0500

    Документирование.

--- src/codex/unit/AbstractUnit.java
+++ src/codex/unit/AbstractUnit.java
@@ -2,13 +2,25 @@ package codex.unit;
 
 import javax.swing.JComponent;
 
+/**
+ * Абстракный класс модуля приложения.
+ */
 public abstract class AbstractUnit {
     
     protected final JComponent view = createViewport();
     
+    /**
+     * Создает виджет модуля для размещение в окне приложения.
+     */
     public abstract JComponent createViewport();
+    /**
+     * Вызывается в момент вставки виджета в окно приложения.
+     */
     public void viewportBound() {};
     
+    /**
+     * Возвращает виджет модуля.
+     */
     public final JComponent getViewport() {
         return view;
     };
