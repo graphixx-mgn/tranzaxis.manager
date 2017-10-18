bd8e2ae8549bee4b24215cb7a498c33b583cb2a7
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-18 23:54:07 +0500

    Документирование, рефакторинг.

--- src/codex/presentation/SwitchInheritance.java
+++ src/codex/presentation/SwitchInheritance.java
@@ -1,28 +1,42 @@
 package codex.presentation;
 
-import codex.command.PropertyCommand;
+import codex.command.EditorCommand;
 import codex.property.PropertyHolder;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
+import java.util.function.Consumer;
 import javax.swing.ImageIcon;
 
-public class SwitchInheritance extends PropertyCommand {
-        
+/**
+ * Команда переключения наследования значения свойства от одноименного свойства 
+ * родительской сущности.
+ */
+public class SwitchInheritance extends EditorCommand {
+    
     public static final ImageIcon OVERRIDE = ImageUtils.resize(ImageUtils.getByPath("/images/override.png"), 20, 20);
     public static final ImageIcon INHERIT  = ImageUtils.resize(ImageUtils.getByPath("/images/inherit.png"), 20, 20);
     
-    private final PropertyHolder parentHolder;
+    private final PropertyHolder           parentHolder;
+    private final Consumer<PropertyHolder> switcher;
 
-    public SwitchInheritance(PropertyHolder parentHolder) {
-        super(OVERRIDE, Language.get("override"));
+    /**
+     * Конструктор команды.
+     * @param childHolder Свойство которое наследует значение.
+     * @param parentHolder Одноименное свойство родительской сущности.
+     */
+    public SwitchInheritance(PropertyHolder childHolder, PropertyHolder parentHolder) {
+        super(childHolder.isInherited() ? OVERRIDE : INHERIT, Language.get("override"));
+        switcher = (holder) -> {
+            holder.setInherited(holder.isInherited() ? null : parentHolder);
+            button.setIcon(holder.isInherited() ? OVERRIDE : INHERIT);
+            button.setHint(Language.get(holder.isInherited() ? "override" : "inherit"));
+        };
         this.parentHolder = parentHolder;
     }
 
     @Override
     public void execute(PropertyHolder context) {
-        context.inherit(context.isInherited() ? null : parentHolder);
-        button.setIcon(context.isInherited() ? OVERRIDE : INHERIT);
-        button.setHint(Language.get(context.isInherited() ? "override" : "inherit"));
+        switcher.accept(context);
     }
 
 }
