bbf6c117af8146efd70b322a1d17ffaec152a37f
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-18 12:41:06 +0500

    Реализация абстрактного виджета задачи для монитора.

---
+++ src/codex/task/AbstractTaskView.java
@@ -0,0 +1,27 @@
+package codex.task;
+
+import java.awt.Color;
+import java.awt.LayoutManager;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+
+/**
+ * Абстрактный класс виджета задачи.
+ */
+public abstract class AbstractTaskView extends JPanel implements ITaskListener {
+    
+    final static Color PROGRESS_NORMAL   = UIManager.getDefaults().getColor("ProgressBar.foreground");
+    final static Color PROGRESS_INFINITE = Color.decode("#A0C8FF");
+    final static Color PROGRESS_FINISHED = Color.decode("#4CAE32");
+    final static Color PROGRESS_ABORTED  = Color.decode("#D93B3B");
+    final static Color PROGRESS_CANCELED = Color.GRAY;
+    
+    /**
+     * Конструктор виджета.
+     * @param layout Менеджер компоновки элементов GUI.
+     */
+    public AbstractTaskView(LayoutManager layout) {
+        super(layout);
+    }
+    
+}
