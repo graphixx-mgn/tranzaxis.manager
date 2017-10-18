a57070d690eeac739b33aaa951f82a36d7cf3d46
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-18 08:15:02 +0500

    Документирование.

--- src/codex/task/ITaskListener.java
+++ src/codex/task/ITaskListener.java
@@ -1,8 +1,24 @@
 package codex.task;
 
+/**
+ * Интерфейс слушателя событий изменения состояния исполнения задачи {@link ITask}
+ */
 public interface ITaskListener {
     
+    /**
+     * Вызывается при смене статуса задачи.
+     * @see Status
+     * @param task Ссылка на задачу.
+     * @param status Новый статус.
+     */
     public void statusChanged(ITask task, Status status);
+    /**
+     * Вызывается при изменении прогресса исполнения.
+     * @param task Ссылка на задачу.
+     * @param percent Значение прогресса в процентах.
+     * @param description Описание текущего состояния, может меняться при росте 
+     * прогресса.
+     */
     public void progressChanged(ITask task,  int percent, String description);
     
 }
