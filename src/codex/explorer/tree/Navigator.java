0be831198ecce7c9367f862209e29a339b3a2dcb
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-18 23:59:10 +0500

    Документирование, рефакторинг.

--- src/codex/explorer/tree/Navigator.java
+++ src/codex/explorer/tree/Navigator.java
@@ -1,10 +1,9 @@
 package codex.explorer.tree;
 
 import codex.log.Logger;
-import java.awt.Font;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.swing.JTree;
 import javax.swing.ToolTipManager;
 import javax.swing.border.EmptyBorder;
@@ -12,23 +11,27 @@ import javax.swing.event.TreeSelectionEvent;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 
+/**
+ * Дерево навигации проводника.
+ */
 public final class Navigator extends JTree {
     
     private TreePath path;
     private final List<NavigateListener> listeners = new LinkedList<>();
 
+    /**
+     * Конструктор дерева.
+     */
     public Navigator() {
         super();
-        
-        setFont(new Font("Tahoma", 0, 12));
         setRowHeight(22);
         setBorder(new EmptyBorder(5, 10, 5, 2));
         addTreeSelectionListener((TreeSelectionEvent event) -> {
             final JTree tree = (JTree) event.getSource();
-            final AbstractNode  node = (AbstractNode) tree.getLastSelectedPathComponent();
+            final INode node = (INode) tree.getLastSelectedPathComponent();
             if (node == null) return;
             
-            if ((node.mode & INode.MODE_SELECTABLE) != INode.MODE_SELECTABLE) {
+            if ((node.getMode() & INode.MODE_SELECTABLE) != INode.MODE_SELECTABLE) {
                 tree.clearSelection();
                 tree.getSelectionModel().setSelectionPath(event.getOldLeadSelectionPath());
                 return;
@@ -37,7 +40,12 @@ public final class Navigator extends JTree {
                 path = tree.getSelectionModel().getSelectionPath();
                 Logger.getLogger().debug(
                         "Selected path: {0}",
-                        String.join("/", Arrays.stream(tree.getSelectionPath().getPath()).map(Object::toString).toArray(String[]::new))
+                        "/" + String.join("/", node
+                                .getPath()
+                                .stream()
+                                .skip(1)
+                                .collect(Collectors.toList())
+                        )
                 );
                 new LinkedList<>(listeners).stream().forEach((listener) -> {
                     listener.nodeChanged(path);
@@ -49,6 +57,9 @@ public final class Navigator extends JTree {
         ToolTipManager.sharedInstance().registerComponent(this);
     }
     
+    /**
+     * Добавление слушателя события смены активного узла дерева.
+     */
     public void addNavigateListener(NavigateListener listener) {
         listeners.add(listener);
     }
