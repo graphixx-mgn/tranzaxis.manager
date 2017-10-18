c98e8e46e6d1ae8659d8ef5316fd9dffffe07ef5
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-18 08:10:16 +0500

    Документирование.

--- src/codex/type/Iconified.java
+++ src/codex/type/Iconified.java
@@ -1,9 +1,19 @@
 package codex.type;
 
+import codex.log.Level;
+import codex.task.Status;
 import javax.swing.ImageIcon;
 
+/**
+ * Интерфейс-маркер перечислений которые имеют связанную со значением иконку.
+ * @see Status
+ * @see Level
+ */
 public interface Iconified {
     
+    /**
+     * Возвращает иконку значения перечисления.
+     */
     public ImageIcon getIcon();
     
 }
