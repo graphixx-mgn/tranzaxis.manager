959fce988ea26ee182cc8ec26ecca70e6865799c
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-23 03:19:47 +0500

    Конечная реализация маски свойства FilePath.

---
+++ src/codex/mask/FileMask.java
@@ -0,0 +1,41 @@
+package codex.mask;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ * Маска, указывающая редактору {@link FilePathEditor} выбирать файлы.
+ */
+public class FileMask implements IPathMask {
+    
+    private final FileNameExtensionFilter filter;
+    
+    /**
+     * Конструктор маски без ограничения на расширения файлов.
+     */
+    public FileMask() {
+        this(null);
+    }
+    
+    /**
+     * Конструктор маски c ограничением на расширения файлов.
+     * @param filter Экземпляр фильтра расширений.
+     * <pre>
+     *   new FileMask(new FileNameExtensionFilter("JPEG files", "jpg", "jpeg"));
+     * </pre>
+     */
+    public FileMask(FileNameExtensionFilter filter) {
+        this.filter = filter;
+    }
+
+    @Override
+    public int getSelectionMode() {
+        return JFileChooser.FILES_ONLY;
+    }
+
+    @Override
+    public FileNameExtensionFilter getFilter() {
+        return filter;
+    }
+    
+}
