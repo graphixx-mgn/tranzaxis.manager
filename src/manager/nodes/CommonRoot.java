e948d0206a9f8e03c78788c4ece63d894b963881
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-23 03:24:47 +0500

    Установка маски ввода для поля "workDir".

--- src/manager/nodes/CommonRoot.java
+++ src/manager/nodes/CommonRoot.java
@@ -3,6 +3,7 @@ package manager.nodes;
 
 import codex.explorer.tree.Entity;
 import codex.log.Level;
+import codex.mask.DirMask;
 import codex.model.Access;
 import codex.property.PropertyHolder;
 import codex.type.Bool;
@@ -16,7 +17,7 @@ public class CommonRoot extends Entity {
 
     public CommonRoot() {
         super(ImageUtils.getByPath("/images/settings.png"), Language.get("title"), Language.get("desc"));
-        model.addPersistProperty(new PropertyHolder("workDir",  new FilePath(null), true), Access.Select);
+        model.addPersistProperty(new PropertyHolder("workDir",  new FilePath(null).setMask(new DirMask()), true), Access.Select);
         model.addPersistProperty(new PropertyHolder("logLevel", new Enum(Level.Debug), false), Access.Select);
         model.addPersistProperty(new PropertyHolder("guiLang",  new Enum(Locale.Russian), false), Access.Select);
         model.addPersistProperty(new PropertyHolder("useTray",  new Bool(Boolean.FALSE), false), Access.Select);
