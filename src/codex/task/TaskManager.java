a7cf064db94d75706fa01aa9647740faffcc26f2
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-23 01:01:38 +0500

    Добавление метода execute (прогресс отображается в диалоге).

--- src/codex/task/TaskManager.java
+++ src/codex/task/TaskManager.java
@@ -1,31 +1,65 @@
 package codex.task;
 
+import codex.component.dialog.Dialog;
 import codex.unit.AbstractUnit;
+import java.awt.event.ActionEvent;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import javax.swing.AbstractAction;
 import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
 
 /**
  * Модуль-исполнитель задач {@link ITask}.
  */
 public final class TaskManager extends AbstractUnit {
     
-    private final ExecutorService threadPool = Executors.newFixedThreadPool(5);
-    private       TaskStatusBar   viewPort;
+    private final ExecutorService queuedThreadPool = Executors.newFixedThreadPool(5);
+    private final ExecutorService demandThreadPool = Executors.newCachedThreadPool();
+    
+    private       TaskStatusBar   taskPanel;
+    private final TaskDialog      taskDialog = new TaskDialog(
+            SwingUtilities.getWindowAncestor(getViewport()),
+            new AbstractAction() {
+                
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    if (event.getID() == Dialog.EXIT || event.getID() == Dialog.CANCEL) {
+                        taskDialog.taskRegistry.keySet().stream().forEach((task) -> {
+                            taskPanel.addTask(task);
+                        });
+                        taskDialog.clear();
+                    }
+                }
+            }
+    );
 
     @Override
     public JComponent createViewport() {
-        viewPort = new TaskStatusBar();
-        return viewPort;
+        taskPanel = new TaskStatusBar();
+        return taskPanel;
     }
     
     /**
      * Добавление задачи в очередь на исполнение и регистрация в окне просмотра
      * задач.
      */
+    public void enqueue(ITask task) {
+        taskPanel.addTask(task);
+        queuedThreadPool.submit(task);
+    }
+    
+    /**
+     * Незамедлительное исполнение задачи и регистрация в модальном диалоге.
+     * При закрытии диалога, все задачи перемещаются в очередь.
+     */
     public void execute(ITask task) {
-        viewPort.addTask(task);
-        threadPool.submit(task);
+        taskDialog.addTask(task);
+        demandThreadPool.submit(task);
+        
+        SwingUtilities.invokeLater(() -> {
+            taskDialog.setVisible(true);
+        });
     }
     
 }
