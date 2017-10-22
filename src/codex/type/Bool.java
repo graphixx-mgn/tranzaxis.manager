375d84b3fb5f11ff254a7ac9ceebb23d30000d6c
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-22 21:20:27 +0500

    Реализация типа-обертки для класса Boolean.

---
+++ src/codex/type/Bool.java
@@ -0,0 +1,41 @@
+package codex.type;
+
+import codex.editor.BoolEditor;
+import codex.editor.IEditorFactory;
+import codex.property.PropertyHolder;
+
+/**
+ * Тип-обертка {@link IComplexType} для класса Boolean.
+ */
+public class Bool implements IComplexType<Boolean> {
+
+    private final static IEditorFactory EDITOR_FACTORY = (PropertyHolder propHolder) -> {
+        return new BoolEditor(propHolder);
+    };
+    
+    private Boolean value;
+    
+    /**
+     * Конструктор типа.
+     * @param value Внутреннее хранимое значение.
+     */
+    public Bool(Boolean value) {
+        this.value = value;
+    }
+
+    @Override
+    public Boolean getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(Boolean value) {
+        this.value = value;
+    }
+
+    @Override
+    public IEditorFactory editorFactory() {
+        return EDITOR_FACTORY;
+    }
+    
+}
