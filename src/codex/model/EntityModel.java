c22998a6006b8fedc917fd78c594766b71c75ec3
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-24 01:40:02 +0500

    Добавление реестра изменений и метода addChangeListener.

--- src/codex/model/EntityModel.java
+++ src/codex/model/EntityModel.java
@@ -1,15 +1,18 @@
 package codex.model;
 
-import codex.property.PropertyHolder;
+import codex.property.PropertyChangeListener;
+import codex.type.IComplexType;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Реализация модели сущности.
  */
-public class EntityModel extends AbstractModel {
+public class EntityModel extends AbstractModel implements PropertyChangeListener {
     
-    private final List<String> persistent = new LinkedList<>();
+    private final List<String> persistent   = new LinkedList<>();
+    private final UndoRegistry undoRegistry = new UndoRegistry();
+    private final List<PropertyChangeListener> listeners = new LinkedList<>();
     
     /**
      * Добавление свойства в сущность с возможностью указания должно ли свойство 
@@ -19,10 +22,40 @@ public class EntityModel extends AbstractModel {
      * селекторе.
      * @param isPersistent Флаг, указывающий что свойсто - хранимое.
      */
-    public void addProperty(PropertyHolder propHolder, Access restriction, boolean isPersistent) {
-        addProperty(propHolder, restriction);
+    public void addProperty(String name, IComplexType value, boolean require, Access restriction, boolean isPersistent) {
+        addProperty(name, value, require, restriction);
         if (isPersistent) {
-            persistent.add(propHolder.getName());
+            persistent.add(name);
+        }
+    }
+
+    @Override
+    public void addProperty(String name, IComplexType value, boolean require, Access restriction) {
+        super.addProperty(name, value, require, restriction);
+        getProperty(name).addChangeListener(this);
+    }
+    
+    /**
+     * Добавление слушателя события изменения значения свойства.
+     */
+    public final void addChangeListener(PropertyChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void propertyChange(String name, Object oldValue, Object newValue) {
+        undoRegistry.put(name, oldValue, newValue);
+        listeners.forEach((listener) -> {
+            listener.propertyChange(name, oldValue, newValue);
+        });
+    }
+
+    @Override
+    public Object getValue(String name) {
+        if (undoRegistry.exists(name)) {
+            return undoRegistry.previous(name);
+        } else {
+            return super.getValue(name);
         }
     }
     
