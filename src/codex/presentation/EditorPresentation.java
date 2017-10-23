b92e20ade8e290f167726e0654e53876138ce715
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-23 20:49:26 +0500

    Документирование.

--- src/codex/presentation/EditorPresentation.java
+++ src/codex/presentation/EditorPresentation.java
@@ -1,15 +1,23 @@
 package codex.presentation;
 
-import codex.explorer.tree.AbstractNode;
+import codex.explorer.tree.Entity;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 
+/**
+ * Презентация редактора сущности. Реализует как функциональность редактирования
+ * непосредственно свойств сущности при помощи редакторов в составе страницы
+ * редактора, так и обеспечивает работу команд сущностей.
+ */
 public final class EditorPresentation extends JPanel {
  
-    public EditorPresentation(AbstractNode node) {
+    /**
+     * Конструктор презентации. 
+     * @param entity Редактируемая сущность.
+     */
+    public EditorPresentation(Entity entity) {
         super(new BorderLayout());
-        
-        add(new EditorPage(node));
+        add(new EditorPage(entity));
     }
     
 }
--- src/codex/presentation/SelectorPresentation.java
+++ src/codex/presentation/SelectorPresentation.java
@@ -4,10 +4,18 @@ import java.awt.Color;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
+/**
+ * Презентация селектора сущности. Реализует как функциональность отображения и 
+ * редактирования дочерних сущностей, так и обеспечивает работу команд по созданию
+ * новых сущностей.
+ */
 public final class SelectorPresentation extends JPanel {
     
+    /**
+     * Конструктор презентации. 
+     */
     public SelectorPresentation() {
-        setBorder(new LineBorder(Color.cyan, 2));
+        setBorder(new LineBorder(Color.RED, 1));
     }
     
 }
