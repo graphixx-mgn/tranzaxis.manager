d800bf25dd22b4eca8fa65189e34d4bd3fb1d17a
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-25 00:22:31 +0500

    Реализация команды сохранения изменений сущности.

---
+++ src/codex/presentation/CommitEntity.java
@@ -0,0 +1,37 @@
+package codex.presentation;
+
+import codex.command.EntityCommand;
+import codex.model.Entity;
+import codex.utils.ImageUtils;
+import codex.utils.Language;
+
+/**
+ * Команда сохранения изменений сущности.
+ */
+public class CommitEntity extends EntityCommand {
+
+    /**
+     * Конструктор команды.
+     */
+    public CommitEntity() {
+        super(
+                "commit", null,
+                ImageUtils.resize(ImageUtils.getByPath("/images/save.png"), 28, 28), 
+                Language.get(EditorPresentation.class.getSimpleName(), "command@commit"),
+                (entity) -> {
+                    return entity.model.hasChanges();
+                }
+        );
+    }
+
+    @Override
+    public void execute(Entity context) {
+        context.model.commit();
+    }
+
+    @Override
+    public void modelRestored() {
+        activator.accept(getContext());
+    }
+    
+}
