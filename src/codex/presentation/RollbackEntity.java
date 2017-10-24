a2367ebc8856ab099d832e1b0ccc973cdb835a24
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-25 00:24:37 +0500

    Реализация команды отката изменений сущности.

---
+++ src/codex/presentation/RollbackEntity.java
@@ -0,0 +1,37 @@
+package codex.presentation;
+
+import codex.command.EntityCommand;
+import codex.model.Entity;
+import codex.utils.ImageUtils;
+import codex.utils.Language;
+
+/**
+ * Команда отката изменений сущности.
+ */
+public class RollbackEntity extends EntityCommand {
+    
+    /**
+     * Конструктор команды.
+     */
+    public RollbackEntity() {
+        super(
+                "rollback", null,
+                ImageUtils.resize(ImageUtils.getByPath("/images/undo.png"), 28, 28), 
+                Language.get(EditorPresentation.class.getSimpleName(), "command@rollback"),
+                (entity) -> {
+                    return entity.model.hasChanges();
+                }
+        );
+    }
+
+    @Override
+    public void execute(Entity context) {
+        context.model.rollback();
+    }
+    
+    @Override
+    public void modelSaved() {
+        activator.accept(getContext());
+    }
+
+}
