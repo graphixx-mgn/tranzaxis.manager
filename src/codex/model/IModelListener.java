d2ac9467fcd433c056d53040753bf501a0a5a5fa
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-24 23:17:24 +0500

    Слушатель событий модели сущности.

---
+++ src/codex/model/IModelListener.java
@@ -0,0 +1,26 @@
+package codex.model;
+
+import java.util.List;
+
+/**
+ * Интерфейс слушателя событий изменения состояния модели с ущности {@link EntityModel}
+ */
+public interface IModelListener {
+    
+    /**
+     * Вызывается при изменении одного из свойств модели.
+     * @param changes Список имен модифицированных свойств.
+     */
+    default void modelChanged(List<String> changes) {};
+    
+    /**
+     * Вызывается при сохранении изменений модели.
+     */
+    default void modelSaved() {};
+    
+    /**
+     * Вызывается при откате изменений модели.
+     */
+    default void modelRestored() {};
+    
+}
