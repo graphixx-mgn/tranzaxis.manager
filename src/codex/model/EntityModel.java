0e4c8154360bf3ce9cba31f75661d1029dfe0955
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-24 23:45:04 +0500

    Реализация механизма отката изменений и заготовка сохранения.

--- src/codex/model/EntityModel.java
+++ src/codex/model/EntityModel.java
@@ -1,18 +1,20 @@
 package codex.model;
 
-import codex.property.PropertyChangeListener;
 import codex.type.IComplexType;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import codex.property.IPropertyChangeListener;
 
 /**
  * Реализация модели сущности.
  */
-public class EntityModel extends AbstractModel implements PropertyChangeListener {
+public class EntityModel extends AbstractModel implements IPropertyChangeListener {
     
     private final List<String> persistent   = new LinkedList<>();
     private final UndoRegistry undoRegistry = new UndoRegistry();
-    private final List<PropertyChangeListener> listeners = new LinkedList<>();
+    private final List<IPropertyChangeListener> changeListeners = new LinkedList<>();
+    private final List<IModelListener>          modelListeners = new LinkedList<>();
     
     /**
      * Добавление свойства в сущность с возможностью указания должно ли свойство 
@@ -38,25 +40,87 @@ public class EntityModel extends AbstractModel implements PropertyChangeListener
     /**
      * Добавление слушателя события изменения значения свойства.
      */
-    public final void addChangeListener(PropertyChangeListener listener) {
-        listeners.add(listener);
+    public final void addChangeListener(IPropertyChangeListener listener) {
+        changeListeners.add(listener);
+    }
+    
+    /**
+     * Добавление слушателя событии модели.
+     */
+    public final void addModelListener(IModelListener listener) {
+        modelListeners.add(listener);
+    }
+    
+    /**
+     * Удаление слушателя событии модели.
+     */
+    public final void removeModelListener(IModelListener listener) {
+        modelListeners.remove(listener);
     }
 
     @Override
     public void propertyChange(String name, Object oldValue, Object newValue) {
-        undoRegistry.put(name, oldValue, newValue);
-        listeners.forEach((listener) -> {
+        if (persistent.contains(name)) {
+            undoRegistry.put(name, oldValue, newValue);
+        }
+        changeListeners.forEach((listener) -> {
             listener.propertyChange(name, oldValue, newValue);
+        });
+        modelListeners.forEach((listener) -> {
+            listener.modelChanged(getChanges());
         });
     }
 
     @Override
-    public Object getValue(String name) {
+    public final Object getValue(String name) {
         if (undoRegistry.exists(name)) {
             return undoRegistry.previous(name);
         } else {
             return super.getValue(name);
         }
+    }
+    
+    /**
+     * Возвращает признак отсуствия несохраненных изменений среди хранимых
+     * свойств модели сущности.
+     */
+    public final boolean hasChanges() {
+        return !undoRegistry.isEmpty();
+    }
+    
+    /**
+     * Возвращает список имен модифицированных свойств.
+     */
+    public final List<String> getChanges() {
+        return persistent.stream()
+                .filter((name) -> {
+                    return undoRegistry.exists(name);
+                })
+                .collect(Collectors.toList());
+    }
+    
+    /**
+     * Сохранение изменений модели.
+     */
+    public final void commit() {
+        System.out.println("codex.model.EntityModel.commit(), listeners="+modelListeners.size());
+        //TODO: Проверить успешность выполнения
+        undoRegistry.clear();
+        modelListeners.forEach((listener) -> {
+            listener.modelSaved();
+        });
+    }
+    
+    /**
+     * Откат изменений модели.
+     */
+    public final void rollback() {
+        getChanges().forEach((name) -> {
+            getProperty(name).setValue(undoRegistry.previous(name));
+        });
+        modelListeners.forEach((listener) -> {
+            listener.modelRestored();
+        });
     }
     
 }
