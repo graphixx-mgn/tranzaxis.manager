44a096c7c036a3a167e98739da4a2f428a90ae27
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-25 00:20:41 +0500

    Реализация команды сохранения изменений сущности.

--- src/codex/presentation/EditorPresentation.java
+++ src/codex/presentation/EditorPresentation.java
@@ -1,7 +1,12 @@
 package codex.presentation;
 
+import codex.command.EntityCommand;
+import codex.model.Access;
 import codex.model.Entity;
 import java.awt.BorderLayout;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import javax.swing.JPanel;
 
 /**
@@ -11,13 +16,27 @@ import javax.swing.JPanel;
  */
 public final class EditorPresentation extends JPanel {
  
+    private final CommandPanel        commandPanel = new CommandPanel();
+    private final List<EntityCommand> commands = new LinkedList<>(Arrays.asList(
+                                                        new CommitEntity(), 
+                                                        new RollbackEntity()
+                                                ));
+    
     /**
      * Конструктор презентации. 
      * @param entity Редактируемая сущность.
      */
     public EditorPresentation(Entity entity) {
         super(new BorderLayout());
-        add(new EditorPage(entity));
+        
+        commandPanel.setVisible(!entity.model.getProperties(Access.Edit).isEmpty());
+        commands.forEach((command) -> {
+            command.setContext(entity);
+        });
+        commandPanel.addCommands(commands.toArray(new EntityCommand[]{}));
+        add(commandPanel, BorderLayout.NORTH);
+        
+        add(new EditorPage(entity), BorderLayout.CENTER);
     }
     
 }
