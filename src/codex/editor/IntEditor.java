77dbaf1b7b274818a6e0ceb74af5e983a48e8973
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-10-25 14:10:35 +0500

    Фикс: фильтр символов не нужен.

--- src/codex/editor/IntEditor.java
+++ src/codex/editor/IntEditor.java
@@ -2,8 +2,6 @@ package codex.editor;
 
 import codex.property.PropertyHolder;
 import codex.type.Int;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 /**
  * Редактор свойств типа {@link Int}, представляет собой поле ввода.
@@ -15,26 +13,17 @@ public class IntEditor extends StrEditor {
      * @param propHolder Редактируемое свойство.
      */
     public IntEditor(PropertyHolder propHolder) {
-        super(propHolder, (text) -> {
-            try {
-                return text.isEmpty() || Integer.valueOf(text) <= Integer.MAX_VALUE;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }, Integer::valueOf);
-        
-        textField.addKeyListener(new KeyAdapter() {
-            
-            @Override
-            public void keyTyped(KeyEvent event) {
-                char c = event.getKeyChar();
-                if (!((c >= '0') && (c <= '9') ||
-                     (c == KeyEvent.VK_BACK_SPACE) ||
-                     (c == KeyEvent.VK_DELETE))) {
-                        event.consume();
-                }
-            }
-        });
+        super(
+                propHolder,
+                (text) -> {
+                    try {
+                        return text.isEmpty() || Integer.valueOf(text) <= Integer.MAX_VALUE;
+                    } catch (NumberFormatException e) {
+                        return false;
+                    }
+                },
+                Integer::valueOf
+        );
     }
 
 }
