5e81f9e8bdb6f1f5c7b43c1c43f43a5dbf34973c
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-26 20:46:11 +0500

    Добавлен узел "Разработка".

---
+++ src/manager/nodes/Development.java
@@ -0,0 +1,13 @@
+package manager.nodes;
+
+import codex.model.Entity;
+import codex.utils.ImageUtils;
+import codex.utils.Language;
+
+public class Development extends Entity {
+    
+    public Development() {
+        super(ImageUtils.getByPath("/images/development.png"), Language.get("title"), Language.get("desc"));
+    }
+    
+}
