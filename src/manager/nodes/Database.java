cbe3d8e0fc4951a50a077b87f02e2b8b0f3f3ae5
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-10-27 08:53:02 +0500

    Рефакторинг.

--- src/manager/nodes/Database.java
+++ src/manager/nodes/Database.java
@@ -7,10 +7,12 @@ import codex.utils.ImageUtils;
 
 public class Database extends Entity {
     
-    public Database() {
-        super(ImageUtils.getByPath("/images/database.png"), "WIRECARD_DEV_TRUNK", null);
+    public Database(String title) {
+        super(ImageUtils.getByPath("/images/database.png"), title, null);
         
-        model.addProperty("dbUrl", new Str(null), true, Access.Select, true);
+        model.addProperty("dbUrl", 
+                new Str(null)/*.setMask(new RegexMask("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}:\\d{1,5}/\\w+"))*/,
+        true, Access.Select, true);
         model.addProperty("dbSchema", new Str(null), true, null, true);
         model.addProperty("dbPass", new Str(null), true, Access.Select, true);
         model.addProperty("instanceId", new Str(null), true, Access.Select, true);
