6bc06bd0b4bb8bec934b8fa257a2aab704b5f390
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-11-02 03:55:27 +0500

    Рефакторинг под изменения в Entity.

--- src/manager/nodes/Database.java
+++ src/manager/nodes/Database.java
@@ -9,18 +9,23 @@ import codex.utils.Language;
 
 public class Database extends Entity {
     
+    private int counter = 0;
+
     public Database(String title) {
         super(ImageUtils.getByPath("/images/database.png"), title, null);
         
-        model.addProperty("dbUrl", 
+        model.addUserProp("dbUrl", 
                 new Str(null).setMask(new RegexMask("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}:\\d{1,5}/\\w+", Language.get("dbUrl.error"))),
-        true, Access.Select, true);
-        model.addProperty("dbSchema", new Str(null), true, null, true);
-        model.addProperty("dbPass", new Str(null), true, Access.Select, true);
-        model.addProperty("instanceId", new Str(null), true, Access.Select, true);
-        model.addProperty("layerURI", new Str(null), true, Access.Select, true);
-        model.addProperty("version", new Str(null), true, null, true);
-        model.addProperty("userNote", new Str(null), false, null, true);
+        true, Access.Select);
+        model.addUserProp("dbSchema", new Str(null), true, null);
+        model.addUserProp("dbPass", new Str(null), true, Access.Select);
+        model.addDynamicProp("version", new Str(null), Access.Select, () -> {
+            return model.getValue("dbSchema");
+        },
+        "dbSchema", "dbPass");
+        model.addUserProp("instanceId", new Str(null), true, Access.Select);
+        model.addUserProp("layerURI", new Str(null), true, Access.Select);
+        model.addUserProp("userNote", new Str(null), false, null);
     }
     
 }
