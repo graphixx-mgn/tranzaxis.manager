3f40ae2cf145a42c0ce3557228c4abe26b608c8e
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-11-15 01:34:49 +0500

    Получение экземпляра перечисления по системной локали.

--- src/manager/type/Locale.java
+++ src/manager/type/Locale.java
@@ -2,19 +2,22 @@ package manager.type;
 
 import codex.type.Iconified;
 import codex.utils.ImageUtils;
+import java.util.EnumSet;
 import javax.swing.ImageIcon;
 
 public enum Locale implements Iconified {
     
-    Russian("Русский", ImageUtils.getByPath("/images/rus.png")),
-    English("English", ImageUtils.getByPath("/images/eng.png"));
+    Russian("Русский", ImageUtils.getByPath("/images/rus.png"), new java.util.Locale("ru", "RU")),
+    English("English", ImageUtils.getByPath("/images/eng.png"), new java.util.Locale("en", "US"));
     
     private final String    title;
     private final ImageIcon icon;
+    private final java.util.Locale locale;
     
-    private Locale(String title, ImageIcon icon) {
-        this.title = title;
-        this.icon  = icon;
+    private Locale(String title, ImageIcon icon, java.util.Locale locale) {
+        this.title  = title;
+        this.icon   = icon;
+        this.locale = locale;
     }
     
     @Override
@@ -25,6 +28,15 @@ public enum Locale implements Iconified {
     @Override
     public String toString() {
         return title;
+    }
+    
+    public static Locale valueOf(java.util.Locale locale) {
+        for (Locale localeItem : EnumSet.allOf(Locale.class)) {
+            if (localeItem.locale.equals(locale)) {
+                return localeItem;
+            }
+        }
+        return English;
     }
     
 }
