796d4674233cf5869d4e4bd4918be7cad4052d4e
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-11-15 00:03:20 +0500

    Режим создания страницы редактора - 'EDIT'.

--- src/codex/presentation/EditorPresentation.java
+++ src/codex/presentation/EditorPresentation.java
@@ -41,7 +41,7 @@ public final class EditorPresentation extends JPanel {
         });
         commandPanel.setVisible(!commands.isEmpty());  
         add(commandPanel, BorderLayout.NORTH);
-        add(new EditorPage(entity), BorderLayout.CENTER);
+        add(new EditorPage(entity, EditorPage.Mode.Edit), BorderLayout.CENTER);
     }
     
     /**
