5c3e730c6b4622cb0b3b3165eb33cc92f0bf7582
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-11-21 20:27:04 +0500

    Ярлык запуска команды с панели быстрого доступа.

---
+++ src/codex/explorer/browser/CommandLauncher.java
@@ -0,0 +1,70 @@
+package codex.explorer.browser;
+
+import codex.command.EntityCommand;
+import codex.model.Entity;
+import codex.utils.ImageUtils;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.MessageFormat;
+import javax.swing.JLabel;
+import javax.swing.event.ChangeEvent;
+
+/**
+ * Ярлык запуска команды с панели быстрого доступа {@link Launcher}.
+ */
+public final class CommandLauncher extends LaunchButton {
+    
+    private final JLabel signDelete;
+    
+    /**
+     * Конструктор ярлыка.
+     * @param entity Ссылка на сущность.
+     * @param command Ссылка на команду, доступную для класса сущности.
+     */
+    CommandLauncher(Entity entity, EntityCommand command) {
+        super(
+                MessageFormat.format("{0} ({1})", command.getButton().getText(), entity.getPID()),
+                command.getButton().getIcon()
+        );
+        setLayout(null);
+        
+        signDelete = new JLabel(ImageUtils.resize(ImageUtils.getByPath("/images/clearval.png"), 18, 18));
+        signDelete.setVisible(false);
+        add(signDelete);
+        
+        
+        Insets insets = signDelete.getInsets();
+        Dimension size = signDelete.getPreferredSize();
+        signDelete.setBounds(
+                getPreferredSize().width - size.width - 3, 
+                insets.top + 3,
+                size.width, 
+                size.height
+        );
+        
+        addActionListener((event) -> {
+            command.execute(entity);
+        });
+        
+        signDelete.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                getModel().setRollover(true);
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {
+                //TODO: delete from DB
+                System.err.println("delete launcher");
+            }
+        });
+    }
+    
+    @Override
+    public void stateChanged(ChangeEvent event) {
+        super.stateChanged(event);
+        signDelete.setVisible(getModel().isRollover());
+    }
+    
+}
