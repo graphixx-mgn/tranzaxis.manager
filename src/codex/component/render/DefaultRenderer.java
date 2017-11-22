ff20990b2f76a46d9c8f96bd91db7aee8d13de87
GraphiXX <GraphiXX@GraphiXX-PC.lan>
2017-11-22 22:54:21 +0500

    Рефакторинг.

--- src/codex/component/render/DefaultRenderer.java
+++ src/codex/component/render/DefaultRenderer.java
@@ -3,15 +3,19 @@ package codex.component.render;
 import codex.component.button.IButton;
 import codex.editor.AbstractEditor;
 import codex.editor.IEditor;
+import codex.explorer.tree.INode;
+import codex.model.Entity;
 import codex.type.Enum;
 import codex.type.Iconified;
 import codex.type.StringList;
 import codex.utils.ImageUtils;
 import java.awt.Color;
 import java.awt.Component;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JTable;
+import javax.swing.JTree;
 import javax.swing.ListCellRenderer;
 import javax.swing.SwingConstants;
 import static javax.swing.SwingConstants.CENTER;
@@ -20,11 +24,12 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
 import javax.swing.event.TableModelEvent;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.tree.TreeCellRenderer;
 
 /**
- * Стандартный рендерер ячеек списка и таблицы.
+ * Стандартный рендерер ячеек списка, таблицы и элементов дерева.
  */
-public final class DefaultRenderer extends JLabel implements ListCellRenderer, TableCellRenderer {
+public final class DefaultRenderer extends JLabel implements ListCellRenderer, TableCellRenderer, TreeCellRenderer {
     
     public DefaultRenderer() {
         setOpaque(true);
@@ -98,6 +103,41 @@ public final class DefaultRenderer extends JLabel implements ListCellRenderer, T
                         row == TableModelEvent.HEADER_ROW ? Color.GRAY : Color.LIGHT_GRAY), 
                 new EmptyBorder(1, 6, 0, 0)
         ));
+        return this;
+    }
+
+    /**
+     * Метод задает внешний вид элементов дерева. Вызывается для каждого элемента
+     * при перерисовке виджета.
+     * @param tree Виджет-дерево.
+     * @param value Ссылка на узел дерева. 
+     * @param selected Признак того что узел дерева выделен.
+     * @param expanded Признак того что узел дерева развернут.
+     * @param leaf Признак что указанный узел не имеет потомков.
+     * @param row Сквозной индекс узла в дереве.
+     * @param hasFocus Признак того что узел имеет фокус.
+     */
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+        Entity entity = (Entity) value;
+        int iconSize = tree.getRowHeight()-2;
+        ImageIcon icon;
+        if (!entity.model.isValid()) {
+            icon = ImageUtils.resize(ImageUtils.combine(
+                entity.getIcon(),
+                ImageUtils.getByPath("/images/warn.png")    
+            ), iconSize, iconSize);
+        } else {
+            icon = ImageUtils.resize(entity.getIcon(), iconSize, iconSize);
+        }
+        setDisabledIcon(ImageUtils.grayscale(icon));
+        setIcon(icon);
+        setText(entity.toString());
+        
+        setForeground(selected ? Color.WHITE : IEditor.COLOR_NORMAL);
+        setBackground(selected ? Color.decode("#55AAFF") : Color.WHITE);
+        setBorder(new EmptyBorder(15, 2, 15, 7));
+        setEnabled((entity.getMode() & INode.MODE_ENABLED) == INode.MODE_ENABLED);
         return this;
     }
     
