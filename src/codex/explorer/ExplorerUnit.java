38fff33a55337dbc4f27a52acae45c1355b53269
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-11-22 14:33:44 +0500

    Реструктуризация макета проводника.

--- src/codex/explorer/ExplorerUnit.java
+++ src/codex/explorer/ExplorerUnit.java
@@ -1,33 +1,22 @@
 package codex.explorer;
 
 import codex.explorer.browser.Browser;
-import codex.explorer.launcher.Launcher;
 import codex.explorer.tree.INode;
 import codex.explorer.tree.Navigator;
 import codex.explorer.tree.NodeTreeModel;
 import codex.log.Logger;
 import codex.service.ServiceRegistry;
 import codex.unit.AbstractUnit;
-import codex.utils.ImageUtils;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
-import javax.swing.JTabbedPane;
-import javax.swing.border.MatteBorder;
 import javax.swing.tree.TreePath;
 
 public final class ExplorerUnit extends AbstractUnit {
     
-    private final static ImageIcon LAUNCH = ImageUtils.resize(ImageUtils.getByPath("/images/launch.png"), 20, 20);
-    private final static ImageIcon VIEWER = ImageUtils.resize(ImageUtils.getByPath("/images/viewer.png"), 20, 20);
-    
-    private JTabbedPane tabPanel;
-    private JScrollPane launchPanel;
     private JScrollPane browsePanel;
     private JScrollPane navigatePanel;
     
@@ -50,42 +39,30 @@ public final class ExplorerUnit extends AbstractUnit {
     
     @Override
     public JComponent createViewport() {
-        tabPanel = new JTabbedPane();
-        tabPanel.setFocusable(false);
-        tabPanel.setBorder(new MatteBorder(1, 0, 0, 0, Color.GRAY));
-        tabPanel.setTabPlacement(JTabbedPane.LEFT);
-        
-        launchPanel = new JScrollPane();
-        launchPanel.setBorder(null);
-        tabPanel.addTab(null, LAUNCH, launchPanel);
-
         JSplitPane splitPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true);
         splitPanel.setBorder(null);
         splitPanel.setDividerLocation(250);
         splitPanel.setContinuousLayout(true);
         splitPanel.setDividerSize(6);
-        tabPanel.addTab(null, VIEWER, splitPanel);
-        
+
         JPanel leftPanel = new JPanel(new BorderLayout());
         leftPanel.setMinimumSize(new Dimension(250, 0));
         navigatePanel = new JScrollPane();
         navigatePanel.setBorder(null);
         leftPanel.add(navigatePanel, BorderLayout.CENTER);
         splitPanel.setLeftComponent(leftPanel);
-        
+
         JPanel rightPanel = new JPanel(new BorderLayout());
         rightPanel.setMinimumSize(new Dimension(500, 0));
         browsePanel = new JScrollPane();
         browsePanel.setBorder(null);
         rightPanel.add(browsePanel, BorderLayout.CENTER);
         splitPanel.setRightComponent(rightPanel);
-        
-        return tabPanel;
+        return splitPanel;
     }
 
     @Override
     public void viewportBound() {
-        launchPanel.setViewportView(new Launcher());
         navigator.setModel(treeModel);
         navigatePanel.setViewportView(navigator);
         browsePanel.setViewportView(browser);
