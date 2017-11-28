1e15c61f20222cc69de1f70052a298ab7d784aaa
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-11-28 19:51:36 +0500

    Рефакторинг.

---
+++ src/codex/provider/IDataSupplier.java
@@ -0,0 +1,12 @@
+package codex.provider;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Базовый интерфейс поставщика данных.
+ * @param <T> Тип возвращаемого поставщиком значения.
+ */
+public interface IDataSupplier<T> extends Callable<T> {
+    
+    
+}
