8d6b432a842aeff6679c6851cea7ed731e248041
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-11-29 23:52:39 +0500

    Интерфейс сервиса взаимодействия с базой данных

---
+++ src/codex/database/IDatabaseAccessService.java
@@ -0,0 +1,43 @@
+package codex.database;
+
+import codex.property.PropertyHolder;
+import codex.service.IService;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Интерфейс сервиса взаимодействия с базой данных.
+ */
+public interface IDatabaseAccessService extends IService {
+    
+    /**
+     * Регистрация соединения/пула для выполнения дпльнейших операций. Возвращает 
+     * уникальный идентификатор соединения. Для каждой уникальной комбинации
+     * адрес-пользователь создается отдельное соединение.
+     * @param url Адрес БД в формате JDBC для конкретного драйвера.
+     * @param user Имя пользователя.
+     * @param password Пароль.
+     * @throws SQLException 
+     */
+    default Integer registerConnection(String url, String user, String password) throws SQLException {
+        return null;
+    }
+    
+    /**
+     * Исполнение запроса на выборку данных из БД.
+     * @param connectionID Идентификатор соединения.
+     * @param query Запрос, при необходимости включающий в себя параметры.
+     * @param params Список значений параметров запроса, не указывать если 
+     * параметров нет.
+     * @throws SQLException 
+     */
+    default ResultSet select(Integer connectionID, String query, PropertyHolder... params) throws SQLException {
+        return null;
+    }
+    
+    @Override
+    default String getTitle() {
+        return "Database Access Service";
+    }
+    
+}
