8efe0767c196f08f32642565d78fc275a76340fe
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2017-12-06 19:33:45 +0500

    Базовый интерфейс маски поля типа ArrStr.

---
+++ src/codex/mask/IArrMask.java
@@ -0,0 +1,25 @@
+package codex.mask;
+
+import codex.command.ICommand;
+import codex.property.PropertyHolder;
+import codex.type.ArrStr;
+import java.util.List;
+
+/**
+ * Базовый интерфейс маски поля типа {@link ArrStr}. Также реализует интерфейс 
+ * команды редактора. При назначении маски стандартный диалог редактора поля 
+ * заменяется на предоставляемый конкретной реализацией маски.
+ */
+public interface IArrMask extends IMask<List<String>>, ICommand<PropertyHolder> {
+    
+    /**
+     * Формат отображения элементов списка в редакторе и селекторе. Указывается 
+     * произвольная строка с точками вставки элементов списка в виде '{n}', где
+     * n - индекс значения в списке. Если значений больше чем мест вставки - 
+     * лишние значени не будут отображаться.
+     */
+    default String getFormat() {
+        return null;
+    }
+    
+}
