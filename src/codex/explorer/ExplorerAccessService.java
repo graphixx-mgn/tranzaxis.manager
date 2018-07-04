bd73f727b0a66c9c12cb6ff781fd24d9c3f9a319
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-07-04 09:25:36 +0500

    Исправление NPE.

--- src/codex/explorer/ExplorerAccessService.java
+++ src/codex/explorer/ExplorerAccessService.java
@@ -38,7 +38,7 @@ public class ExplorerAccessService implements IExplorerAccessService {
         for (INode node : model) {
             if (entityClass.equals(node.getChildClass())) {
                 return (Entity) node.childrenList().stream().filter((child) -> {
-                    return ((Entity) child).model.getID().equals(ID);
+                    return ((Entity) child).model.getID() == ID;
                 }).findFirst().orElse(null);
             }
             if (entityClass.isAssignableFrom(node.getClass()) && ((Entity) node).model.getID().equals(ID)) {
