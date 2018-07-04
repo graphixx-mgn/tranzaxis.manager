a83d6997d52700bffa20d9868ce8363d5689d1c5
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-07-04 10:52:15 +0500

    Локализация состояния SVN.

--- src/manager/type/WCStatus.java
+++ src/manager/type/WCStatus.java
@@ -2,15 +2,16 @@ package manager.type;
 
 import codex.type.Iconified;
 import codex.utils.ImageUtils;
+import codex.utils.Language;
 import javax.swing.ImageIcon;
 
 
 public enum WCStatus implements Iconified {
 
-    Absent("<none>", ImageUtils.getByPath("/images/svn_none.png")),
-    Interrupted("<interrupted>", ImageUtils.getByPath("/images/svn_interrupted.png")),
-    Erroneous("<erroneous>", ImageUtils.getByPath("/images/svn_erroneous.png")),
-    Succesfull("<successful>", ImageUtils.getByPath("/images/svn_successful.png"));
+    Absent(Language.get("none"), ImageUtils.getByPath("/images/svn_none.png")),
+    Interrupted(Language.get("interrupted"), ImageUtils.getByPath("/images/svn_interrupted.png")),
+    Erroneous(Language.get("erroneous"), ImageUtils.getByPath("/images/svn_erroneous.png")),
+    Succesfull(Language.get("successful"), ImageUtils.getByPath("/images/svn_successful.png"));
     
     private final String    title;
     private final ImageIcon icon;
