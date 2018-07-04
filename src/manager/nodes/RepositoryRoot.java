0980fde38b505b897ed1f6807f0b3100a100cb60
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-07-04 09:49:10 +0500

    Рефакторинг.

--- src/manager/nodes/RepositoryRoot.java
+++ src/manager/nodes/RepositoryRoot.java
@@ -1,13 +1,14 @@
 package manager.nodes;
 
+import codex.explorer.tree.INode;
 import codex.model.Catalog;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
 
 public class RepositoryRoot extends Catalog {
 
-    public RepositoryRoot() {
-        super(ImageUtils.getByPath("/images/repositories.png"), Language.get("desc"));
+    public RepositoryRoot(INode parent) {
+        super(parent, ImageUtils.getByPath("/images/repositories.png"), "title", Language.get("desc"));
     }
 
     @Override
