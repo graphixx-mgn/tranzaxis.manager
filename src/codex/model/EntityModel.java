5a179a09402ab891747a758466150c883c131e4f
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-08-14 11:22:20 +0500

    Исправление NPE при создании сущности, не имеющей владельца.

--- src/codex/model/EntityModel.java
+++ src/codex/model/EntityModel.java
@@ -340,7 +340,8 @@ public class EntityModel extends AbstractModel implements IPropertyChangeListene
                         propDefinitions.put(propName, getProperty(propName).getPropValue());
                     });
                 
-                Integer ownerId = ((EntityRef) getProperty(EntityModel.OWN).getPropValue()).getValue().model.getID();
+                EntityRef ownerRef = (EntityRef) getProperty(EntityModel.OWN).getPropValue();
+                Integer ownerId = ownerRef.getValue() != null ? ownerRef.getValue().model.getID() : null;
                 Map<String, Integer> keys = CAS.initClassInstance(entityClass, getPID(), propDefinitions, ownerId);
                 setValue(ID, keys.get(ID));
                 setValue(SEQ, keys.get(SEQ));
