e475b28a94c59c156ecdf19606e8695d3c63242e
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-08-15 12:47:22 +0500

    Поддержка групп команд.

--- src/codex/presentation/CommandPanel.java
+++ src/codex/presentation/CommandPanel.java
@@ -1,9 +1,14 @@
 package codex.presentation;
 
 import codex.command.EntityCommand;
+import codex.component.button.GroupButton;
+import codex.component.button.GroupItemButton;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
@@ -17,6 +22,8 @@ import javax.swing.border.MatteBorder;
  * Панель команд презентаций редактора и селектора сущности.
  */
 public final class CommandPanel extends Box {
+    
+    private final Map<String, GroupButton> groupButtons = new HashMap<>();
     
     /**
      * Конструктор панели.
@@ -37,8 +44,32 @@ public final class CommandPanel extends Box {
      */
     public void addCommands(EntityCommand ... commands) {
         for (EntityCommand command : commands) {
-            add((JComponent) command.getButton());
-            add(Box.createHorizontalStrut(5));
+            if (command.getGroupId() != null) {
+                if (!groupButtons.containsKey(command.getGroupId())) {
+                    GroupButton groupButton = new GroupButton(command.getIcon());
+                    groupButton.addActionListener(command);
+                    try {
+                        Field button = EntityCommand.class.getDeclaredField("button");
+                        button.setAccessible(true);
+                        button.set(command, groupButton);
+                    } catch (NoSuchFieldException | IllegalAccessException e) {}
+                    
+                    groupButtons.put(command.getGroupId(), groupButton);
+                    add((JComponent) groupButton);
+                    add(Box.createHorizontalStrut(5));
+                } else {
+                    GroupItemButton groupItem = groupButtons.get(command.getGroupId()).createGroupItem(command.getIcon(), command.toString());
+                    groupItem.addActionListener(command);
+                    try {
+                        Field button = EntityCommand.class.getDeclaredField("button");
+                        button.setAccessible(true);
+                        button.set(command, groupItem);
+                    } catch (NoSuchFieldException | IllegalAccessException e) {}
+                }
+            } else {
+                add((JComponent) command.getButton());
+                add(Box.createHorizontalStrut(5));
+            }
         }
     }
     
