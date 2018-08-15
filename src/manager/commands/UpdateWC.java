474df50aacc399ea59bac38081b9ccf6814d2354
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-08-15 12:49:51 +0500

    Рефакторинг сообщений трассировки.

--- src/manager/commands/UpdateWC.java
+++ src/manager/commands/UpdateWC.java
@@ -12,9 +12,13 @@ import codex.type.IComplexType;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
 import java.io.File;
+import java.nio.channels.ClosedChannelException;
 import java.text.MessageFormat;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+import javax.swing.SwingUtilities;
 import manager.nodes.Offshoot;
 import manager.svn.SVN;
 import manager.type.WCStatus;
@@ -39,6 +43,7 @@ public class UpdateWC extends EntityCommand {
                     return !entity.model.getValue("wcStatus").equals(WCStatus.Invalid);
                 }
         );
+        setGroupId("update");
     }
 
     @Override
@@ -53,7 +58,7 @@ public class UpdateWC extends EntityCommand {
         );
     }
     
-    private class UpdateTask extends AbstractTask<Void> {
+    public class UpdateTask extends AbstractTask<Void> {
 
         private final Offshoot offshoot;
 
@@ -71,82 +76,105 @@ public class UpdateWC extends EntityCommand {
             String repoUrl = Entity.getOwner(offshoot).model.getValue("repoUrl")+"/dev/"+offshoot.model.getPID();
             
             setProgress(0, Language.get(UpdateWC.class.getSimpleName(), "command@calc"));
-            Long changes = SVN.diff(wcPath, repoUrl, SVNRevision.HEAD, null, null);
-            if (changes > 0) {
-                Logger.getLogger().info(
-                        "Start update working copy\n"+
-                        "                     * Remote URL: ''{0}''\n"+
-                        "                     * Local path: ''{1}''", 
-                        new Object[]{repoUrl, wcPath}
-                );
-                AtomicInteger loaded   = new AtomicInteger(0);
-                AtomicInteger added    = new AtomicInteger(0);
-                AtomicInteger deleted  = new AtomicInteger(0);
-                AtomicInteger restored = new AtomicInteger(0);
-                AtomicInteger changed  = new AtomicInteger(0);
-                
-                SVN.update(repoUrl, wcPath, SVNRevision.HEAD, null, null, new ISVNEventHandler() {
-                        @Override
-                        public void handleEvent(SVNEvent event, double d) throws SVNException {
-                            if (event.getAction() != SVNEventAction.UPDATE_STARTED && event.getAction() != SVNEventAction.UPDATE_COMPLETED) {
-                                if (event.getNodeKind() != SVNNodeKind.DIR) {
-                                    loaded.addAndGet(1);
-                                    int percent = (int) (loaded.get() * 100 / changes);
-                                    setProgress(
-                                            percent > 100 ? 100 : percent, 
-                                            MessageFormat.format(
-                                                    Language.get(UpdateWC.class.getSimpleName(), "command@progress"), 
-                                                    event.getFile().getPath().replace(wcPath+File.separator, "")
-                                            )
-                                    );
-                                    SVNEventAction action = event.getAction();
-                                    if (action == SVNEventAction.UPDATE_ADD) {
-                                        added.addAndGet(1);
-                                    } else if (action == SVNEventAction.UPDATE_DELETE) {
-                                        deleted.addAndGet(1);
-                                    } else if (action == SVNEventAction.UPDATE_UPDATE) {
-                                        changed.addAndGet(1);
-                                    } else if (action == SVNEventAction.RESTORE) {
-                                        restored.addAndGet(1);
-                                    } else {
-                                        System.err.println(action + " / " + event.getFile().getPath().replace(wcPath+File.separator, ""));
+            try {
+                Long changes = SVN.diff(wcPath, repoUrl, SVNRevision.HEAD, null, null, new ISVNEventHandler() {
+                    @Override
+                    public void handleEvent(SVNEvent svne, double d) throws SVNException {}
+
+                    @Override
+                    public void checkCancelled() throws SVNCancelException {
+                        if (UpdateTask.this.isCancelled()) {
+                            throw new SVNCancelException();
+                        }
+                    }
+                });
+                if (changes > 0) {
+                    Logger.getLogger().info(
+                            "UPDATE [{0}] started", 
+                            new Object[]{wcPath}
+                    );
+                    AtomicInteger loaded   = new AtomicInteger(0);
+                    AtomicInteger added    = new AtomicInteger(0);
+                    AtomicInteger deleted  = new AtomicInteger(0);
+                    AtomicInteger restored = new AtomicInteger(0);
+                    AtomicInteger changed  = new AtomicInteger(0);
+
+                    SVN.update(repoUrl, wcPath, SVNRevision.HEAD, null, null, new ISVNEventHandler() {
+                            @Override
+                            public void handleEvent(SVNEvent event, double d) throws SVNException {
+                                if (event.getAction() != SVNEventAction.UPDATE_STARTED && event.getAction() != SVNEventAction.UPDATE_COMPLETED) {
+                                    if (event.getNodeKind() != SVNNodeKind.DIR) {
+                                        loaded.addAndGet(1);
+                                        int percent = (int) (loaded.get() * 100 / changes);
+                                        setProgress(
+                                                percent > 100 ? 100 : percent, 
+                                                MessageFormat.format(
+                                                        Language.get(UpdateWC.class.getSimpleName(), "command@progress"), 
+                                                        event.getFile().getPath().replace(wcPath+File.separator, "")
+                                                )
+                                        );
+                                        SVNEventAction action = event.getAction();
+                                        if (action == SVNEventAction.UPDATE_ADD) {
+                                            added.addAndGet(1);
+                                        } else if (action == SVNEventAction.UPDATE_DELETE) {
+                                            deleted.addAndGet(1);
+                                        } else if (action == SVNEventAction.UPDATE_UPDATE) {
+                                            changed.addAndGet(1);
+                                        } else if (action == SVNEventAction.RESTORE) {
+                                            restored.addAndGet(1);
+                                        } else {
+                                            System.err.println(action + " / " + event.getFile().getPath().replace(wcPath+File.separator, ""));
+                                        }
                                     }
                                 }
                             }
-                        }
 
-                        @Override
-                        public void checkCancelled() throws SVNCancelException {
-                            if (UpdateTask.this.isCancelled()) {
-                                throw new SVNCancelException();
+                            @Override
+                            public void checkCancelled() throws SVNCancelException {
+                                if (UpdateTask.this.isCancelled()) {
+                                    throw new SVNCancelException();
+                                }
                             }
                         }
-                    }
-                );
-                
-                Logger.getLogger().info(
-                        "Update working copy finished\n"+
-                        (added.get()    == 0 ? "" : "                     * Added:    {0}\n")+
-                        (deleted.get()  == 0 ? "" : "                     * Deleted:  {2}\n")+
-                        (restored.get() == 0 ? "" : "                     * Restored: {1}\n")+
-                        (changed.get()  == 0 ? "" : "                     * Changed:  {1}\n")+
-                                                    "                     * Total:    {3}", 
-                        new Object[]{added.get(), changed.get(), deleted.get(), loaded.get()}
-                );
-            } else {
-                Logger.getLogger().info("No need to update working copy: {0}", wcPath);
+                    );
+
+                    Logger.getLogger().info(
+                            "UPDATE [{0}] finished\n"+
+                            (added.get()    == 0 ? "" : "                     * Added:    {1}\n")+
+                            (deleted.get()  == 0 ? "" : "                     * Deleted:  {2}\n")+
+                            (restored.get() == 0 ? "" : "                     * Restored: {3}\n")+
+                            (changed.get()  == 0 ? "" : "                     * Changed:  {4}\n")+
+                                                        "                     * Total:    {5}", 
+                            new Object[]{wcPath, added.get(), deleted.get(), restored.get(), changed.get(), loaded.get()}
+                    );
+                } else {
+                    Logger.getLogger().info("No need to update working copy: {0}", wcPath);
+                }
+            } catch (SVNException e) {
+                Optional<Throwable> rootCause = Stream
+                        .iterate(e, Throwable::getCause)
+                        .filter(element -> element.getCause() == null)
+                        .findFirst();
+                if (rootCause.get() instanceof SVNCancelException || rootCause.get() instanceof ClosedChannelException) {
+                    Logger.getLogger().warn(
+                            "UPDATE [{0}] canceled", 
+                            new Object[]{wcPath}
+                    );
+                }
             }
             return null;
         }
 
         @Override
         public void finished(Void res) {
-            WCStatus status = offshoot.getStatus();
-            offshoot.model.setValue("wcStatus", status);
-            offshoot.model.setValue("loaded",  !status.equals(WCStatus.Absent));
-            offshoot.model.commit();
-            offshoot.model.updateDynamicProp("localRev");
-            offshoot.setMode(INode.MODE_SELECTABLE + (status.equals(WCStatus.Absent) ? 0 : INode.MODE_ENABLED));
+            SwingUtilities.invokeLater(() -> {
+                WCStatus status = offshoot.getStatus();
+                offshoot.model.setValue("wcStatus", status);
+                offshoot.model.setValue("loaded",  !status.equals(WCStatus.Absent));
+                offshoot.model.commit();
+                offshoot.model.updateDynamicProp("localRev");
+                offshoot.setMode(INode.MODE_SELECTABLE + (status.equals(WCStatus.Absent) ? 0 : INode.MODE_ENABLED));
+            });
         }
     
     }
