8280c146fd15be09dfcf45ea603c9149135734cb
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-08-15 12:46:52 +0500

    Реализация кнопок вызова команд, объединенных в группу.

---
+++ src/codex/component/button/GroupItemButton.java
@@ -0,0 +1,40 @@
+package codex.component.button;
+
+import codex.utils.ImageUtils;
+import javax.swing.ImageIcon;
+import javax.swing.JMenuItem;
+
+/**
+ * Кнопка вторичной команды в группе.
+ */
+public final class GroupItemButton extends JMenuItem implements IButton {
+    
+    /**
+     * Конструктор кнопки.
+     * @param icon Иконка кнопки.
+     * @param title Надпись кнопки.
+     */
+    public GroupItemButton(ImageIcon icon, String title) {
+        super(title, icon);
+        setDisabledIcon(ImageUtils.grayscale(icon));
+    }
+
+    @Override
+    public void setHint(String text) {}
+
+    @Override
+    public void setInactive(boolean inactive) {
+        setEnabled(inactive);
+    }
+
+    @Override
+    public boolean isInactive() {
+        return isEnabled();
+    }
+
+    @Override
+    public void click() {
+        doClick();
+    }
+    
+}
