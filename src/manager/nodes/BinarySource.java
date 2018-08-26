40b2dab24cb428188da28af54a99803712f9ce03
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-08-26 15:46:55 +0500

    Добавлен сортировщик версий по номеру.

--- src/manager/nodes/BinarySource.java
+++ src/manager/nodes/BinarySource.java
@@ -1,12 +1,35 @@
 package manager.nodes;
 
-import codex.explorer.tree.INode;
 import codex.model.Catalog;
+import codex.type.EntityRef;
+import java.util.Comparator;
 import javax.swing.ImageIcon;
 
 public abstract class BinarySource extends Catalog {
+    
+    public static final Comparator<String> VERSION_SORTER = new Comparator<String>() {
+        @Override
+        public int compare(String prev, String next) {
+            if ("trunk".equals(prev)) {
+                return 1;
+            } else if ("trunk".equals(next)) {
+                return -1;
+            } else {
+                String[] components1 = prev.split("\\.");
+                String[] components2 = next.split("\\.");
+                int length = Math.min(components1.length, components2.length);
+                for(int i = 0; i < length; i++) {
+                    int result = new Integer(components1[i]).compareTo(Integer.parseInt(components2[i]));
+                    if(result != 0) {
+                        return result;
+                    }
+                }
+                return Integer.compare(components1.length, components2.length);
+            }
+        }
+    };
 
-    public BinarySource(INode parent, ImageIcon icon, String title) {
+    public BinarySource(EntityRef parent, ImageIcon icon, String title) {
         super(parent, icon, title, null);
     }
 
