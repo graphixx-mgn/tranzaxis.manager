89aa763801e464541195c281dfa55d5936a15acb
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-08-26 15:49:22 +0500

    Рефакторинг.

--- src/manager/nodes/Database.java
+++ src/manager/nodes/Database.java
@@ -5,7 +5,6 @@ import codex.component.messagebox.MessageType;
 import codex.database.IDatabaseAccessService;
 import codex.database.OracleAccessService;
 import codex.database.RowSelector;
-import codex.explorer.tree.INode;
 import codex.mask.DataSetMask;
 import codex.mask.RegexMask;
 import codex.model.Access;
@@ -13,6 +12,7 @@ import codex.model.Entity;
 import codex.service.ServiceRegistry;
 import codex.supplier.IDataSupplier;
 import codex.type.ArrStr;
+import codex.type.EntityRef;
 import codex.type.IComplexType;
 import codex.type.Str;
 import codex.utils.ImageUtils;
@@ -31,7 +31,7 @@ public class Database extends Entity {
         DAS = (IDatabaseAccessService) ServiceRegistry.getInstance().lookupService(OracleAccessService.class);
     }
     
-    private Function<Boolean, Integer> connectionGetter = (showError) -> {
+    private final Function<Boolean, Integer> connectionGetter = (showError) -> {
         if (IComplexType.notNull(
                 model.getUnsavedValue("dbUrl"), 
                 model.getUnsavedValue("dbSchema"), 
@@ -79,9 +79,10 @@ public class Database extends Entity {
             "SELECT LAYERURI, VERSION, UPGRADEDATE FROM RDX_DDSVERSION"
     );
 
-    public Database(INode parent, String title) {
+    public Database(EntityRef parent, String title) {
         super(parent, ImageUtils.getByPath("/images/database.png"), title, null);
         
+        // Properties
         model.addUserProp("dbUrl", 
                 new Str(null).setMask(new RegexMask(
                         "((([0-1]?[0-9]{1,2}\\.)|(2[0-4][0-9]\\.)|(25[0-5]\\.)){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))|[^\\s]+):"+
@@ -90,13 +91,15 @@ public class Database extends Entity {
                         Language.get("dbUrl.error")
                 )),
         true, Access.Select);
-        model.addUserProp("dbSchema",   new Str(null), true, null);
-        model.addUserProp("dbPass",     new Str(null), true, Access.Select);
-        model.addUserProp("layerURI",   new ArrStr().setMask(new DataSetMask(
+        model.addUserProp("dbSchema", new Str(null), true, null);
+        model.addUserProp("dbPass",   new Str(null), true, Access.Select);
+        model.addUserProp("layerURI", new ArrStr().setMask(new DataSetMask(
                 "{0}", layerSupplier
         )),  true, Access.Select);
         model.addUserProp("userNote", new Str(null), false, null);
         
+        
+        // Commands
         addCommand(new CheckDatabase());
     }
     
