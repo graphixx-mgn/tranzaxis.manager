9ab6662f902f1b71a57fb7b7c0fee9d44705c7a5
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-08-29 10:48:57 +0500

    Добавленсемафор для чтения/записи.

--- src/codex/model/EnityCache.java
+++ src/codex/model/EnityCache.java
@@ -1,14 +1,16 @@
 package codex.model;
 
 import codex.type.IComplexType;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Semaphore;
 
 class EnityCache {
     
     private final Map<Class<?>, List<Entity>> store = new HashMap<>();
+    private final Semaphore semaphore = new Semaphore(1, true);
     private final static EnityCache INSTANCE = new EnityCache();
     
     private EnityCache() {}
@@ -19,12 +21,18 @@ class EnityCache {
 
     public Entity findEntity(Class entityClass, Integer ownerId, String PID)  {
         if (store.containsKey(entityClass) && PID != null) {
-            return store.get(entityClass).stream().filter((entity) -> {
-                return 
-                        PID.equals(entity.model.getPID()) && (
-                            (entity.model.getOwner() == null && ownerId == null) || (ownerId.equals(entity.model.getOwner().model.getID()))
-                        );
-            }).findFirst().orElse(null);
+            try {
+                semaphore.acquire();
+                return store.get(entityClass).stream().filter((entity) -> {
+                    return 
+                            PID.equals(entity.model.getPID()) && (
+                                (entity.model.getOwner() == null && ownerId == null) || (ownerId.equals(entity.model.getOwner().model.getID()))
+                            );
+                }). findFirst().orElse(null);
+            } catch (InterruptedException e) {
+            } finally {
+                semaphore.release();
+            }
         }
         return null;
     }
@@ -34,8 +42,14 @@ class EnityCache {
             return;
         }
         if (!store.containsKey(entity.getClass())) {
-            store.put(entity.getClass(), new ArrayList<>());
+            store.put(entity.getClass(), new LinkedList<>());
         }
-        store.get(entity.getClass()).add(entity);
+        try {
+            semaphore.acquire();
+            store.get(entity.getClass()).add(entity);
+        } catch (InterruptedException e) {
+        } finally {
+            semaphore.release();
+        }
     }
 } 
