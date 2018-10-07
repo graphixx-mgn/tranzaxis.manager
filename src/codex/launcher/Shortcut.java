034dca72b8f9f741a39b2ee4cb377d40f36a378e
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-10-07 14:36:02 +0500

    В модель ярлыка добавлена ссылка на секцию.

--- src/codex/launcher/Shortcut.java
+++ src/codex/launcher/Shortcut.java
@@ -9,17 +9,20 @@ public class Shortcut extends Entity {
     public Shortcut(EntityRef parent, String title) {
         super(null, null, title, null);
         try {
+            model.addUserProp("section", new EntityRef(ShortcutSection.class), true, null);
             model.addUserProp("class", new Str(null), true, null);
-            if (model.getValue("class") != null) {
-                Class entityClass = Class.forName((String) model.getValue("class"));
-                model.addUserProp("entity", new EntityRef(entityClass), true, null);
-            } else {
-                model.addUserProp("entity", new EntityRef(null), true, null);
-            }
+            String entityClass = (String) model.getValue("class");
+            model.addUserProp("entity", new EntityRef(
+                    entityClass == null ? null : Class.forName(entityClass)
+            ), true, null);
             model.addUserProp("command", new Str(null), true, null);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+    
+    ShortcutSection getSection() {
+        return (ShortcutSection) model.getValue("section");
     }
     
 }
