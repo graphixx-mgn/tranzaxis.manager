ef35ceed294b075ba38fa63fa8bfe7478a2a8504
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-10-22 19:28:53 +0500

    Изменения в связи с модификацией конструктора супер-класса. Добавлены getter-ы и setter-ы для свойств модели.

--- src/manager/nodes/Repository.java
+++ src/manager/nodes/Repository.java
@@ -1,6 +1,5 @@
 package manager.nodes;
 
-import codex.editor.AbstractEditor;
 import codex.explorer.tree.INode;
 import codex.mask.RegexMask;
 import codex.model.Access;
@@ -9,11 +8,9 @@ import codex.property.PropertyHolder;
 import codex.type.Bool;
 import codex.type.EntityRef;
 import codex.type.Enum;
-import codex.type.IComplexType;
 import codex.type.Str;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
-import java.lang.reflect.Field;
 import manager.commands.LoadWC;
 import manager.type.SVNAuth;
 import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
@@ -23,39 +20,46 @@ import org.tmatesoft.svn.core.auth.SVNPasswordAuthentication;
 
 public class Repository extends Entity {
     
-    public Repository(EntityRef parent, String title) {
-        super(parent, ImageUtils.getByPath("/images/repository.png"), title, null);
+    public final static String PROP_REPO_URL  = "repoUrl";
+    public final static String PROP_AUTH_MODE = "authMode";
+    public final static String PROP_SVN_USER  = "svnUser";
+    public final static String PROP_SVN_PASS  = "svnPass";
+    public final static String PROP_USER_NOTE = "userNote";
+    public final static String PROP_LOCKED    = "locked";
+    
+    public Repository(EntityRef owner, String title) {
+        super(owner, ImageUtils.getByPath("/images/repository.png"), title, null);
         
-        MutablePropHolder svnUser = new MutablePropHolder("svnUser", new Str(null), true);
-        MutablePropHolder svnPass = new MutablePropHolder("svnPass", new Str(null), true);
-
+        PropertyHolder svnUser = new PropertyHolder(PROP_SVN_USER, new Str(null), true);
+        PropertyHolder svnPass = new PropertyHolder(PROP_SVN_PASS, new Str(null), true);
+        
         // Properties
-        model.addUserProp("repoUrl",  new Str(null).setMask(new RegexMask("svn(|\\+[\\w]+)://[\\w\\./\\d]+", "Invalid SVN url")), true, null);
-        model.setPropUnique("repoUrl");
-        model.addUserProp("authMode", new Enum(SVNAuth.None), true, Access.Select);
+        model.addUserProp(PROP_REPO_URL,  new Str(null).setMask(new RegexMask("svn(|\\+[\\w]+)://[\\w\\./\\d]+", "Invalid SVN url")), true, null);
+        model.addUserProp(PROP_AUTH_MODE, new Enum(SVNAuth.None), true, Access.Select);
         model.addUserProp(svnUser, Access.Select);
         model.addUserProp(svnPass, Access.Select);
-        model.addUserProp("locked", new Bool(false), false, Access.Any);
+        model.addUserProp(PROP_USER_NOTE, new Str(null), false, null);
+        model.addUserProp(PROP_LOCKED,    new Bool(false), false, Access.Any);
         
         // Property settings
-        svnUser.setMandatory(model.getValue("authMode") == SVNAuth.Password);
-        svnPass.setMandatory(model.getValue("authMode") == SVNAuth.Password);
+        model.setPropUnique(PROP_REPO_URL);
+        svnUser.setRequired(getAuthMode(true) == SVNAuth.Password);
+        svnPass.setRequired(getAuthMode(true) == SVNAuth.Password);
         
         // Editor settings
-        model.addPropertyGroup(Language.get("group@auth"), "authMode", "svnUser", "svnPass");
-       
-        model.getEditor("repoUrl").setEditable(!(Boolean) model.getValue("locked"));
-        model.getEditor("svnUser").setVisible(model.getValue("authMode").equals(SVNAuth.Password));
-        model.getEditor("svnPass").setVisible(model.getValue("authMode").equals(SVNAuth.Password));
+        model.addPropertyGroup(Language.get("group@auth"), PROP_AUTH_MODE, PROP_SVN_USER, PROP_SVN_PASS);
+
+        model.getEditor(PROP_SVN_USER).setVisible(getAuthMode(true) == SVNAuth.Password);
+        model.getEditor(PROP_SVN_PASS).setVisible(getAuthMode(true) == SVNAuth.Password);
         
         // Handlers
         model.addChangeListener((name, oldValue, newValue) -> {
-            switch (name) {
-                case "authMode":
-                    model.getEditor("svnUser").setVisible(model.getUnsavedValue("authMode").equals(SVNAuth.Password));
-                    model.getEditor("svnPass").setVisible(model.getUnsavedValue("authMode").equals(SVNAuth.Password));
-                    svnUser.setMandatory(model.getUnsavedValue("authMode") == SVNAuth.Password);
-                    svnPass.setMandatory(model.getUnsavedValue("authMode") == SVNAuth.Password);
+           switch (name) {
+                case PROP_AUTH_MODE:
+                    model.getEditor(PROP_SVN_USER).setVisible(getAuthMode(true) == SVNAuth.Password);
+                    model.getEditor(PROP_SVN_PASS).setVisible(getAuthMode(true) == SVNAuth.Password);
+                    svnUser.setRequired(getAuthMode(true) == SVNAuth.Password);
+                    svnPass.setRequired(getAuthMode(true) == SVNAuth.Password);
                     break;
             }
         });
@@ -63,7 +67,47 @@ public class Repository extends Entity {
         // Commands
         addCommand(new LoadWC());
         
-        setMode(((Boolean) model.getValue("locked") ? INode.MODE_ENABLED : INode.MODE_NONE) + INode.MODE_SELECTABLE);
+        setMode((isLocked(false) && getChildCount() > 0 ? INode.MODE_ENABLED : INode.MODE_NONE) + INode.MODE_SELECTABLE);
+    }
+    
+    public final String getRepoUrl() {
+        return (String) model.getValue(PROP_REPO_URL);
+    }
+    
+    public final SVNAuth getAuthMode(boolean unsaved) {
+        return (SVNAuth) (unsaved ? model.getUnsavedValue(PROP_AUTH_MODE) : model.getValue(PROP_AUTH_MODE));
+    }
+    
+    public final String getSvnUser(boolean unsaved) {
+        return (String) (unsaved ? model.getUnsavedValue(PROP_SVN_USER) : model.getValue(PROP_SVN_USER));
+    }
+    
+    public final String getSvnPass(boolean unsaved) {
+        return (String) (unsaved ? model.getUnsavedValue(PROP_SVN_PASS) : model.getValue(PROP_SVN_PASS));
+    }
+    
+    public final boolean isLocked(boolean unsaved) {
+        return (unsaved ? model.getUnsavedValue(PROP_LOCKED) : model.getValue(PROP_LOCKED)) == Boolean.TRUE;
+    }
+    
+    public final void setRepoUrl(SVNAuth value) {
+        model.setValue(PROP_REPO_URL, value);
+    }
+    
+    public final void setAuthMode(String value) {
+        model.setValue(PROP_AUTH_MODE, value);
+    }
+    
+    public final void setSvnUser(String value) {
+        model.setValue(PROP_SVN_USER, value);
+    }
+    
+    public final void setSvnPass(String value) {
+        model.setValue(PROP_SVN_PASS, value);
+    }
+    
+    public final void setLocked(boolean value) {
+        model.setValue(PROP_LOCKED, value);
     }
 
     @Override
@@ -73,13 +117,13 @@ public class Repository extends Entity {
     }
     
     public ISVNAuthenticationManager getAuthManager() {
-        SVNAuth authType = (SVNAuth) model.getUnsavedValue("authMode");
+        SVNAuth authType = getAuthMode(true);
         switch (authType) {
             case Password:
-                return new BasicAuthenticationManager(new SVNAuthentication[] {
+                return new BasicAuthenticationManager(new SVNAuthentication[]{
                     new SVNPasswordAuthentication(
-                            (String) model.getUnsavedValue("svnUser"), 
-                            (String) model.getUnsavedValue("svnPass"), 
+                            getSvnUser(true), 
+                            getSvnPass(true), 
                             false
                     )
                 });
@@ -90,25 +134,6 @@ public class Repository extends Entity {
     
     public static String urlToDirName(String url) {
         return url.replaceAll("svn(|\\+[\\w]+)://([\\w\\./\\d]+)", "$2").replaceAll("[/\\\\]{1}", ".");
-    }
-    
-    private class MutablePropHolder extends PropertyHolder {
-    
-        public MutablePropHolder(String name, IComplexType value, boolean require) {
-            super(name, value, require);
-        }
-        
-        public void setMandatory(boolean mandatory) {
-            try {
-                Field require = PropertyHolder.class.getDeclaredField("require");
-                require.setAccessible(true);
-                require.set(this, mandatory);
-                ((AbstractEditor) model.getEditor(getName())).updateUI();
-            } catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-    
     }
     
 }
