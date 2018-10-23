ea7ea409a7b2ac8cc1ae3327dfce6490bbaed9e8
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-10-23 19:20:46 +0500

    Изменения в связи с модификацией конструктора супер-класса. Добавлены getter-ы и setter-ы для свойств модели.

--- src/manager/nodes/EnvironmentRoot.java
+++ src/manager/nodes/EnvironmentRoot.java
@@ -3,18 +3,37 @@ package manager.nodes;
 import codex.model.Access;
 import codex.model.Catalog;
 import codex.type.ArrStr;
-import codex.type.EntityRef;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
+import java.util.List;
 
 public class EnvironmentRoot extends Catalog {
     
-    public EnvironmentRoot(EntityRef parent) {
-        super(parent, ImageUtils.getByPath("/images/system.png"), "title", Language.get("desc"));
+    public final static String PROP_JVM_SERVER   = "jvmServer";
+    public final static String PROP_JVM_EXPLORER = "jvmExplorer";
+    
+    public EnvironmentRoot() {
+        super(null, ImageUtils.getByPath("/images/system.png"), "title", Language.get("desc"));
         
         // Properties
-        model.addUserProp("jvmServer",    new ArrStr("-Xmx2G"), false, Access.Select);
-        model.addUserProp("jvmExplorer",  new ArrStr("-Xmx1G"), false, Access.Select);
+        model.addUserProp(PROP_JVM_SERVER,   new ArrStr("-Xmx2G"), false, Access.Select);
+        model.addUserProp(PROP_JVM_EXPLORER, new ArrStr("-Xmx1G"), false, Access.Select);
+    }
+    
+    public final List<String> getJvmServer() {
+        return (List<String>) model.getValue(PROP_JVM_SERVER);
+    }
+    
+    public final List<String> getJvmExplorer() {
+        return (List<String>) model.getValue(PROP_JVM_EXPLORER);
+    }
+    
+    public final void setJvmServer(List<String> value) {
+        model.setValue(PROP_JVM_SERVER, value);
+    }
+    
+    public final void setJvmExplorer(List<String> value) {
+        model.setValue(PROP_JVM_EXPLORER, value);
     }
 
     @Override
