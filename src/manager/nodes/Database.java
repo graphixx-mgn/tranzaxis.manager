d723eaefc832fda9351e784fe0e83d6de95ceb52
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-10-24 18:32:40 +0500

    Изменения в связи с модификацией конструктора супер-класса. Добавлены getter-ы и setter-ы для свойств модели.

--- src/manager/nodes/Database.java
+++ src/manager/nodes/Database.java
@@ -18,38 +18,43 @@ import java.sql.SQLException;
 import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.function.Function;
-import manager.commands.CheckDatabase;
+import manager.commands.database.CheckDatabase;
 
 public class Database extends Entity {
+    
+    public final static String PROP_BASE_URL  = "dbUrl";
+    public final static String PROP_BASE_USER = "dbSchema";
+    public final static String PROP_BASE_PASS = "dbPass";
+    public final static String PROP_USER_NOTE = "userNote";
    
-    private static IDatabaseAccessService DAS = OracleAccessService.getInstance();
+    private static final IDatabaseAccessService OAS = OracleAccessService.getInstance();
     
     static {
-        ServiceRegistry.getInstance().registerService(DAS);
+        ServiceRegistry.getInstance().registerService(OAS);
     }
     
     private final Function<Boolean, Integer> connectionGetter = (showError) -> {
-        String url  = (String) model.getUnsavedValue("dbUrl");
-        String user = (String) model.getUnsavedValue("dbSchema"); 
-        String pass = (String) model.getUnsavedValue("dbPass");
-        
+        String url  = getDatabaseUrl(true);
+        String user = getDatabaseUser(true); 
+        String pass = getDatabasePassword(true);
+
         if (IComplexType.notNull(url, user, pass)) {
             if (!CheckDatabase.checkUrlPort(url)) {
                 if (showError) {
                     MessageBox.show(MessageType.WARNING, MessageFormat.format(
                             Language.get(Database.class.getSimpleName(), "error@unavailable"),
-                            model.getPID(), url.substring(0, url.indexOf("/"))
+                            getPID(), url.substring(0, url.indexOf("/"))
                     ));
                 } else {
                     Logger.getLogger().warn(
                             Language.get(Database.class.getSimpleName(), "error@unavailable", Locale.US),
-                            model.getPID(), url.substring(0, url.indexOf("/"))
+                            getPID(), url.substring(0, url.indexOf("/"))
                     );
                 }
                 return null;
             }
             try {
-                return Database.DAS.registerConnection("jdbc:oracle:thin:@//"+url, user, pass);
+                return Database.OAS.registerConnection("jdbc:oracle:thin:@//"+url, user, pass);
             } catch (SQLException e) {
                 if (showError) {
                     MessageBox.show(MessageType.ERROR, e.getMessage());
@@ -75,11 +80,11 @@ public class Database extends Entity {
         return null;
     };
     
-    public Database(EntityRef parent, String title) {
-        super(parent, ImageUtils.getByPath("/images/database.png"), title, null);
+    public Database(EntityRef owner, String title) {
+        super(owner, ImageUtils.getByPath("/images/database.png"), title, null);
 
         // Properties
-        model.addUserProp("dbUrl", 
+        model.addUserProp(PROP_BASE_URL, 
                 new Str(null).setMask(new RegexMask(
                         "((([0-1]?[0-9]{1,2}\\.)|(2[0-4][0-9]\\.)|(25[0-5]\\.)){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))|[^\\s]+):"+
                         "(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9])"+
@@ -87,12 +92,44 @@ public class Database extends Entity {
                         Language.get("dbUrl.error")
                 )),
         true, Access.Select);
-        model.addUserProp("dbSchema", new Str(null), true, null);
-        model.addUserProp("dbPass",   new Str(null), true, Access.Select);
-        model.addUserProp("userNote", new Str(null), false, null);
-        
+        model.addUserProp(PROP_BASE_USER, new Str(null), true, null);
+        model.addUserProp(PROP_BASE_PASS, new Str(null), true, Access.Select);
+        model.addUserProp(PROP_USER_NOTE, new Str(null), false, null);
+
         // Commands
         addCommand(new CheckDatabase());
+    }
+    
+    public final String getDatabaseUrl(boolean unsaved) {
+        return (String) (unsaved? model.getUnsavedValue(PROP_BASE_URL) : model.getValue(PROP_BASE_URL));
+    }
+    
+    public final String getDatabaseUser(boolean unsaved) {
+        return (String) (unsaved? model.getUnsavedValue(PROP_BASE_USER) : model.getValue(PROP_BASE_USER));
+    }
+    
+    public final String getDatabasePassword(boolean unsaved) {
+        return (String) (unsaved? model.getUnsavedValue(PROP_BASE_PASS) : model.getValue(PROP_BASE_PASS));
+    }
+    
+    public final String getUserNote() {
+        return (String) model.getValue(PROP_USER_NOTE);
+    }
+    
+    public final void setDatabaseUrl(String value) {
+        model.setValue(PROP_BASE_URL, value);
+    }
+    
+    public final void setDatabaseUser(String value) {
+        model.setValue(PROP_BASE_USER, value);
+    }
+    
+    public final void setDatabasePassword(String value) {
+        model.setValue(PROP_BASE_PASS, value);
+    }
+    
+    public final void setUserNote(String value) {
+        model.setValue(PROP_USER_NOTE, value);
     }
     
     public Integer getConnectionID(boolean showError) {
