3feeccc7c4f3a1f699f012a8e5b5e434a4f06a4a
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-10-31 13:27:23 +0500

    Временно отключена настройка поведения при сворачивании.

--- src/manager/nodes/Common.java
+++ src/manager/nodes/Common.java
@@ -1,6 +1,5 @@
 package manager.nodes;
 
-
 import codex.component.messagebox.MessageBox;
 import codex.component.messagebox.MessageType;
 import codex.explorer.tree.INode;
@@ -13,12 +12,11 @@ import codex.model.Catalog;
 import codex.model.EntityModel;
 import codex.model.IModelListener;
 import codex.service.ServiceRegistry;
-import codex.type.Bool;
 import codex.type.Enum;
 import codex.type.FilePath;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
-import java.awt.SystemTray;
+import java.nio.file.Path;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
@@ -26,28 +24,28 @@ import java.util.Map;
 import java.util.prefs.Preferences;
 import javax.swing.SwingUtilities;
 import manager.Manager;
-import manager.commands.DiskUsageReport;
+import manager.commands.common.DiskUsageReport;
 import manager.type.Locale;
 
-public class Common extends Catalog {
+public final class Common extends Catalog {
     
-    private final Preferences      prefs  = Preferences.userRoot().node(Manager.class.getSimpleName());
-    private final java.util.Locale locale = new java.util.Locale(
-            java.lang.System.getProperty("user.language"), 
-            java.lang.System.getProperty("user.country")
-    );
+    private final Preferences   PREFERENCES    = Preferences.userRoot().node(Manager.class.getSimpleName());
+    
+    public  final static String PROP_WORK_DIR  = "workDir";
+    public  final static String PROP_LOG_LEVEL = "logLevel";
+    public  final static String PROP_GUI_LANG  = "guiLang";
+    public  final static String PROP_USE_TRAY  = "useTray";
 
     public Common() {
         super(null, ImageUtils.getByPath("/images/settings.png"), "title", Language.get("desc"));
-        
         // Properties
-        model.addUserProp("workDir",  new FilePath(null).setMask(new DirMask()), true, Access.Select);
-        model.addUserProp("logLevel", new Enum(Level.Debug), false, Access.Select);
-        model.addUserProp("guiLang",  new Enum(Locale.valueOf(locale)), false, Access.Select);
-        model.addUserProp("useTray",  new Bool(false), false, Access.Select);
+        model.addUserProp(PROP_WORK_DIR,  new FilePath(null).setMask(new DirMask()), true, Access.Select);
+        model.addUserProp(PROP_LOG_LEVEL, new Enum(Level.Debug), false, Access.Select);
+        model.addUserProp(PROP_GUI_LANG,  new Enum(Locale.valueOf(Language.getLocale())), false, Access.Select);
+        //model.addUserProp(PROP_USE_TRAY,  new Bool(false), false, Access.Select);
 
         // Editor settings
-        model.getEditor("useTray").setEditable(SystemTray.isSupported());
+        //model.getEditor(PROP_USE_TRAY).setEditable(SystemTray.isSupported());
 
         // Handlers
         model.addModelListener(new IModelListener() {
@@ -55,46 +53,82 @@ public class Common extends Catalog {
             public void modelSaved(EntityModel model, List<String> changes) {
                 changes.forEach((propName) -> {
                     switch (propName) {
-                        case "workDir":
+                        case PROP_WORK_DIR:
                             childrenList().forEach((child) -> {
-                                enableChildNode(child, true);
+                                setChildMode(child, getWorkDir() != null);
                             });
                             break;
-                        case "guiLang":
-                            prefs.put(propName, ((Locale) model.getValue(propName)).name());
+                        
+                        case PROP_GUI_LANG:
+                            PREFERENCES.put(propName, ((Locale) model.getValue(propName)).name());
                             SwingUtilities.invokeLater(() -> {
                                 MessageBox.show(MessageType.INFORMATION, Language.get(Common.class.getSimpleName(), "guiLang.notify"));
                             });
                             break;
-                        case "useTray":
-                            prefs.putBoolean(propName, (boolean) model.getValue(propName));
-                            break;
-                        case "logLevel":
+                            
+//                        case "useTray":
+//                            prefs.putBoolean(propName, (boolean) model.getValue(propName));
+//                            break;
+                            
+                        case PROP_LOG_LEVEL:
                             Level minLevel = (Level) model.getValue(propName);
-                            ILogMgmtService logMgmt = (ILogMgmtService) ServiceRegistry
-                                    .getInstance()
-                                    .lookupService(LogUnit.LogMgmtService.class);
+                            ILogMgmtService logMgmt = (ILogMgmtService) ServiceRegistry.getInstance().lookupService(LogUnit.LogMgmtService.class);
                             Map<Level, Boolean> levelMap = new HashMap<>();
                             EnumSet.allOf(Level.class).forEach((level) -> {
                                 levelMap.put(level, level.ordinal() >= minLevel.ordinal());
                             });
                             logMgmt.changeLevels(levelMap);
-                            prefs.put(propName, ((Level) model.getValue(propName)).name());
+                            PREFERENCES.put(propName, ((Level) model.getValue(propName)).name());
                             break;
                     }
-                });
-                
+                });          
             }
         });
         
         // Commands
         addCommand(new DiskUsageReport());
     }
+    
+    public final Path getWorkDir() {
+        return (Path) model.getValue(PROP_WORK_DIR);
+    }
+    
+    public final Level getLogLevel() {
+        return (Level) model.getValue(PROP_LOG_LEVEL);
+    }
+    
+    public final Locale getGuiLang() {
+        return (Locale) model.getValue(PROP_GUI_LANG);
+    }
+    
+//    public Boolean getUseTray() {
+//        return (Boolean) model.getValue(PROP_USE_TRAY);
+//    }
+    
+    public final Common setWorkDir(Path value) {
+        model.setValue(PROP_WORK_DIR, value);
+        return this;
+    }
+    
+    public final Common setLogLevel(Level value) {
+        model.setValue(PROP_LOG_LEVEL, value);
+        return this;
+    }
+    
+    public final Common setGuiLang(Locale value) {
+        model.setValue(PROP_GUI_LANG, value);
+        return this;
+    }
+    
+//    public Common setUseTray(Boolean value) {
+//        model.setValue(PROP_USE_TRAY, value);
+//        return this;
+//    }
 
     @Override
     public void insert(INode child) {
         super.insert(child);
-        enableChildNode(child, model.getValue("workDir") != null);
+        setChildMode(child, getWorkDir() != null);
     }
     
     @Override
@@ -102,7 +136,7 @@ public class Common extends Catalog {
         return null;
     }
     
-    private void enableChildNode(INode node, boolean enabled) {
+    private void setChildMode(INode node, boolean enabled) {
         node.setMode(enabled ? MODE_ENABLED + MODE_SELECTABLE : MODE_NONE);
     }
     
