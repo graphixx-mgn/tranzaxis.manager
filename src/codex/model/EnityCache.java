d181a758baca6e9a6e213c98e17c605d1050d90c
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-10-31 12:14:49 +0500

    Переработан механизм кеширования сощностей и поиска.

--- src/codex/model/EnityCache.java
+++ src/codex/model/EnityCache.java
@@ -1,13 +1,18 @@
 package codex.model;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 class EnityCache {
     
-    private final Map<Class<?>, List<Entity>> store = new HashMap<>();
+    private final Map<Class<?>, Set<Entity>>  registry  = new ConcurrentHashMap<>();
     private final static EnityCache INSTANCE = new EnityCache();
     
     private EnityCache() {}
@@ -16,29 +21,58 @@ class EnityCache {
         return INSTANCE;
     }
     
-    public synchronized Entity findEntity(Class entityClass, Integer ownerId, String PID)  {
-        if (store.containsKey(entityClass) && PID != null) {
-            return store.get(entityClass).stream().filter((entity) -> {
-                Integer entityOwnerId = entity.model.getOwner() == null ? null : entity.model.getOwner().model.getID();
+    List<Entity> list() {
+        return new HashMap<>(registry).values().stream().flatMap(entity -> entity.stream()).collect(Collectors.toList());
+    }
+    
+    Entity find(Class entityClass, Integer ownerId, String PID)  {
+        if (registry.containsKey(entityClass)) {
+            Optional<Entity> found = registry.get(entityClass).stream().filter((entity) -> {
+                Integer entityOwnerId = entity.getOwner() == null ? null : entity.getOwner().getID();
                 return 
-                        PID.equals(entity.model.getPID()) && (
+                        entity.model.getPID(false).equals(PID) && (
                             (entityOwnerId == null && ownerId == null) || (ownerId == entityOwnerId)
                         );
-            }).findFirst().orElse(null);
+            }).findFirst();
+            if (found.isPresent()) {
+                Entity entity = found.get();
+//                Logger.getLogger().debug(
+//                        "Provided cached entity ({0}): {1} / Owner: {2}", 
+//                        String.format("%10d", entity.hashCode()), 
+//                        entity.model.getQualifiedName(),
+//                        entity.getOwner() == null ? "<NULL>" : entity.getOwner().model.getQualifiedName()
+//                );
+                return entity;
+            }
         }
         return null;
     }
 
-    public synchronized void addEntity(Entity entity) {
-        if (!store.containsKey(entity.getClass())) {
-            store.put(entity.getClass(), new LinkedList<>());
+    void cache(Entity entity) {
+        if (!registry.containsKey(entity.getClass())) {
+            registry.put(entity.getClass(), Collections.synchronizedSet(new HashSet<>()));
         }
-        store.get(entity.getClass()).add(entity);
+        if (registry.get(entity.getClass()).add(entity)) {
+//            Logger.getLogger().debug(
+//                    "Registered new entity  ({0}) in cache: {1} / Owner: {2}", 
+//                    String.format("%10d", entity.hashCode()), 
+//                    entity.model.getQualifiedName(),
+//                    entity.getOwner() == null ? "<NULL>" : entity.getOwner().model.getQualifiedName()
+//            );
+        }
     }
     
-    public synchronized void removeEntity(Entity entity) {
-        if (store.containsKey(entity.getClass())) {
-            store.get(entity.getClass()).remove(entity);
+    void remove(Entity entity) {
+        if (registry.containsKey(entity.getClass())) {
+            if (registry.get(entity.getClass()).remove(entity)) {
+//                Logger.getLogger().debug(
+//                        "Deleted entity ({0}) from cache: {1} / Owner: {2}", 
+//                        String.format("%10d", entity.hashCode()), 
+//                        entity.model.getQualifiedName(),
+//                        entity.getOwner() == null ? "<NULL>" : entity.getOwner().model.getQualifiedName()
+//                );
+            }
         }
     }
+
 } 
