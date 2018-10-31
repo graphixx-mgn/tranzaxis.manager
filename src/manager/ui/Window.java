abb8a47cdb9e921d34721c6c0e023c4dec2f9851
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-10-31 13:25:01 +0500

    Удаление инициализации сервиса нотификации.

--- src/manager/ui/Window.java
+++ src/manager/ui/Window.java
@@ -1,52 +1,31 @@
 package manager.ui;
 
-import codex.log.Logger;
-import codex.notification.NotificationService;
-import codex.service.ServiceRegistry;
 import codex.unit.AbstractUnit;
 import codex.utils.ImageUtils;
-import java.awt.AWTException;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.SystemTray;
-import java.awt.TrayIcon;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowStateListener;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.prefs.Preferences;
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingUtilities;
 import javax.swing.border.MatteBorder;
-import manager.Manager;
 
-public final class Window extends JFrame implements WindowStateListener {
+public final class Window extends JFrame {
     
     private final static ImageIcon LAUNCH  = ImageUtils.resize(ImageUtils.getByPath("/images/launch.png"), 20, 20);
     private final static ImageIcon VIEWER  = ImageUtils.resize(ImageUtils.getByPath("/images/viewer.png"), 20, 20);
-    //private final static ImageIcon SERVICE = ImageUtils.resize(ImageUtils.getByPath("/images/services.png"), 20, 20);
+//    private final static ImageIcon SERVICE = ImageUtils.resize(ImageUtils.getByPath("/images/services.png"), 20, 20);
     
-    private JTabbedPane tabbedPanel  = new JTabbedPane(JTabbedPane.LEFT);
+    private final JTabbedPane tabbedPanel  = new JTabbedPane(JTabbedPane.LEFT);
     public final JPanel upgradePanel = new JPanel();
     public final JPanel taskmgrPanel = new JPanel();
     public final JPanel loggingPanel = new JPanel();
     public final JPanel explorePanel = new JPanel();
     public final JPanel launchPanel  = new JPanel();
     //public final JPanel servicePanel = new JPanel();
-    
-    private final TrayIcon  trayIcon;
-    private int             prevWindowState;
-    Map<String, Boolean>    prevVisibleState = new LinkedHashMap<>();
     
     public Window(String title, ImageIcon icon) {
         super(title);
@@ -92,41 +71,6 @@ public final class Window extends JFrame implements WindowStateListener {
                     .addComponent(loggingPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                 )
         );
-        if (SystemTray.isSupported()) {
-            trayIcon = new TrayIcon(getIconImage(), "TranzAxis Manager");
-            trayIcon.setImageAutoSize(true);
-            trayIcon.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    setVisible(true);
-                    setExtendedState(prevWindowState);
-                }
-            });
-            NotificationService notifier = new NotificationService(trayIcon);
-            notifier.setCondition(() -> {
-                return getState() == JFrame.ICONIFIED || getState() == 7 || !isActive();
-            });
-            ServiceRegistry.getInstance().registerService(notifier);
-            try {
-                SystemTray.getSystemTray().add(trayIcon);
-            } catch (AWTException e) {
-                Logger.getLogger().warn("Unable to minimize window to tray: {0}", e.getMessage());
-            }
-        } else {
-            trayIcon = null;
-        }
-        addWindowStateListener(this);
-    }
-    
-    public void restoreState() {
-        if (getState() == JFrame.ICONIFIED || getState() == 7 || !isActive()) {
-            setVisible(true);
-            setExtendedState(prevWindowState);
-            setAlwaysOnTop(true);
-            toFront();
-            requestFocus();
-            setAlwaysOnTop(false);
-        }
     }
     
     @Override
@@ -142,33 +86,5 @@ public final class Window extends JFrame implements WindowStateListener {
         container.add(unit.getViewport(), BorderLayout.CENTER);
         unit.getViewport().setPreferredSize(container.getSize());
         unit.viewportBound();
-    }
-
-    @Override
-    public void windowStateChanged(WindowEvent event) {
-        boolean hideToTray = Preferences.userRoot().node(Manager.class.getSimpleName()).getBoolean("useTray", false);
-        
-        if (event.getNewState() == ICONIFIED || event.getNewState() == 7) {
-            prevWindowState = event.getOldState();
-            prevVisibleState.clear();
-            Arrays.asList(Frame.getFrames()).forEach((frame) -> {
-                if (frame.isDisplayable() && frame != event.getWindow()) {
-                    prevVisibleState.put(frame.getName(), frame.isVisible());
-                    frame.setVisible(false);
-                }
-            });
-            if (hideToTray) {
-                setVisible(false);
-            }
-        }
-        if (event.getNewState() == MAXIMIZED_BOTH || event.getNewState() == NORMAL) {
-            Arrays.asList(Frame.getFrames()).forEach((frame) -> {
-                if (prevVisibleState.containsKey(frame.getName())) {
-                    SwingUtilities.invokeLater(() -> {
-                        frame.setVisible(prevVisibleState.get(frame.getName()));
-                    });
-                }
-            });
-        }
     }
 }
