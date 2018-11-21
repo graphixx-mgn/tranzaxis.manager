099dacb7d79e3c6dcc7bcc725f4deacadaeb13b6
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-11-21 11:06:43 +0500

    Добавлены методы startService и isStarted.

--- src/codex/service/IService.java
+++ src/codex/service/IService.java
@@ -9,7 +9,19 @@ public interface IService {
      * Возвращает имя сервиса.
      */
     default String getTitle() {
-        return "IService interface";
+        return "IService interface instance ["+getClass().getCanonicalName()+"]";
+    };
+    
+    /**
+     * Запуск сервиса - регистрация в модуле управления сервисами.
+     */
+    default void startService() {};
+    
+    /**
+     * Возвращает признак регистрации сервиса в модуле управления сервисами.
+     */
+    default boolean isStarted() {
+        return true;
     };
     
 }
