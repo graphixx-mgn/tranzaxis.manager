44c7bf7171b31aeb33040f6b7926ebfba8669586
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-11-22 18:09:20 +0500

    Добавлен метод getOption. Документирование.

--- src/codex/service/AbstractService.java
+++ src/codex/service/AbstractService.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.Stack;
 
 
@@ -20,6 +21,9 @@ public abstract class AbstractService<T extends CommonServiceOptions> implements
     private final static Map<Class, Boolean> STARTED_SERVICES = new HashMap<>();
     private T serviceConfig;
     
+    /**
+     * Возвращает признак возможности остановить сервис.
+     */
     public boolean isStoppable() {
         return true;
     }
@@ -41,12 +45,27 @@ public abstract class AbstractService<T extends CommonServiceOptions> implements
         return Boolean.TRUE.equals(STARTED_SERVICES.get(getClass()));
     };
     
+    /**
+     * Получение сущности с настройками сервиса.
+     */
     public final T getConfig() {
         if (serviceConfig == null) {
             serviceConfig = (T) Entity.newInstance(getConfigClass(), null, getTitle());
         }
         return serviceConfig;
     };
+    
+    /**
+     * Получение настроек сервиса из META-INF/options/{Имя класса}.
+     * @param key Значение ключа, по которому выбирается строка-значение.
+     */
+    public final String getOption(String key) {
+        if (ClassLoader.getSystemClassLoader().getResource("META-INF/options/"+getClass().getSimpleName()+".properties") != null) {
+            return ResourceBundle.getBundle("META-INF/options/"+getClass().getSimpleName()).getString(key);
+        } else {
+            return null;
+        }
+    }
     
     // https://habr.com/post/66593/
     private Class<? extends CommonServiceOptions> getConfigClass() {
