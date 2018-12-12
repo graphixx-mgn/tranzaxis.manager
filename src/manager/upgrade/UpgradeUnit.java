b8b43e8c61e48f922bfb03f81801f1d48898b0bf
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2018-12-12 17:42:58 +0500

    Модуль обновления приложения.

--- src/manager/upgrade/UpgradeUnit.java
+++ src/manager/upgrade/UpgradeUnit.java
@@ -1,78 +1,86 @@
 package manager.upgrade;
 
+import codex.component.dialog.Dialog;
 import codex.instance.IInstanceListener;
 import codex.instance.Instance;
 import codex.instance.InstanceCommunicationService;
 import codex.log.Logger;
-import codex.notification.NotificationService;
-import codex.notification.NotifyCondition;
 import codex.service.ServiceRegistry;
 import codex.unit.AbstractUnit;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
-import java.awt.Font;
 import java.awt.Insets;
-import java.awt.TrayIcon;
+import java.net.InetSocketAddress;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.text.MessageFormat;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 import manager.xml.Version;
+import manager.xml.VersionsDocument;
 
 public final class UpgradeUnit extends AbstractUnit implements IInstanceListener {
     
     private final static ImageIcon ICON = ImageUtils.resize(ImageUtils.getByPath("/images/upgrade.png"), 17, 17);
     private final static InstanceCommunicationService ICS = (InstanceCommunicationService) ServiceRegistry.getInstance().lookupService(InstanceCommunicationService.class);
-    private final static NotificationService          NSS = (NotificationService) ServiceRegistry.getInstance().lookupService(NotificationService.class);
-    final static String  NS_SOURCE = "Upgrade/New version";
     
-    private Version currentVersion;
+    private final Version currentVersion;
+    private final AtomicBoolean skipUpgrade = new AtomicBoolean(false);
     
     public UpgradeUnit() {
         Logger.getLogger().debug("Initialize unit: Upgrade Manager");
         ICS.addInstanceListener(this);
-        try {
-            currentVersion = ((IUpgradeService) ICS.getService(UpgradeService.class)).getCurrentVersion();
-        } catch (NotBoundException e) {}
-        NSS.registerSource(NS_SOURCE, NotifyCondition.ALWAYS);
+        currentVersion = UpgradeService.getVersion();
     }
 
     @Override
     public JComponent createViewport() {
-        JButton button = new JButton(MessageFormat.format(Language.get("current"), currentVersion.getNumber()));
-        button.setIcon(ICON);
-        button.setFocusPainted(false);
-        button.setOpaque(false);
-        button.setContentAreaFilled(false);
-        button.setRolloverEnabled(true);
-        button.setMargin(new Insets(0, 5, 0, 5));
-        
-        button.addActionListener((e) -> {
-            
-        });
-        return button;
+        return new JLabel(
+                MessageFormat.format(Language.get("current"), currentVersion.getNumber()), 
+                ICON, SwingConstants.CENTER
+        ) {{
+            setBorder(new EmptyBorder(new Insets(2, 10, 2, 10)));
+        }};
     }
 
     @Override
     public void instanceLinked(Instance instance) {
-        try {
-            IUpgradeService remoteUpService = (IUpgradeService) instance.getService(UpgradeService.class);
-            JButton button = (JButton) getViewport();
-            Version availVersion = remoteUpService.getCurrentVersion();
-            button.setText(MessageFormat.format(Language.get("available"), availVersion.getNumber()));
-            button.setFont(button.getFont().deriveFont(Font.BOLD));
-            
-            NSS.showMessage(
-                    NS_SOURCE, 
-                    Language.get("notify@title"), 
-                    MessageFormat.format(Language.get("notify@message"), availVersion.getNumber()),
-                    TrayIcon.MessageType.INFO
-            );
-        } catch (NotBoundException | RemoteException e) {
-            e.printStackTrace();
-        }
+        new Thread(() -> {
+            synchronized (skipUpgrade) {
+                if (skipUpgrade.get()) return;
+                try {
+                    IUpgradeService remoteUpService = (IUpgradeService) instance.getService(UpgradeService.class);
+                    Logger.getLogger().debug("Check remote instance {0} for upgrade", instance);
+                    Version availVersion = remoteUpService.getCurrentVersion();
+                    if (UpgradeService.VER_COMPARATOR.compare(currentVersion, availVersion) < 0) {
+                        Logger.getLogger().debug("Available upgrade {0} => {1}", currentVersion.getNumber(), availVersion.getNumber());
+                        VersionsDocument diff = remoteUpService.getDiffVersions(currentVersion, availVersion);
+                        new UpgradeDialog(instance, diff, (event) -> {
+                            skipUpgrade.set(event.getID() != Dialog.OK);
+                            if (event.getID() == Dialog.OK) {
+                                upgrade(instance);
+                            }
+                        }).setVisible(true);
+                    } else {
+                        Logger.getLogger().debug("Upgrade is not available. Remote instance version: {0}", availVersion.getNumber());
+                    }
+                } catch (NotBoundException | RemoteException e) {
+                    // Do nothing
+                }
+            }
+        }).start();
+    }
+    
+    private void upgrade(Instance instance) {
+        InetSocketAddress rmiAddress = instance.getRemoteAddress();
+        new Updater(
+                rmiAddress.getAddress().getHostAddress(),
+                rmiAddress.getPort()
+        ).start();
     }
     
 }
