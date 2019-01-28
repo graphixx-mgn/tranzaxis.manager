1e8a3b382a36b488cf40434d50d0cdd7c784e48c
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2019-01-28 16:24:00 +0500

    Заглушка неиспользуемых методов интерфеса Radix IProgressHandle.

---
+++ core/src/main/java/manager/commands/offshoot/build/IProgressHandle.java
@@ -0,0 +1,23 @@
+package manager.commands.offshoot.build;
+
+
+public interface IProgressHandle extends org.radixware.kernel.common.builder.api.IProgressHandle {
+
+    @Override
+    default void switchToIndeterminate() {}
+
+    @Override
+    default void switchToDeterminate(int i) {}
+
+    @Override
+    default void start() {}
+
+    @Override
+    default void start(int i) {}
+
+    @Override
+    default void progress(String string, int i) {
+        progress(0);
+    }
+
+}
