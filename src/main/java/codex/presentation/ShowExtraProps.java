cdb2b730756f197adbd227cadaad74e60ac73001
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2019-05-15 10:50:04 +0500

    Наличие некорректных значений свойств отображается в иконке.

--- src/main/java/codex/presentation/ShowExtraProps.java
+++ src/main/java/codex/presentation/ShowExtraProps.java
@@ -1,5 +1,6 @@
 package codex.presentation;
 
+import codex.command.CommandStatus;
 import codex.command.EntityCommand;
 import codex.component.button.DialogButton;
 import codex.component.dialog.Dialog;
@@ -18,10 +19,13 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 class ShowExtraProps extends EntityCommand<Entity> {
+
+    private final static ImageIcon IMAGE_WARN = ImageUtils.resize(ImageUtils.getByPath("/images/warn.png"), 20, 20);
 
     ShowExtraProps() {
         super(
@@ -30,6 +34,17 @@ class ShowExtraProps extends EntityCommand<Entity> {
                 Language.get(EditorPresentation.class, "command@extra"),
                 null
         );
+        Function<List<Entity>, CommandStatus> defaultActivator = activator;
+        activator = entities -> {
+            Entity context = entities.get(0);
+            boolean hasInvalidProp = context.model.getProperties(Access.Any).stream()
+                    .filter(context.model::isPropertyExtra)
+                    .anyMatch(propName -> !context.model.getProperty(propName).isValid());
+            return new CommandStatus(
+                    defaultActivator.apply(entities).isActive(),
+                    hasInvalidProp ? ImageUtils.combine(getIcon(), IMAGE_WARN, SwingConstants.SOUTH_EAST) : getIcon()
+            );
+        };
     }
 
     @Override
