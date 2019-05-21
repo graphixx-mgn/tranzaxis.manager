cf189e8c0521be8deaa829b638a706ef125d825d
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2019-05-21 16:55:09 +0500

    Рефакторинг.

--- src/main/java/codex/service/CommonServiceOptions.java
+++ src/main/java/codex/service/CommonServiceOptions.java
@@ -1,14 +1,11 @@
 package codex.service;
 
 import codex.command.EntityCommand;
-import codex.mask.IMask;
 import codex.model.Access;
 import codex.model.Catalog;
 import codex.model.Entity;
-import codex.type.Bool;
-import codex.type.EntityRef;
-import codex.type.IComplexType;
-import codex.type.Iconified;
+import codex.model.EntityModel;
+import codex.type.*;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
 import java.util.Map;
@@ -19,8 +16,8 @@ import javax.swing.ImageIcon;
  */
 public class CommonServiceOptions extends Catalog {
     
-    public  final static String PROP_CLASS   = "class";
-    public  final static String PROP_STARTED = "started";
+    private final static String PROP_CLASS   = "class";
+    private final static String PROP_STARTED = "started";
     
     private final static ImageIcon ICON_STARTED = ImageUtils.getByPath("/images/start.png"); 
     private final static ImageIcon ICON_STOPPED = ImageUtils.getByPath("/images/stop.png"); 
@@ -29,10 +26,21 @@ public class CommonServiceOptions extends Catalog {
     
     public CommonServiceOptions(EntityRef owner, String title) {
         super(owner, ImageUtils.getByPath("/images/start.png"), title, null);
-        model.addDynamicProp(PROP_CLASS, new ServiceLabel(), Access.Edit, () -> {
-            return new ServiceStatus();
+        model.addDynamicProp(PROP_CLASS, new AnyType(), Access.Edit, () -> new Iconified() {
+            @Override
+            public ImageIcon getIcon() {
+                return CommonServiceOptions.this.isStarted() ? ICON_STARTED : ICON_STOPPED;
+            }
+
+            @Override
+            public String toString() {
+                return CommonServiceOptions.this.getService().getTitle();
+            }
         });
         model.addUserProp(PROP_STARTED, new Bool(true), false, Access.Any);
+
+        // Property settings
+        setPropertyRestriction(EntityModel.THIS, Access.Any);
         
         // Commands
         addCommand(new StartService());
@@ -60,58 +68,20 @@ public class CommonServiceOptions extends Catalog {
         model.setValue(PROP_STARTED, started);
         try {
             model.commit(false);
-        } catch (Exception e) {}
-    }
-    
-    class ServiceLabel implements IComplexType<ServiceStatus, IMask<ServiceStatus>> {
-
-        private ServiceStatus value;
-        
-        @Override
-        public ServiceStatus getValue() {
-            return value;
+        } catch (Exception e) {
+            //
         }
-
-        @Override
-        public void setValue(ServiceStatus value) {
-            this.value = value;
-        }
-
-        @Override
-        public void valueOf(String value) {}
-
-        @Override
-        public String getQualifiedValue(ServiceStatus val) {
-            return null;
-        }
-    
-    }
-    
-    class ServiceStatus implements Iconified {
-
-        @Override
-        public ImageIcon getIcon() {
-            return CommonServiceOptions.this.isStarted() ? ICON_STARTED : ICON_STOPPED;
-        }
-        
-        @Override
-        public String toString() {
-            return CommonServiceOptions.this.getService().getTitle();
-        }
-    
     }
     
     private class StartService extends EntityCommand<CommonServiceOptions> {
 
-        public StartService() {
+        StartService() {
             super(
                     "start", 
                     Language.get(CommonServiceOptions.class, "start@title"),
                     ImageUtils.resize(ICON_STARTED, 28, 28), 
                     Language.get(CommonServiceOptions.class, "start@title"),
-                    (control) -> {
-                        return control.getService().isStoppable() && !control.isStarted();
-                    }
+                    (control) -> control.getService().isStoppable() && !control.isStarted()
             );
         }
 
@@ -124,15 +94,13 @@ public class CommonServiceOptions extends Catalog {
     
     private class StopService extends EntityCommand<CommonServiceOptions> {
 
-        public StopService() {
+        StopService() {
             super(
                     "stop", 
                     Language.get(CommonServiceOptions.class, "stop@title"),
                     ImageUtils.resize(ICON_STOPPED, 28, 28), 
                     Language.get(CommonServiceOptions.class, "stop@title"),
-                    (control) -> {
-                        return control.getService().isStoppable() && control.isStarted();
-                    }
+                    (control) -> control.getService().isStoppable() && control.isStarted()
             );
         }
 
