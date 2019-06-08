8188b03e5e57b39025eae1136f8ca261657ac55e
igredyaev <i.gredyaev@compassplus.com>
2019-06-09 01:06:10 +0500

    Реализация сущности настроек сетевого сервиса ICS.

--- src/main/java/codex/service/AbstractRemoteService.java
+++ src/main/java/codex/service/AbstractRemoteService.java
@@ -2,12 +2,15 @@ package codex.service;
 
 import codex.instance.InstanceCommunicationService;
 import codex.instance.MultihomeRMIClientSocketFactory;
+import codex.model.Entity;
 import java.net.InetAddress;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.stream.Collectors;
 
-public abstract class AbstractRemoteService extends UnicastRemoteObject {
+public abstract class AbstractRemoteService<T extends RemoteServiceOptions> extends UnicastRemoteObject implements IRemoteService {
+
+    private T serviceConfig;
 
     protected AbstractRemoteService() throws RemoteException {
         super(0,
@@ -18,6 +21,21 @@ public abstract class AbstractRemoteService extends UnicastRemoteObject {
                 ),
                 null
         );
+    }
+
+    public final T getConfig() {
+        if (serviceConfig == null) {
+            try {
+                serviceConfig = Entity.newInstance(ServiceCatalog.getServiceConfigClass(
+                        RemoteServiceOptions.class,
+                        AbstractRemoteService.class,
+                        getClass()
+                ), null, getTitle());
+            } catch (RemoteException e) {
+                // Must not appear
+            }
+        }
+        return serviceConfig;
     }
 
 }
