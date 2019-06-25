ca053b94e3968ed73475b9c875a379c65c49faa3
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2019-06-25 14:56:25 +0500

    Маска для фильтрации сущностей.

---
+++ src/main/java/codex/mask/EntityFilter.java
@@ -0,0 +1,65 @@
+package codex.mask;
+
+import codex.model.Entity;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EntityFilter<E extends Entity> implements IRefMask<E> {
+
+    private final Predicate<E>       entityFilter;
+    private final Function<E, Match> entityMatcher;
+
+    /**
+     * Конструктор маски.
+     * @param entityFilter Пользовательский фильтр допустимых значений.
+     */
+    public EntityFilter(Predicate<E> entityFilter) {
+        this(entityFilter, null);
+    }
+
+    /**
+     * Конструктор маски.
+     * @param entityFilter Пользовательский фильтр допустимых значений.
+     * @param entityMatcher Условие соответствия сущности. Используется для
+     * подсветки цветом в выпадающем списке {@link codex.editor.EntityRefEditor}.
+     */
+    public EntityFilter(Predicate<E> entityFilter, Function<E, Match> entityMatcher) {
+        this.entityFilter = entityFilter != null ? entityFilter : (entity) -> true;
+        this.entityMatcher = entityMatcher != null ? entityMatcher : (entity) -> Match.Unknown;
+    }
+
+    /**
+     * Возвращает условие проверки сущностей.
+     */
+    public final Function<E, Match> getEntityMatcher() {
+        return entityMatcher;
+    }
+
+    @Override
+    public boolean verify(E value) {
+        return entityFilter.test(value);
+    }
+
+
+    /**
+     * Результат проверки сущностей на предмет соответствия условию.
+     */
+    public enum Match {
+        /**
+         * Точное совпадение.
+         */
+        Exact,
+        /**
+         * Частичное совпадение.
+         */
+        About,
+        /**
+         * Не совпадает.
+         */
+        None,
+        /**
+         * Проверка не может быть выполнена.
+         */
+        Unknown
+    }
+}
