686108f389af2cae8d9caff5d7dedeb00dec2aba
igredyaev <igredyaev@igredyaev.cp.compassplus.ru>
2019-07-28 15:03:42 +0500

    Рефакторинг.

--- src/main/java/codex/service/AbstractRemoteService.java
+++ src/main/java/codex/service/AbstractRemoteService.java
@@ -27,7 +27,6 @@ public abstract class AbstractRemoteService<P extends RemoteServiceOptions, C ex
         if (serviceConfig == null) {
             try {
                 serviceConfig = Entity.newInstance(ServiceCatalog.getServiceConfigClass(
-                        RemoteServiceOptions.class,
                         AbstractRemoteService.class,
                         getClass()
                 ), null, getTitle());
