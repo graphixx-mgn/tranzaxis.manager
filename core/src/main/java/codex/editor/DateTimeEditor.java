41e2e418d58c6292d5016cb4cb01388c5d848c1e
igredyaev <i.gredyaev@compassplus.com>
2019-11-13 00:33:58 +0500

    Поддержка маски даты в редакторе DateTime.

--- core/src/main/java/codex/editor/DateTimeEditor.java
+++ core/src/main/java/codex/editor/DateTimeEditor.java
@@ -14,17 +14,12 @@ import com.github.lgooddatepicker.components.DatePickerSettings;
 import com.privatejgoodies.forms.factories.CC;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 import javax.swing.text.DateFormatter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.MessageFormat;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -34,19 +29,14 @@ import java.util.Date;
 
 public class DateTimeEditor extends AbstractEditor<DateTime, Date> {
 
+    private static final long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
+
     private static final ImageIcon PICK = ImageUtils.getByPath("/images/calendar.png");
     private static final ImageIcon CURR = ImageUtils.getByPath("/images/now.png");
     private static final ImageIcon NEXT_MONTH = ImageUtils.resize(ImageUtils.getByPath("/images/next.png"), 0.7f);
     private static final ImageIcon PREV_MONTH = ImageUtils.resize(ImageUtils.getByPath("/images/prev.png"), 0.7f);
     private static final ImageIcon NEXT_YEAR  = ImageUtils.resize(ImageUtils.getByPath("/images/end.png"), 0.7f);
     private static final ImageIcon PREV_YEAR  = ImageUtils.resize(ImageUtils.getByPath("/images/begin.png"), 0.7f);
-
-    private static final String DATETIME_FORMAT = MessageFormat.format(
-            "{0} {1}",
-            ((SimpleDateFormat) DateFormat.getDateInstance(DateFormat.LONG, Language.getLocale())).toPattern(),
-            "HH:mm:ss,SSS"
-    );
-    private static final SimpleDateFormat FORMATTER = new SimpleDateFormat(DATETIME_FORMAT, Language.getLocale());
 
     private JTextField textField;
     private final JLabel signDelete;
@@ -111,7 +101,7 @@ public class DateTimeEditor extends AbstractEditor<DateTime, Date> {
 
     @Override
     public void setValue(Date date) {
-        textField.setText(date == null ? "" : FORMATTER.format(date));
+        textField.setText(date == null ? "" : propHolder.getOwnPropValue().getMask().getFormat().format(date));
         if (signDelete!= null) {
             signDelete.setVisible(!propHolder.isEmpty() && isEditable() && textField.isFocusOwner());
         }
@@ -135,6 +125,13 @@ public class DateTimeEditor extends AbstractEditor<DateTime, Date> {
         signDelete.setVisible(false);
     }
 
+    private boolean showDate() {
+        switch (propHolder.getOwnPropValue().getMask().getFormat()) {
+            case Full: return true;
+            default:   return false;
+        }
+    }
+
     private class CurrentTime extends EditorCommand<DateTime, Date> {
 
         private CurrentTime() {
@@ -143,7 +140,11 @@ public class DateTimeEditor extends AbstractEditor<DateTime, Date> {
 
         @Override
         public void execute(PropertyHolder<DateTime, Date> context) {
-            context.setValue(new Date());
+            Date newValue = new Date();
+            if (!showDate()) {
+                newValue = new Date(newValue.getTime() % MILLIS_PER_DAY);
+            }
+            context.setValue(newValue);
         }
     }
 
@@ -191,24 +192,30 @@ public class DateTimeEditor extends AbstractEditor<DateTime, Date> {
         private final DialogButton apply = Dialog.Default.BTN_OK.newInstance("");
         private final DialogButton now   = Dialog.Default.BTN_OK.newInstance(CURR, "");
         private final Component invoker  = DateTimeEditor.this.textField.getParent();
-        private final JPopupMenu popup   = new JPopupMenu(){{
+        private final JPopupMenu popup   = new JPopupMenu() {{
             add(new JPanel(new BorderLayout()) {{
                 JSpinner spinner = new JSpinner(timeSpinner);
-                spinner.setFont(IEditor.FONT_VALUE.deriveFont(IEditor.FONT_VALUE.getSize()*0.9f));
+                spinner.setFont(IEditor.FONT_VALUE.deriveFont(IEditor.FONT_VALUE.getSize()));
 
-                JSpinner.DateEditor editor = new JSpinner.DateEditor(spinner, "HH:mm:ss,SSS");
+                JSpinner.DateEditor editor = new JSpinner.DateEditor(
+                        spinner,
+                        propHolder.getOwnPropValue().getMask().getFormat().getTimeFormat().toPattern()
+                );
                 spinner.setEditor(editor);
 
                 DateFormatter formatter = (DateFormatter)editor.getTextField().getFormatter();
                 formatter.setAllowsInvalid(false);
                 formatter.setOverwriteMode(true);
 
-                add(new JPanel(new BorderLayout()){{
-                    setBorder(new EmptyBorder(0,5,0,5));
+                add(calendarPanel, BorderLayout.NORTH);
+                add(new JPanel(new BorderLayout()) {{
+                    setBorder(new EmptyBorder(0, 5, 0, 5));
+                    int defWidth = calendarPanel.getComponent(0).getPreferredSize().width;
+                    if (!showDate()) {
+                        add(Box.createRigidArea(new Dimension(defWidth, 10)), BorderLayout.NORTH);
+                    }
                     add(spinner, BorderLayout.CENTER);
                 }}, BorderLayout.CENTER);
-
-                add(calendarPanel, BorderLayout.NORTH);
                 add(new JPanel(){{
                     add(now);
                     add(apply);
@@ -222,22 +229,26 @@ public class DateTimeEditor extends AbstractEditor<DateTime, Date> {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     Date currDate = new Date();
-                    calendarPanel.setSelectedDate(convertDate(new Date()));
+                    calendarPanel.setSelectedDate(convertDate(currDate));
                     timeSpinner.setValue(currDate);
                 }
             });
             apply.addActionListener(new AbstractAction() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    LocalDate localDate = calendarPanel.getSelectedDate();
-                    if (localDate != null) {
-                        Calendar calendar = Calendar.getInstance();
-                        calendar.setTime(timeSpinner.getDate());
-                        calendar.set(Calendar.YEAR,  localDate.getYear());
-                        calendar.set(Calendar.MONTH, localDate.getMonthValue()-1);
-                        calendar.set(Calendar.DAY_OF_MONTH, localDate.getDayOfMonth());
-                        propHolder.setValue(calendar.getTime());
+                    Date time = new Date(timeSpinner.getDate().getTime() % MILLIS_PER_DAY);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(time);
+
+                    if (showDate()) {
+                        LocalDate localDate = calendarPanel.getSelectedDate();
+                        if (localDate != null) {
+                            calendar.set(Calendar.YEAR,  localDate.getYear());
+                            calendar.set(Calendar.MONTH, localDate.getMonthValue()-1);
+                            calendar.set(Calendar.DAY_OF_MONTH, localDate.getDayOfMonth());
+                        }
                     }
+                    propHolder.setValue(calendar.getTime());
                     popup.setVisible(false);
                 }
             });
@@ -245,13 +256,15 @@ public class DateTimeEditor extends AbstractEditor<DateTime, Date> {
 
         @Override
         public void execute(PropertyHolder<DateTime, Date> context) {
+            Date currDate = new Date();
             timeSpinner.setValue(IComplexType.coalesce(
                     context.getPropValue().getValue(),
-                    new Date()
+                    currDate
             ));
+            calendarPanel.setVisible(showDate());
             calendarPanel.setSelectedDate(convertDate(IComplexType.coalesce(
                     context.getPropValue().getValue(),
-                    new Date()
+                    currDate
             )));
             popup.show(
                     invoker,
