43e9a11ef43d8101e855143370d237b52fd64bc3
igredyaev <i.gredyaev@compassplus.com>
2019-11-20 00:50:12 +0500

    Рефакторинг сервиса CAS после смены логики хранения настроек.

--- core/src/main/java/codex/config/ConfigServiceOptions.java
+++ core/src/main/java/codex/config/ConfigServiceOptions.java
@@ -1,19 +1,21 @@
 package codex.config;
 
 import codex.model.Access;
-import codex.service.LocalServiceOptions;
+import codex.model.EntityDefinition;
+import codex.service.Service;
+import codex.type.EntityRef;
 import codex.type.Str;
-import codex.utils.ImageUtils;
 
-public class ConfigServiceOptions extends LocalServiceOptions<ConfigStoreService> {
+@EntityDefinition(icon="/images/config.png")
+public class ConfigServiceOptions extends Service<ConfigStoreService> {
 
-    private final static String PROP_DB_FILE  = "dbFile";
+    private final static String PROP_DB_FILE = "dbFile";
 
-    public ConfigServiceOptions(ConfigStoreService service) {
-        super(service);
-        setIcon(ImageUtils.getByPath("/images/config.png"));
+    public ConfigServiceOptions(EntityRef owner, String title) {
+        super(owner, title);
+
         model.addDynamicProp(PROP_DB_FILE, new Str(null), Access.Select, () -> {
-            return System.getProperty("user.home")+ConfigServiceOptions.this.getService().getOption("file");
+            return System.getProperty("user.home") + ConfigServiceOptions.this.getService().getOption("file");
         });
     }
 }
