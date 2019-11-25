e98ffbc8aa73db86b6ad6f718ca36a193aba0b42
igredyaev <i.gredyaev@compassplus.com>
2019-11-26 04:15:27 +0500

    Запрет удаления репозитория при наличии ссылок.

--- application/src/main/java/manager/nodes/RepositoryRoot.java
+++ application/src/main/java/manager/nodes/RepositoryRoot.java
@@ -1,17 +1,22 @@
 package manager.nodes;
 
 import codex.model.Catalog;
+import codex.model.Entity;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
 
 public class RepositoryRoot extends Catalog {
+
+    public static <E extends Entity> void deleteInstance(E entity, boolean cascade, boolean confirmation) {
+        Entity.deleteInstance(entity, false, true);
+    }
 
     public RepositoryRoot() {
         super(null, ImageUtils.getByPath("/images/repositories.png"), null, Language.get("desc"));
     }
 
     @Override
-    public Class getChildClass() {
+    public Class<? extends Entity> getChildClass() {
         return Repository.class;
     }
     
