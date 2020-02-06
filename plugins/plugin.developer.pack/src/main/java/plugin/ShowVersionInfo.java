34092c348a063b00b5c0dca0662da1c8d48a7a81
igredyaev <i.gredyaev@compassplus.com>
2020-02-06 14:47:54 +0500

    Изменение алгоритма расчета статуса версии. Рефакторинг.

--- plugins/plugin.developer.pack/src/main/java/plugin/ShowVersionInfo.java
+++ plugins/plugin.developer.pack/src/main/java/plugin/ShowVersionInfo.java
@@ -6,8 +6,10 @@ import codex.model.Entity;
 import codex.model.ParamModel;
 import codex.presentation.EditorPage;
 import codex.presentation.SelectorPresentation;
+import codex.property.PropertyHolder;
 import codex.service.ServiceRegistry;
 import codex.task.AbstractTask;
+import codex.task.CancelException;
 import codex.task.ITaskExecutorService;
 import codex.type.*;
 import codex.utils.ImageUtils;
@@ -29,10 +31,10 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.io.InputStream;
 import java.text.MessageFormat;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.LinkedHashMap;
+import java.util.*;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ShowVersionInfo extends CommandPlugin<Offshoot> {
 
@@ -44,14 +46,22 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
     private static String PROP_BRANCH_CREATE = "branchCreated";
     private static String PROP_RELEASE_INFO  = "releaseInfo";
 
+    private static String PARAM_RELEASE_INFO = "releaseRevisions";
+
     public ShowVersionInfo() {
         super(offshoot -> true);
+        setParameters(
+            new PropertyHolder<>(PARAM_RELEASE_INFO, new Bool(false), false)
+        );
     }
 
     @Override
     public void execute(Offshoot offshoot, Map<String, IComplexType> params) {
         if (!offshoot.getRepository().isRepositoryOnline(true)) return;
-        TES.executeTask(new CollectVersionData(offshoot));
+        TES.executeTask(new CollectVersionData(
+                offshoot,
+                params.get(PARAM_RELEASE_INFO).getValue() == Boolean.TRUE
+        ));
     }
 
     @Override
@@ -61,32 +71,45 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
 
     @Override
     public Kind getKind() {
-        return Kind.Info;
+        return Kind.Admin;
     }
 
     class CollectVersionData extends AbstractTask<ParamModel> {
 
         private final Offshoot offshoot;
-        CollectVersionData(Offshoot offshoot) {
-            super(Language.get("title"));
+        private final boolean  loadReleaseRev;
+
+        CollectVersionData(Offshoot offshoot, boolean loadReleaseRev) {
+            super(MessageFormat.format(Language.get("title"), offshoot.getPID()));
             this.offshoot = offshoot;
+            this.loadReleaseRev = loadReleaseRev;
         }
 
         @Override
         public ParamModel execute() {
             ParamModel paramModel = new ParamModel();
             try {
+                setProgress(0, Language.get(ShowVersionInfo.class, "progress@file"));
                 BranchDocument branch = getBranch(offshoot);
+                setProgress(0, Language.get(ShowVersionInfo.class, "progress@last"));
                 BinarySource lastRelease = getLastRelease(branch);
 
                 if (lastRelease instanceof Release) {
-                    Release release = (Release) lastRelease;
+                    Map<String, ReleaseInfo> releaseInfos = getReleaseInfos(branch);
+                    List<ReleaseStatus> statuses = releaseInfos.values().stream()
+                            .map(releaseInfo -> releaseInfo.status)
+                            .collect(Collectors.toList());
 
-                    paramModel.addProperty(PROP_RELEASE_STAT, new AnyType(), false);
-                    paramModel.setValue(PROP_RELEASE_STAT, getReleaseStatus(release));
+                    paramModel.addProperty(PROP_RELEASE_STAT, new AnyType(
+                            statuses.isEmpty() ? getReleaseStatus((Release) lastRelease) : (
+                                statuses.contains(ReleaseStatus.Expired) ? ReleaseStatus.Expired : (
+                                        statuses.contains(ReleaseStatus.Prod) ? ReleaseStatus.Prod : ReleaseStatus.Test
+                                )
+                            )
+                    ), false);
+                    paramModel.addProperty(PROP_LAST_RELEASE, new AnyType(lastRelease), false);
 
-                    paramModel.addProperty(PROP_LAST_RELEASE, new AnyType(), false);
-                    paramModel.setValue(PROP_LAST_RELEASE, lastRelease);
+                    setProgress(100, Language.get(ShowVersionInfo.class, "progress@rev"));
 
                     paramModel.addProperty(PROP_BRANCH_CREATE, new AnyType(), false);
                     SVNRevision revision = SVN.getMinimalRevision(offshoot.getRemotePath(), offshoot.getRepository().getAuthManager());
@@ -97,11 +120,11 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
                             Offshoot.DATE_FORMAT.format(date)
                     ));
 
-                    paramModel.addProperty(PROP_RELEASE_INFO, new codex.type.Map<>(
-                            new Str(),
-                            new EntityRef<>(ReleaseInfo.class),
-                            getReleaseInfos(branch)
-                    ), false);
+                    paramModel.addProperty(
+                            PROP_RELEASE_INFO,
+                            new codex.type.Map<>(new Str(), new EntityRef<>(ReleaseInfo.class), releaseInfos),
+                            false
+                    );
                 } else {
                     paramModel.addProperty(PROP_LAST_OFFSHOOT, new AnyType(), false);
                     paramModel.setValue(PROP_LAST_OFFSHOOT, lastRelease);
@@ -115,43 +138,45 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
         @Override
         @SuppressWarnings("unchecked")
         public void finished(ParamModel paramModel) {
-            SwingUtilities.invokeLater(() -> new Dialog(
-                    Dialog.findNearestWindow(),
-                    ImageUtils.getByPath(Language.get(ShowVersionInfo.class, "icon")),
-                    Language.get(ShowVersionInfo.class, "title"),
-                    new JPanel(new BorderLayout()) {{
-                        // Common properties
-                        EditorPage page = new EditorPage(paramModel);
-                        add(page, BorderLayout.NORTH);
+            if (getStatus() == codex.task.Status.FINISHED) {
+                SwingUtilities.invokeLater(() -> new Dialog(
+                        Dialog.findNearestWindow(),
+                        ImageUtils.getByPath(Language.get(ShowVersionInfo.class, "icon")),
+                        Language.get(ShowVersionInfo.class, "title"),
+                        new JPanel(new BorderLayout()) {{
+                            // Common properties
+                            EditorPage page = new EditorPage(paramModel);
+                            add(page, BorderLayout.NORTH);
 
-                        // Releases
-                        if (paramModel.hasProperty(PROP_RELEASE_INFO)) {
-                            paramModel.getEditor(PROP_RELEASE_INFO).setVisible(false);
-                            Map<String, ReleaseInfo> releases = ((Map<String, ReleaseInfo>) paramModel.getValue(PROP_RELEASE_INFO));
-                            if (!releases.isEmpty()) {
-                                ReleaseCatalog catalog = new ReleaseCatalog();
-                                releases.forEach((version, releaseInfo) -> catalog.attach(releaseInfo));
+                            // Releases
+                            if (paramModel.hasProperty(PROP_RELEASE_INFO)) {
+                                paramModel.getEditor(PROP_RELEASE_INFO).setVisible(false);
+                                Map<String, ReleaseInfo> releases = ((Map<String, ReleaseInfo>) paramModel.getValue(PROP_RELEASE_INFO));
+                                if (!releases.isEmpty()) {
+                                    ReleaseCatalog catalog = new ReleaseCatalog();
+                                    releases.forEach((version, releaseInfo) -> catalog.attach(releaseInfo));
 
-                                SelectorPresentation releasesView = catalog.getSelectorPresentation();
-                                if (releasesView != null) {
-                                    releasesView.setBorder(new TitledBorder(
-                                            new LineBorder(Color.GRAY, 1),
-                                            Language.get(ShowVersionInfo.class, "releases@title")
-                                    ));
-                                    add(catalog.getSelectorPresentation(), BorderLayout.CENTER);
+                                    SelectorPresentation releasesView = catalog.getSelectorPresentation();
+                                    if (releasesView != null) {
+                                        releasesView.setBorder(new TitledBorder(
+                                                new LineBorder(Color.GRAY, 1),
+                                                Language.get(ShowVersionInfo.class, "releases@title")
+                                        ));
+                                        add(catalog.getSelectorPresentation(), BorderLayout.CENTER);
+                                    }
                                 }
                             }
-                        }
-                    }},
-                    null,
-                    Dialog.Default.BTN_CLOSE.newInstance()
-            ) {
-                @Override
-                public Dimension getPreferredSize() {
-                    Dimension prefSize = super.getPreferredSize();
-                    return new Dimension(650, prefSize.getSize().height);
-                }
-            }.setVisible(true));
+                        }},
+                        null,
+                        Dialog.Default.BTN_CLOSE.newInstance()
+                ) {
+                    @Override
+                    public Dimension getPreferredSize() {
+                        Dimension prefSize = super.getPreferredSize();
+                        return new Dimension(650, prefSize.getSize().height);
+                    }
+                }.setVisible(true));
+            }
         }
 
         private BranchDocument getBranch(Offshoot offshoot) throws Exception {
@@ -167,11 +192,15 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
         private BinarySource getLastRelease(BranchDocument branch) {
             Type.Enum branchType = branch.getBranch().getType();
             BinarySource release;
-            if (branchType == Type.DEV) {
-                release = Entity.newPrototype(Offshoot.class);
-                release.setTitle(branch.getBranch().getLastRelease());
+            if (branch.getBranch().getLastRelease() == null) {
+                return Entity.newInstance(Release.class, offshoot.getRepository().toRef(), branch.getBranch().getBaseRelease());
             } else {
-                release = Entity.newInstance(Release.class, offshoot.getRepository().toRef(), branch.getBranch().getLastRelease());
+                if (branchType == Type.DEV) {
+                    release = Entity.newPrototype(Offshoot.class);
+                    release.setTitle(branch.getBranch().getLastRelease());
+                } else {
+                    release = Entity.newInstance(Release.class, offshoot.getRepository().toRef(), branch.getBranch().getLastRelease());
+                }
             }
             return release;
         }
@@ -183,6 +212,8 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
                 return ReleaseStatus.Expired;
             } else if (status == Status.TEST || status == Status.NEW) {
                 return ReleaseStatus.Test;
+            } else if (status == Status.INVALID) {
+                return ReleaseStatus.Invalid;
             } else {
                 return ReleaseStatus.Prod;
             }
@@ -197,21 +228,46 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
                 SVNURL svnUrl = SVNURL.parseURIEncoded(offshoot.getRepository().getRepoUrl())
                         .appendPath(releaseDir, false);
 
-                SVN.list(svnUrl.toString(), offshoot.getRepository().getAuthManager()).stream()
+                List<SVNDirEntry> entryList = SVN.list(svnUrl.toString(), offshoot.getRepository().getAuthManager()).stream()
                         .filter(svnDirEntry -> svnDirEntry.getName().startsWith(baseRelease))
                         .sorted(Comparator.comparing(
                                 SVNDirEntry::getName,
                                 BinarySource.VERSION_SORTER.reversed()
                         ))
-                        .forEach(svnDirEntry -> {
-                            try {
-                                SVNRevision revision = SVN.getMinimalRevision(svnDirEntry.getURL().toString(), offshoot.getRepository().getAuthManager());
-                                Date date = SVN.info(svnDirEntry.getURL().toString(), revision, true, offshoot.getRepository().getAuthManager()).getCommittedDate();
-                                releases.put(svnDirEntry.getName(), new ReleaseInfo(svnDirEntry.getName(), new Revision(revision.getNumber(), date)));
-                            } catch (SVNException ignore) {
-                                //
-                            }
-                        });
+                        .collect(Collectors.toList());
+
+                for (SVNDirEntry svnDirEntry : entryList) {
+                    if (isCancelled()) {
+                        throw new CancelException();
+                    }
+                    final String releaseName = svnDirEntry.getName();
+                    try {
+                        setProgress(
+                                (entryList.indexOf(svnDirEntry) + 1) * 100 / entryList.size(),
+                                MessageFormat.format(
+                                        Language.get(ShowVersionInfo.class, "progress@release"),
+                                        releaseName
+                                )
+                        );
+                        ReleaseStatus status = getReleaseStatus(Entity.newInstance(Release.class, offshoot.getRepository().toRef(), svnDirEntry.getName()));
+
+                        if (loadReleaseRev) {
+                            SVNRevision revision = SVN.getMinimalRevision(svnDirEntry.getURL().toString(), offshoot.getRepository().getAuthManager());
+                            Date date = SVN.info(svnDirEntry.getURL().toString(), revision, true, offshoot.getRepository().getAuthManager()).getCommittedDate();
+                            releases.put(
+                                    releaseName,
+                                    new ReleaseInfo(releaseName, status, new Revision(revision.getNumber(), date))
+                            );
+                        } else {
+                            releases.put(
+                                    releaseName,
+                                    new ReleaseInfo(releaseName, status, null)
+                            );
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
             return releases;
         }
@@ -237,14 +293,27 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
 
 
     private static class ReleaseInfo extends Catalog {
+
         private final static ImageIcon ICON = ImageUtils.getByPath("/images/release.png");
+        private final static String    PROP_STATUS   = "status";
         private final static String    PROP_REVISION = "revision";
 
-        private ReleaseInfo(String title, Revision revision) {
+        private final ReleaseStatus status;
+
+        private ReleaseInfo(String title, ReleaseStatus status) {
+            this(title, status, null);
+        }
+
+        private ReleaseInfo(String title, ReleaseStatus status, Revision revision) {
             super(null, ICON, title, null);
+            this.status = status;
 
             // Properties
-            model.addDynamicProp(PROP_REVISION, new Str(revision.toString()), null, null);
+            model.addDynamicProp(PROP_STATUS, new AnyType(), null, () -> status);
+
+            if (revision != null) {
+                model.addDynamicProp(PROP_REVISION, new Str(revision.toString()), null, null);
+            }
         }
     }
 
@@ -270,6 +339,7 @@ public class ShowVersionInfo extends CommandPlugin<Offshoot> {
 
 
     enum ReleaseStatus implements Iconified {
+        Invalid,
         Test,
         Prod,
         Expired;
