d2898c7f84e3706045072102c79df64d524bcc8a
igredyaev <i.gredyaev@compassplus.com>
2020-04-08 23:17:27 +0500

    Изменение назначения доменов классов.

--- core/src/main/java/codex/model/ClassCatalog.java
+++ core/src/main/java/codex/model/ClassCatalog.java
@@ -1,19 +1,59 @@
 package codex.model;
 
+import codex.log.Logger;
 import codex.type.EntityRef;
 import org.atteo.classindex.ClassIndex;
 import org.atteo.classindex.IndexSubclasses;
 import javax.swing.*;
 import java.lang.annotation.*;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.text.MessageFormat;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @IndexSubclasses
 @ClassCatalog.Definition
 public abstract class ClassCatalog extends Entity {
+
+    private static final List<Class<? extends ClassCatalog>> catalogs = new LinkedList<>();
+
+    static {
+        synchronized (catalogs) {
+            catalogs.addAll(StreamSupport.stream(ClassIndex.getSubclasses(codex.model.ClassCatalog.class).spliterator(), false)
+                    .filter(aClass -> !aClass.equals(PolyMorph.class))
+                    .collect(Collectors.toList())
+            );
+            SwingUtilities.invokeLater(() -> Logger.getLogger().debug(
+                    "Registered class catalogs:\n{0}",
+                    catalogs.stream()
+                        .collect(Collectors.groupingBy(aClass -> {
+                            if (PolyMorph.class.isAssignableFrom(aClass)) {
+                                return PolyMorph.getPolymorphClass(aClass);
+                            } else {
+                                return Object.class;
+                            }
+                        }))
+                        .entrySet().stream()
+                        .sorted(Comparator.comparing((Map.Entry<Class<? extends Object>, List<Class<? extends ClassCatalog>>> o) ->
+                                o.getKey().getPackage().getName())
+                                .thenComparing(o -> o.getKey().getSimpleName())
+                        )
+                        .map(classListEntry -> MessageFormat.format(
+                                "[{0}]\n{1}",
+                                classListEntry.getKey().getTypeName(),
+                                classListEntry.getValue().stream()
+                                        .filter(aClass -> !aClass.equals(classListEntry.getKey()))
+                                        .sorted(Comparator.comparing((Class<?> o) ->
+                                                o.getPackage().getName())
+                                                .thenComparing(Class::getSimpleName)
+                                        )
+                                        .map(aClass -> " * ".concat(aClass.getTypeName()))
+                                        .collect(Collectors.joining("\n"))
+                        ))
+                        .collect(Collectors.joining("\n"))
+            ));
+        }
+    }
 
     @Inherited
     @Target(ElementType.TYPE)
@@ -24,7 +64,13 @@ public abstract class ClassCatalog extends Entity {
 
     @Target(ElementType.TYPE)
     @Retention(RetentionPolicy.RUNTIME)
-    public @interface Domain {}
+    public @interface Domains {
+        Class<? extends IDomain>[] value();
+    }
+
+    public interface IDomain {
+        /* The inherited classes or interfaces must have localization tag 'title' */
+    }
 
     ClassCatalog(EntityRef owner, ImageIcon icon, String title, String hint) {
         super(owner, icon, title, hint);
@@ -34,15 +80,28 @@ public abstract class ClassCatalog extends Entity {
      * Возвращает список классов, разрешенных для создания и загрузки в данном каталоге.
      * Каждый из этих классов наследуется от одного класса {@link ClassCatalog}.
      */
-    public static List<Class<? extends Entity>> getClassCatalog(Entity entity) {
+    public synchronized static List<Class<? extends Entity>> getClassCatalog(Entity entity) {
         Class<? extends Entity> childClass = entity.getChildClass();
         if (childClass.isAnnotationPresent(ClassCatalog.Definition.class)) {
-            return StreamSupport.stream(ClassIndex.getSubclasses(codex.model.ClassCatalog.class).spliterator(), false)
+            return catalogs.stream()
                     .filter(childClass::isAssignableFrom)
                     .sorted(Comparator.comparing(Class::getTypeName))
                     .collect(Collectors.toList());
         } else {
-            return Collections.nCopies(1, childClass);
+            return new LinkedList<Class<? extends Entity>>() {{
+                add(childClass);
+            }};
         }
+    }
+
+    public synchronized static void registerClassCatalog(Class<? extends ClassCatalog> classCatalog) {
+        catalogs.add(classCatalog);
+        Logger.getLogger().debug("Register class catalog: {0}", classCatalog.getTypeName());
+    }
+
+    static synchronized Class<? extends ClassCatalog> forName(String className) throws ClassNotFoundException {
+        return catalogs.stream()
+                .filter(aClass -> aClass.getTypeName().equals(className))
+                .findFirst().orElseThrow(() -> new ClassNotFoundException(className));
     }
 }
--- core/src/main/java/codex/presentation/ClassSelector.java
+++ core/src/main/java/codex/presentation/ClassSelector.java
@@ -8,20 +8,20 @@ import codex.editor.IEditor;
 import codex.model.ClassCatalog;
 import codex.model.Entity;
 import codex.model.EntityDefinition;
+import codex.type.Iconified;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.TreeSelectionEvent;
-import javax.swing.plaf.basic.BasicTreeUI;
 import javax.swing.tree.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.lang.reflect.Modifier;
 import java.text.MessageFormat;
+import java.util.*;
 import java.util.List;
-import java.util.Stack;
 import java.util.function.Supplier;
 
 class ClassSelector {
@@ -37,8 +37,8 @@ class ClassSelector {
     public static Class<? extends Entity> select(Entity entity) {
         List<Class<? extends Entity>> classCatalog = ClassCatalog.getClassCatalog(entity);
         classCatalog.removeIf(aClass ->
-                Modifier.isAbstract(aClass.getModifiers()) &&
-                !aClass.isAnnotationPresent(ClassCatalog.Domain.class)
+                Modifier.isAbstract(aClass.getModifiers()) //&&
+                //!aClass.isAnnotationPresent(ClassCatalog.Domain.class)
         );
         if (classCatalog.size() == 0) {
             MessageBox.show(MessageType.WARNING, Language.get(ClassSelector.class, "empty"));
@@ -48,13 +48,9 @@ class ClassSelector {
         } else {
             return classCatalog.get(0);
         }
-//        } else if (classCatalog.size() == 1) {
-//            return classCatalog.get(0);
-//        } else {
-//            return new ClassSelector(buildTree(classCatalog)).select();
-//        }
     }
 
+    @SuppressWarnings("unchecked")
     private static DefaultTreeModel buildTree(List<Class<? extends Entity>> classCatalog) {
         classCatalog.sort((class1, class2) -> {
             if (class1.isAssignableFrom(class2)) {
@@ -64,24 +60,27 @@ class ClassSelector {
             } return 0;
         });
 
-        final Stack<DefaultMutableTreeNode> stack = new Stack<>();
-        stack.push(new DefaultMutableTreeNode(Object.class));
-        final DefaultTreeModel treeModel = new DefaultTreeModel(stack.peek());
-
+        final Node root = new DirNode(null);
+        final DefaultTreeModel treeModel = new DefaultTreeModel(root);
         classCatalog.forEach(aClass -> {
-            if (((Class<?>) stack.peek().getUserObject()).isAssignableFrom(aClass)) {
-                DefaultMutableTreeNode parent = stack.peek();
-                stack.push(new DefaultMutableTreeNode(aClass));
-                parent.add(stack.peek());
-            } else {
-                DefaultMutableTreeNode parent;
-                do {
-                    stack.pop();
-                    parent = stack.peek();
-                } while (!((Class<?>) parent.getUserObject()).isAssignableFrom(aClass));
-                stack.push(new DefaultMutableTreeNode(aClass));
-                parent.add(stack.peek());
+            Node currentNode = root;
+            Class<? extends ClassCatalog> ctlClass = (Class<? extends ClassCatalog>) aClass;
+            if (ctlClass.isAnnotationPresent(ClassCatalog.Domains.class)) {
+                final Class<? extends ClassCatalog.IDomain>[] domains = ctlClass.getAnnotation(ClassCatalog.Domains.class).value();
+
+                for (Class<? extends ClassCatalog.IDomain> domain : domains) {
+                    String domainName = Language.get(domain, "title");
+                    Node nextNode = Collections.list((Enumeration<Node>) currentNode.children()).stream()
+                            .filter(childNode -> domainName.equals(childNode.getUserObject()))
+                            .findFirst()
+                            .orElse(new DirNode(domainName));
+                    if (nextNode.getParent() == null) {
+                        currentNode.add(nextNode);
+                    }
+                    currentNode = nextNode;
+                }
             }
+            currentNode.add(new ClassNode(aClass.asSubclass(ClassCatalog.class)));
         });
         return treeModel;
     }
@@ -89,75 +88,51 @@ class ClassSelector {
     private ClassSelector(DefaultTreeModel classTreeModel) {
         JTree classTree = new JTree(classTreeModel);
         classTree.setRowHeight(SIZE+2);
-        classTree.setUI(new BasicTreeUI() {
-            @Override
-            protected void paintVerticalLine(Graphics g, JComponent c, int x, int top, int bottom) {}
-
-            @Override
-            protected void paintHorizontalLine(Graphics g, JComponent c, int y, int left, int right) {}
-        });
         classTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         classTree.setBorder(new EmptyBorder(-15, 5, 5, 5));
 
-        DefaultMutableTreeNode root = (DefaultMutableTreeNode) classTreeModel.getRoot();
-        classTree.expandPath(new TreePath(((DefaultMutableTreeNode) root.getFirstChild()).getPath()));
+        Node root = (Node) classTreeModel.getRoot();
+        classTree.expandPath(new TreePath(((Node) root.getFirstChild()).getPath()));
 
         DialogButton acceptBtn = Dialog.Default.BTN_OK.newInstance();
         acceptBtn.setEnabled(false);
-
-        classTree.addTreeSelectionListener((TreeSelectionEvent event) -> {
-            acceptBtn.setEnabled(isNodeSelectable((DefaultMutableTreeNode) classTree.getLastSelectedPathComponent()));
-        });
-
-        classTree.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent event) {
-                if (event.getClickCount() == 2 && isNodeSelectable((DefaultMutableTreeNode) classTree.getLastSelectedPathComponent())) {
-                    selector.setVisible(false);
-                }
-            }
-        });
 
         classTree.setCellRenderer((tree, value, selected, expanded, leaf, row, hasFocus) -> {
             JLabel label = new JLabel();
             label.setOpaque(true);
             label.setIconTextGap(6);
-            label.setForeground(selected ? Color.WHITE : IEditor.COLOR_NORMAL);
             label.setBorder(new EmptyBorder(15, 2, 15, 7));
             label.setVerticalAlignment(SwingConstants.CENTER);
 
-            DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
-            Class<?> entityClass = (Class<?>) node.getUserObject();
-            EntityDefinition entityDef = entityClass.getAnnotation(EntityDefinition.class);
+            Node node = (Node) value;
+            label.setIcon(node.getIcon());
+            label.setText(node.toString());
 
-            if (value.equals(tree.getModel().getRoot())) {
-                //
-            } else if (entityClass.isAnnotationPresent(ClassCatalog.Domain.class)) {
-                label.setIcon(ICON_FOLDER);
-                label.setText(Language.get(entityClass, "domain@name"));
-            } else {
-                label.setIcon(entityDef != null && !entityDef.icon().isEmpty() ?
-                            ImageUtils.resize(ImageUtils.getByPath(entityDef.icon()), SIZE, SIZE) :
-                            ICON_UNKNOWN
-                );
-                if (entityDef != null && !entityDef.title().isEmpty()) {
-                    label.setText(Language.get(entityClass, entityDef.title()));
-                } else {
-                    label.setText(MessageFormat.format("<{0}>", entityClass.getTypeName()));
-                    label.setForeground(IEditor.COLOR_INVALID);
-                }
-            }
-            label.setBackground(selected ?
+            label.setBackground(selected && isNodeSelectable(node) ?
                     UIManager.getDefaults().getColor("Tree.selectionBackground") :
                     UIManager.getDefaults().getColor("Tree.background")
             );
+            label.setForeground(selected && isNodeSelectable(node) ? Color.WHITE : IEditor.COLOR_NORMAL);
             return label;
         });
 
+        classTree.addTreeSelectionListener((TreeSelectionEvent event) -> {
+            boolean selectable = isNodeSelectable((Node) classTree.getLastSelectedPathComponent());
+            acceptBtn.setEnabled(selectable);
+        });
+
+        classTree.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                if (event.getClickCount() == 2 && isNodeSelectable((Node) classTree.getLastSelectedPathComponent())) {
+                    selector.setVisible(false);
+                }
+            }
+        });
+
         classSupplier = () -> {
-            DefaultMutableTreeNode node = (DefaultMutableTreeNode) classTree.getLastSelectedPathComponent();
+            ClassNode node = (ClassNode) classTree.getLastSelectedPathComponent();
             if (node != null) {
-                Class<?> entityClass = (Class<?>) node.getUserObject();
-                return entityClass.asSubclass(Entity.class);
+                return node.catalogClass.asSubclass(Entity.class);
             }
             return null;
         };
@@ -190,12 +165,68 @@ class ClassSelector {
         };
     }
 
-    private boolean isNodeSelectable(DefaultMutableTreeNode node) {
-        return node != null && !((Class<?>) node.getUserObject()).isAnnotationPresent(ClassCatalog.Domain.class);
+    private boolean isNodeSelectable(Node node) {
+        return node instanceof ClassNode;
     }
 
     private Class<? extends Entity> select() {
         selector.setVisible(true);
         return classSupplier.get();
+    }
+
+    private static abstract class Node extends DefaultMutableTreeNode implements Iconified {}
+
+    private static class DirNode extends Node {
+
+        private final String title;
+
+        DirNode(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public String toString() {
+            return title;
+        }
+
+        @Override
+        public ImageIcon getIcon() {
+            return title == null ? new ImageIcon() : ICON_FOLDER;
+        }
+    }
+
+    private static class ClassNode extends Node {
+
+        private final Class<? extends ClassCatalog> catalogClass;
+        private final ImageIcon icon;
+        private final String    title;
+
+        ClassNode(Class<? extends ClassCatalog> catalogClass) {
+            this.catalogClass = catalogClass;
+
+            final EntityDefinition entityDef = catalogClass.getAnnotation(EntityDefinition.class);
+            icon = entityDef != null && !entityDef.icon().isEmpty() ?
+                    ImageUtils.resize(ImageUtils.getByPath(
+                            catalogClass,
+                            entityDef.icon()
+                    ), SIZE, SIZE) :
+                    ICON_UNKNOWN;
+
+            if (entityDef != null && !entityDef.title().isEmpty()) {
+                title = Language.get(catalogClass, entityDef.title());
+            } else {
+                title = MessageFormat.format("<{0}>", catalogClass.getTypeName());
+            }
+        }
+
+        @Override
+        public ImageIcon getIcon() {
+            return icon;
+        }
+
+        @Override
+        public String toString() {
+            return title;
+        }
     }
 }
