a02ea8ad142cda5d9560a962ef79aeea60616fb8
igredyaev <i.gredyaev@compassplus.com>
2020-05-18 03:24:08 +0500

    Добавлен метод форматирования с заданной размерностью.

--- core/src/main/java/codex/utils/FileUtils.java
+++ core/src/main/java/codex/utils/FileUtils.java
@@ -4,7 +4,15 @@ import java.text.DecimalFormat;
 
 public class FileUtils {
 
+    public enum Dimension {
+        KB, MB, GB, TB, AUTO
+    }
+
     public static String formatFileSize(long size) {
+        return formatFileSize(size, Dimension.AUTO);
+    }
+
+    public static String formatFileSize(long size, Dimension dim) {
         String hrSize;
 
         double bytes     = size;
@@ -15,13 +23,13 @@ public class FileUtils {
 
         DecimalFormat dec = new DecimalFormat("0.00");
 
-        if (terabytes > 1) {
+        if ((terabytes > 1 && dim == Dimension.AUTO) || dim == Dimension.TB) {
             hrSize = dec.format(terabytes).concat(" TB");
-        } else if (gigabytes > 1) {
+        } else if ((gigabytes > 1 && dim == Dimension.AUTO) || dim == Dimension.GB) {
             hrSize = dec.format(gigabytes).concat(" GB");
-        } else if (megabytes > 1) {
+        } else if ((megabytes > 1 && dim == Dimension.AUTO) || dim == Dimension.MB) {
             hrSize = dec.format(megabytes).concat(" MB");
-        } else if (kilobytes > 1) {
+        } else if ((kilobytes > 1 && dim == Dimension.AUTO) || dim == Dimension.KB) {
             hrSize = dec.format(kilobytes).concat(" KB");
         } else {
             hrSize = dec.format(bytes).concat(" B");
