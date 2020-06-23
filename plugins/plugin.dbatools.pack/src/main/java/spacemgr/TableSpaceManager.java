60498e059f4cb6beb6aca4e213110016aa5bc415
igredyaev <i.gredyaev@compassplus.com>
2020-06-24 00:42:21 +0500

    Перенос параметров команды в настройки плагина.

--- plugins/plugin.dbatools.pack/src/main/java/spacemgr/TableSpaceManager.java
+++ plugins/plugin.dbatools.pack/src/main/java/spacemgr/TableSpaceManager.java
@@ -1,6 +1,10 @@
 package spacemgr;
 
+import codex.command.EditorCommand;
+import codex.component.messagebox.MessageBox;
+import codex.component.messagebox.MessageType;
 import codex.database.IDatabaseAccessService;
+import codex.editor.AbstractEditor;
 import codex.log.Logger;
 import codex.model.Entity;
 import codex.model.ParamModel;
@@ -11,44 +15,37 @@ import codex.task.ITaskExecutorService;
 import codex.type.EntityRef;
 import codex.type.IComplexType;
 import codex.type.Str;
+import codex.utils.ImageUtils;
 import codex.utils.Language;
 import manager.nodes.Common;
 import manager.nodes.Database;
 import plugin.Pluggable;
 import plugin.command.CommandPlugin;
 import spacemgr.command.objects.TableSpace;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.*;
 import java.util.List;
 
 @Pluggable.PluginOptions(provider = TableSpaceManager.Options.class)
 public class TableSpaceManager extends CommandPlugin<Common> {
 
-    private final static String PARAM_DB_URL  = "url";
-    private final static String PARAM_DB_USER = "user";
-    private final static String PARAM_DB_PASS = "password";
-
     private final static String FILTER = "^(RBS|RADIX).*$";
 
     public TableSpaceManager() {
         super(common -> true);
-        setParameters(
-                new PropertyHolder<>(PARAM_DB_URL, new EntityRef<>(Database.class), true),
-                new PropertyHolder<>(PARAM_DB_USER, new Str("SYS AS SYSDBA"), true),
-                new PropertyHolder<>(PARAM_DB_PASS, new Str("SYS"), true)
-        );
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void execute(Common context, Map<String, IComplexType> params) {
-        Database srcDb = ((EntityRef<Database>) params.get(PARAM_DB_URL)).getValue();
-        Database tmpDb = createDBConnection(
-                srcDb.getDatabaseUrl(false),
-                ((Str) params.get(PARAM_DB_USER)).getValue(),
-                ((Str) params.get(PARAM_DB_PASS)).getValue()
-        );
+        Database database = (Database) getOption(Options.OPT_DATABASE);
+        String baseUri  = database.getDatabaseUrl(false);
+        String userName = database.getDatabaseUser(false);
+        String userPass = database.getDatabasePassword(false);
+
+        Database tmpDb = createDBConnection(baseUri, userName, userPass);
         if (tmpDb.getConnectionID(true) != null) {
             ServiceRegistry.getInstance()
                     .lookupService(ITaskExecutorService.class)
@@ -121,16 +118,91 @@ public class TableSpaceManager extends CommandPlugin<Common> {
     }
 
 
-    class Options extends Pluggable.OptionsProvider {
+    public class Options extends Pluggable.OptionsProvider {
+
+        static final String OPT_DATABASE = "database";
+        static final String OPT_USERNAME = "user";
+        static final String OPT_USERPASS = "password";
 
         private final ParamModel model = new ParamModel();
         {
-            model.addProperty(new PropertyHolder<>("test", new Str("TEST"), true));
+            model.addProperty(new PropertyHolder<>(OPT_DATABASE, getOptTitle(OPT_DATABASE), null, new EntityRef<>(Database.class), true));
+            model.addProperty(new PropertyHolder<>(OPT_USERNAME, getOptTitle(OPT_USERNAME), null, new Str(null) {
+                @Override
+                public String getValue() {
+                    Database db = (Database) model.getValue(OPT_DATABASE);
+                    return db == null ? null : db.getDatabaseUser(false);
+                }
+            }, true));
+            model.addProperty(new PropertyHolder<>(OPT_USERPASS, getOptTitle(OPT_USERPASS), null, new Str(null) {
+                @Override
+                public String getValue() {
+                    Database db = (Database) model.getValue(OPT_DATABASE);
+                    return db == null ? null : db.getDatabasePassword(false);
+                }
+            }, true));
+
+            model.addChangeListener((name, oldValue, newValue) -> {
+                if (name.equals(OPT_DATABASE)) {
+                    ((AbstractEditor) model.getEditor(OPT_USERNAME)).updateUI();
+                    ((AbstractEditor) model.getEditor(OPT_USERPASS)).updateUI();
+                }
+            });
+            //noinspection unchecked
+            model.getEditor(OPT_DATABASE).addCommand(new CheckDBA());
+            model.getEditor(OPT_USERNAME).setEditable(false);
+            model.getEditor(OPT_USERPASS).setEditable(false);
         }
 
         @Override
         public ParamModel getOptions() {
             return model;
+        }
+
+        private String getOptTitle(String optName) {
+            return Language.get(TableSpaceManager.class, optName.concat(PropertyHolder.PROP_NAME_SUFFIX));
+        }
+
+
+        private class CheckDBA extends EditorCommand<EntityRef<Database>, Database> {
+
+            private CheckDBA() {
+                super(
+                        ImageUtils.resize(ImageUtils.getByPath("/images/question.png"), 19, 19),
+                        Language.get(TableSpaceManager.class, "check.dba@title"),
+                        PropertyHolder::isValid
+                );
+            }
+
+            @Override
+            public void execute(PropertyHolder<EntityRef<Database>, Database> context) {
+                final String query = Language.get(TableSpaceManager.class, "check.dba@query", Locale.US);
+                try (final ResultSet resultSet = ServiceRegistry.getInstance()
+                            .lookupService(IDatabaseAccessService.class).select(
+                                    context.getPropValue().getValue().getConnectionID(true),
+                                    query
+                            )
+                ) {
+                    if (resultSet.next()) {
+                        if (resultSet.getInt(1) == 1) {
+                            MessageBox.show(
+                                    MessageType.INFORMATION,
+                                    Language.get(TableSpaceManager.class, "check.dba@success")
+                            );
+                        } else {
+                            MessageBox.show(
+                                    MessageType.ERROR,
+                                    Language.get(TableSpaceManager.class, "check.dba@fail")
+                            );
+                        }
+                    }
+                } catch (SQLException e) {
+                    MessageBox.show(MessageType.ERROR, MessageFormat.format(
+                            Language.get(TableSpaceManager.class, "check.dba@error"),
+                            e.getLocalizedMessage()
+                    ));
+                }
+            }
         }
     }
 }
