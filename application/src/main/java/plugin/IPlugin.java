f8ef33dbecb5ef81846a8bc7686c8fb8d5fe055f
igredyaev <i.gredyaev@compassplus.com>
2020-06-26 02:07:18 +0500

    Добавлены аннотации для установки параметров плагина.

--- application/src/main/java/plugin/IPlugin.java
+++ application/src/main/java/plugin/IPlugin.java
@@ -1,5 +1,17 @@
 package plugin;
 
+import codex.context.IContext;
+import codex.log.Logger;
 
-@Pluggable(pluginHandlerClass = PluginHandler.class)
-public interface IPlugin {}
+@Pluggable()
+public interface IPlugin extends IContext {
+
+    default Object getOption(String optName) {
+        try {
+            return PluginManager.getOption(getClass(), optName);
+        } catch (ClassNotFoundException e) {
+            Logger.getContextLogger(getClass()).error("Option not found", e);
+        }
+        return null;
+    }
+}
--- application/src/main/java/plugin/Pluggable.java
+++ application/src/main/java/plugin/Pluggable.java
@@ -1,5 +1,6 @@
 package plugin;
 
+import codex.model.ParamModel;
 import org.atteo.classindex.IndexAnnotated;
 import java.lang.annotation.*;
 
@@ -8,5 +9,15 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Pluggable {
-    Class<? extends PluginHandler> pluginHandlerClass();
+    Class<? extends PluginHandler> pluginHandlerClass() default PluginHandler.class;
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface PluginOptions {
+        Class<? extends OptionsProvider> provider();
+    }
+
+    abstract class OptionsProvider {
+        public abstract ParamModel getOptions();
+    }
 }
