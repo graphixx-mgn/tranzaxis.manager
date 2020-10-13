92c5adc6949f168399afaf9fe168219d7093b28b
igredyaev <i.gredyaev@compassplus.com>
2020-10-13 20:45:00 +0500

    Исправлен NPE при получении адреса сетевого интерфейса.
    Логика получения адреса допускала возможность возвращения NULL-значения. Не очень понятно почему, но в редких случаях это было возмозможно.

--- plugins/plugin.port.config/src/main/java/type/NetInterface.java
+++ plugins/plugin.port.config/src/main/java/type/NetInterface.java
@@ -7,8 +7,7 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class NetInterface implements Iconified {
@@ -18,30 +17,35 @@ public class NetInterface implements Iconified {
     private final static ImageIcon ICON_VIRTUAL = ImageUtils.getByPath("/images/iface_virtual.png");
 
     public static List<NetInterface> getInterfaces() {
+        Map<NetworkInterface, InetAddress> ifaceAddrs = new LinkedHashMap<>();
         try {
-            return Collections.list(NetworkInterface.getNetworkInterfaces()).stream()
-                    .filter(iface -> {
-                        try {
-                            return iface.isUp();
-                        } catch (SocketException e) {
-                            return false;
-                        }
-                    }).map(NetInterface::new)
-                    .collect(Collectors.toList());
+            final Enumeration<NetworkInterface> netifs = NetworkInterface.getNetworkInterfaces();
+            while (netifs.hasMoreElements()) {
+                NetworkInterface iface = netifs.nextElement();
+                if (!iface.isUp()) continue;
+
+                Enumeration<InetAddress> inAddrs = iface.getInetAddresses();
+                while (inAddrs.hasMoreElements()) {
+                    InetAddress inAddr = inAddrs.nextElement();
+                    if (inAddr instanceof Inet4Address) {
+                        ifaceAddrs.put(iface, inAddr);
+                    }
+                }
+            }
         } catch (SocketException e) {
-            return Collections.emptyList();
+            //
         }
+        return ifaceAddrs.entrySet().stream()
+                .map(entry -> new NetInterface(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
     }
 
-    private NetworkInterface iface;
-    private NetInterface (NetworkInterface iface) {
-        this.iface = iface;
-    }
+    private final NetworkInterface iface;
+    private final InetAddress address;
 
-    private InetAddress getAddress() {
-        return Collections.list(iface.getInetAddresses()).stream()
-                .filter(inetAddress -> inetAddress instanceof Inet4Address)
-                .findFirst().orElse(null);
+    private NetInterface (NetworkInterface iface, InetAddress address) {
+        this.iface   = iface;
+        this.address = address;
     }
 
     @Override
@@ -56,6 +60,6 @@ public class NetInterface implements Iconified {
 
     @Override
     public String toString() {
-        return getAddress().getHostAddress().replaceAll("127.0.0.1", "localhost");
+        return address.getHostAddress().replaceAll("127.0.0.1", "localhost");
     }
 }
