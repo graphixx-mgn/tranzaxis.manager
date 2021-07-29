881a1fc522bbafd7f51ffe6ca6a8af8aa69ca82f
Ivan S. Gredyaev <i.gredyaev@compassplus.com>
2021-07-29 09:59:35 +0500

    Добавлен класс Wizard для отображения прогресса исполнения пошаговых задач.

--- core/src/main/java/codex/task/TaskOutput.java
+++ core/src/main/java/codex/task/TaskOutput.java
@@ -1,10 +1,16 @@
 package codex.task;
 
+import codex.component.button.DialogButton;
+import codex.component.dialog.Dialog;
 import codex.component.panel.HTMLView;
+import codex.editor.IEditor;
 import codex.log.Level;
 import codex.utils.ImageUtils;
+import codex.utils.Language;
 import org.bridj.util.Pair;
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.text.*;
 import javax.swing.text.html.HTMLDocument;
@@ -12,11 +18,13 @@ import javax.swing.text.html.HTMLEditorKit;
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Semaphore;
 import java.util.function.Function;
 
 public class TaskOutput extends JPanel {
@@ -97,12 +105,7 @@ public class TaskOutput extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 Toolkit.getDefaultToolkit()
                         .getSystemClipboard()
-                        .setContents(
-                                new StringSelection(
-                                        pane.getSelectedText().replaceAll("\\u00a0"," ")
-                                        ),
-                                null
-                        );
+                        .setContents(new StringSelection(pane.getSelectedText().replaceAll("\\u00a0"," ")), null);
             }
         });
     }};
@@ -117,6 +120,11 @@ public class TaskOutput extends JPanel {
         scrollPane.getViewport().add(pane);
         scrollPane.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
         add(scrollPane, BorderLayout.CENTER);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(560, 400);
     }
 
     abstract public static class ExecPhase<R> {
@@ -144,29 +152,137 @@ public class TaskOutput extends JPanel {
         private static String RC_ERROR   = "<font color='red'>&#x26D4;</font>";
         private static Function<String, String> FMT_SUCCESS  = input -> String.join("", Collections.nCopies(68-input.length(), ".")).concat(RC_SUCCESS);
         private static Function<String, String>  FMT_ERROR   = input -> String.join("", Collections.nCopies(68-input.length(), ".")).concat(RC_ERROR);
-        private static Function<String, Integer> HTML_LENGTH = input -> input.replaceAll("\\<[^>]*>","").length();
+        private static Function<String, Integer> HTML_LENGTH = input -> input.replaceAll("<[^>]*>","").length();
 
         private static String fillStepResult(String step, String result, Throwable error) {
             return new StringBuilder(" &bull; ")
                     .append(step)
                     .append(error  == null ? (
-                                    result == null ?
-                                            FMT_SUCCESS.apply(step) :
-                                            String
-                                                    .join("", Collections.nCopies(69-step.length()-HTML_LENGTH.apply(result), "."))
-                                                    .concat(result)
-                            ) : (
-                                    error.getMessage() == null ?
-                                            FMT_ERROR.apply(step) :
-                                            String
-                                                    .join("", Collections.nCopies(68-step.length(), "."))
-                                                    .concat(MessageFormat.format(
-                                                            "<font color='red'>&#x26D4;</font><br/>   <font color='maroon'>{0}: {1}</font>",
-                                                            error.getClass().getCanonicalName(),
-                                                            error.getMessage()
-                                                    ))
-                            )
+                            result == null ?
+                            FMT_SUCCESS.apply(step) :
+                            String.join("", Collections.nCopies(69-step.length()-HTML_LENGTH.apply(result), ".")).concat(result)
+                        ) : (
+                            error.getMessage() == null ?
+                            FMT_ERROR.apply(step) :
+                            String.join("", Collections.nCopies(68-step.length(), ".")).concat(MessageFormat.format(
+                                    "<font color='red'>&#x26D4;</font><br/>   <font color='maroon'>{0}: {1}</font>",
+                                    error.getClass().getCanonicalName(),
+                                    error.getMessage()
+                            ))
+                        )
                     ).toString();
+        }
+    }
+
+
+    public abstract static class Wizard<R> extends AbstractTask<R> {
+
+        private final ImageIcon dialogIcon;
+        public Wizard(String title, ImageIcon dialogIcon) {
+            super(title);
+            this.dialogIcon = dialogIcon;
+        }
+
+        protected abstract R process() throws Exception;
+
+        @Override
+        public final R execute() throws Exception {
+            final DialogButton dialogButton = Dialog.Default.BTN_CLOSE.newInstance();
+
+            dialogButton.setEnabled(false);
+            Dialog dialog = new Dialog(
+                    Dialog.findNearestWindow(),
+                    dialogIcon,
+                    Language.get(TaskMonitor.class, "dialog.title"),
+                    new JPanel(new BorderLayout(0, 5)) {{
+                        setBorder(new EmptyBorder(5, 5, 5, 5));
+                        AbstractTaskView taskView = createView(null);
+                        taskView.setBorder(new CompoundBorder(
+                                new LineBorder(Color.LIGHT_GRAY, 1),
+                                new EmptyBorder(5, 5, 5, 5)
+                        ));
+                        add(taskView, BorderLayout.NORTH);
+                        add(TaskOutput.createOutput(Wizard.this), BorderLayout.CENTER);
+                    }},
+                    event -> {},
+                    dialogButton
+            ) {{
+                setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+                setResizable(false);
+
+                final Function<DialogButton, ActionListener> defHandler = handler;
+                handler = (button) -> (event) -> {
+                    if (getDefaultCloseOperation() == WindowConstants.DO_NOTHING_ON_CLOSE) {
+                        if (allowTermination()) {
+                            // Interrupt task
+                            cancel(true);
+                            // Allow to close
+                            setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                            defHandler.apply(button).actionPerformed(event);
+                        }
+                    } else {
+                        defHandler.apply(button).actionPerformed(event);
+                    }
+                };
+            }};
+            SwingUtilities.invokeLater(() -> dialog.setVisible(true));
+
+            final R result = process();
+
+            dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            dialogButton.setEnabled(true);
+            return result;
+        }
+
+        protected boolean allowTermination() {
+            final Semaphore lock = new Semaphore(1);
+            final DialogButton submitBtn = Dialog.Default.BTN_CANCEL.newInstance(Language.get(TaskMonitor.class, "dialog@warn.cancel"));
+            final JPanel warnMessage = new JPanel(new BorderLayout()) {{
+                JLabel label = new JLabel(
+                        Language.get(TaskMonitor.class, "dialog@warn.mess"),
+                        ImageUtils.getByPath("/images/warn.png"),
+                        SwingConstants.LEFT
+                );
+                label.setOpaque(true);
+                label.setIconTextGap(5);
+                label.setBackground(new Color(0x33DE5347, true));
+                label.setForeground(IEditor.COLOR_INVALID);
+                label.setBorder(new CompoundBorder(
+                        new LineBorder(Color.decode("#DE5347"), 1),
+                        new EmptyBorder(5, 5, 5, 5)
+                ));
+                setBorder(new EmptyBorder(0, 0, 5, 0));
+                add(label, BorderLayout.CENTER);
+            }};
+
+            final Dialog dialog = new Dialog(
+                    Dialog.findNearestWindow(),
+                    dialogIcon,
+                    Language.get(TaskMonitor.class, "dialog@warn.title"),
+                    new JPanel(new BorderLayout()) {{
+                        setBorder(new EmptyBorder(5, 10, 5, 10));
+                        add(warnMessage, BorderLayout.NORTH);
+                    }},
+                    e -> lock.release(),
+                    submitBtn
+            ) {
+                {
+                    setResizable(false);
+                }
+
+                @Override
+                public Dimension getPreferredSize() {
+                    return new Dimension(500, super.getPreferredSize().height);
+                }
+            };
+            try {
+                lock.acquire();
+                dialog.setVisible(true);
+                lock.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return dialog.getExitCode() == Dialog.CANCEL;
         }
     }
 }
