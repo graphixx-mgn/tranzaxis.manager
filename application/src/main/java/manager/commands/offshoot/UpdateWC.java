1c6f7e32eb4709ee88b7895c328f30795ba59b59
Ivan S. Gredyaev <i.gredyaev@compassplus.com>
2022-11-21 14:38:02 +0500

    Рефакторинг.

--- application/src/main/java/manager/commands/offshoot/UpdateWC.java
+++ application/src/main/java/manager/commands/offshoot/UpdateWC.java
@@ -6,21 +6,20 @@ import codex.task.*;
 import codex.type.IComplexType;
 import codex.utils.ImageUtils;
 import codex.utils.Language;
-import java.io.File;
 import java.nio.channels.ClosedChannelException;
 import java.text.MessageFormat;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import manager.nodes.BinarySource;
 import manager.nodes.Offshoot;
-import manager.svn.SVN;
 import manager.type.WCStatus;
+import org.codehaus.plexus.util.StringUtils;
 import org.tmatesoft.svn.core.*;
-import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
 import org.tmatesoft.svn.core.wc.*;
 import javax.swing.*;
+
 
 @EntityCommand.Definition(parentCommand = RefreshWC.class)
 public class UpdateWC extends EntityCommand<Offshoot> {
@@ -88,96 +87,39 @@ public class UpdateWC extends EntityCommand<Offshoot> {
                     );
                 }
             }
-
-            String wcPath  = offshoot.getLocalPath();
-            String repoUrl = offshoot.getRemotePath();
-            SVNRevision R1 = offshoot.getWorkingCopyRevision(false);
-            String strR1 = SVNRevision.UNDEFINED.equals(R1) ? "<none>" : MessageFormat.format(
-                    "{0} / {1}",
-                    R1, Offshoot.DATE_FORMAT.format(offshoot.getWorkingCopyRevisionDate(false))
-            );
-            ISVNAuthenticationManager authMgr = offshoot.getRepository().getAuthManager();
-
             setProgress(0, Language.get(UpdateWC.class, "command@calc"));
-
             try {
-                List<SVNURL> changes = SVN.changes(wcPath, repoUrl, revision, authMgr, new ISVNEventHandler() {
-                    @Override
-                    public void handleEvent(SVNEvent event, double d) {
-                        if (event.getErrorMessage() != null && event.getErrorMessage().getErrorCode() == SVNErrorCode.WC_CLEANUP_REQUIRED) {
-                            if (event.getExpectedAction() == SVNEventAction.RESOLVER_STARTING) {
-                                setProgress(0, Language.get(UpdateWC.class, "command@cleanup"));
-                                Logger.getLogger().info(
-                                        "Perform automatic cleanup repository [{0}/{1}]",
-                                        offshoot.getRepository().getPID(),
-                                        offshoot.getPID()
-                                );
-                            } else {
-                                Logger.getLogger().info(
-                                        "Continue update [{0}/{1}] after cleanup",
-                                        offshoot.getRepository().getPID(),
-                                        offshoot.getPID()
-                                );
-                                setProgress(0, Language.get(UpdateWC.class, "command@calc"));
+                List<SVNDirEntry> changes = !SVNWCUtil.isVersionedDirectory(offshoot.getLocalPath().toFile()) ?
+                        offshoot.getRepository().listEntries(offshoot.getRemotePath(), true, new ISVNEventHandler() {
+                            @Override
+                            public void handleEvent(SVNEvent svnEvent, double v) throws SVNException {}
+                            @Override
+                            public void checkCancelled() throws SVNCancelException {
+                                checkPaused();
+                                if (UpdateTask.this.isCancelled()) {
+                                    throw new SVNCancelException();
+                                }
                             }
-                        }
-                    }
-
-                    @Override
-                    public void checkCancelled() throws SVNCancelException {
-                        checkPaused();
-                        if (UpdateTask.this.isCancelled()) {
-                            throw new SVNCancelException();
-                        }
-                    }
-                });
-
-                if (changes.size() > 0) {
-                    Logger.getLogger().debug("Found changes of branch ''{0}'': {1}", wcPath, changes.size());
-
-                    offshoot.setWCLoaded(false);
-                    offshoot.model.commit(false);
-
-                    Logger.getLogger().info(
-                            "Update [{0}/{1}] started",
-                            offshoot.getRepository().getPID(),
-                            offshoot.getPID()
-                    );
-                    AtomicInteger loaded   = new AtomicInteger(0);
-                    AtomicInteger added    = new AtomicInteger(0);
-                    AtomicInteger deleted  = new AtomicInteger(0);
-                    AtomicInteger restored = new AtomicInteger(0);
-                    AtomicInteger changed  = new AtomicInteger(0);
-                    AtomicInteger skipped  = new AtomicInteger(0);
-
-                    long total = changes.size();
-                    SVN.update(repoUrl, wcPath, revision, authMgr, new ISVNEventHandler() {
+                        }).parallelStream().filter(svnDirEntry -> svnDirEntry.getKind() == SVNNodeKind.FILE).collect(Collectors.toList())
+                        :
+                        offshoot.getRepository().getChanges(offshoot.getLocalPath(), revision, new ISVNEventHandler() {
                             @Override
                             public void handleEvent(SVNEvent event, double d) {
-                                if (event.getAction() != SVNEventAction.UPDATE_STARTED && event.getAction() != SVNEventAction.UPDATE_COMPLETED) {
-                                    if (event.getNodeKind() == SVNNodeKind.FILE) {
-                                        loaded.addAndGet(1);
-                                        int percent = (int) (loaded.get() * 100 / total);
-                                        setProgress(
-                                                Math.min(percent, 100),
-                                                MessageFormat.format(
-                                                        Language.get(UpdateWC.class, "command@progress"),
-                                                        event.getFile().getPath().replace(wcPath+File.separator, "")
-                                                )
+                                if (event.getErrorMessage() != null && event.getErrorMessage().getErrorCode() == SVNErrorCode.WC_CLEANUP_REQUIRED) {
+                                    if (event.getExpectedAction() == SVNEventAction.RESOLVER_STARTING) {
+                                        setProgress(0, Language.get(UpdateWC.class, "command@cleanup"));
+                                        Logger.getLogger().info(
+                                                "Perform automatic cleanup repository [{0}/{1}]",
+                                                offshoot.getRepository().getPID(),
+                                                offshoot.getPID()
                                         );
-                                        SVNEventAction action = event.getAction();
-
-                                        if (action == SVNEventAction.UPDATE_ADD || action == SVNEventAction.UPDATE_SHADOWED_ADD) {
-                                            added.addAndGet(1);
-                                        } else if (action == SVNEventAction.UPDATE_DELETE || action == SVNEventAction.UPDATE_SHADOWED_DELETE) {
-                                            deleted.addAndGet(1);
-                                        } else if (action == SVNEventAction.UPDATE_UPDATE || action == SVNEventAction.UPDATE_SHADOWED_UPDATE) {
-                                            changed.addAndGet(1);
-                                        } else if (action == SVNEventAction.RESTORE) {
-                                            restored.addAndGet(1);
-                                        } else if (action == SVNEventAction.SKIP_CONFLICTED) {
-                                            skipped.addAndGet(1);
-                                        }
+                                    } else {
+                                        Logger.getLogger().info(
+                                                "Continue update [{0}/{1}] after cleanup",
+                                                offshoot.getRepository().getPID(),
+                                                offshoot.getPID()
+                                        );
+                                        setProgress(0, Language.get(UpdateWC.class, "command@calc"));
                                     }
                                 }
                             }
@@ -189,34 +131,100 @@ public class UpdateWC extends EntityCommand<Offshoot> {
                                     throw new SVNCancelException();
                                 }
                             }
-                        }
-                    );
-                    String strR2 = MessageFormat.format(
-                            "{0} / {1}",
-                            offshoot.getWorkingCopyRevision(false),
-                            Offshoot.DATE_FORMAT.format(offshoot.getWorkingCopyRevisionDate(false))
-                    );
-                    Logger.getLogger().info(
-                            "Update [{0}/{1}] finished\nRevision: {2} -> {3}\n"+
-                            (added.get()    == 0 ? "" : " * Added:    {4}\n")+
-                            (deleted.get()  == 0 ? "" : " * Deleted:  {5}\n")+
-                            (restored.get() == 0 ? "" : " * Restored: {6}\n")+
-                            (changed.get()  == 0 ? "" : " * Changed:  {7}\n")+
-                            (skipped.get()  == 0 ? "" : " * Skipped:  {8}\n")+
-                                                        " * Total:    {9}",
-                            offshoot.getRepository().getPID(),
-                            offshoot.getPID(),
-                            strR1, strR2,
-                            added.get(),
-                            deleted.get(),
-                            restored.get(),
-                            changed.get(),
-                            skipped.get(),
-                            loaded.get()
+                        });
+                if (changes.size() > 0) {
+                    Logger.getLogger().debug("Found changes of branch ''{0}'': {1}", offshoot.getRemotePath(), changes.size());
+
+                    offshoot.setWCLoaded(false);
+                    offshoot.model.commit(false);
+
+                    final List<SVNEvent> events = new LinkedList<>();
+                    offshoot.getRepository().update(
+                            offshoot.getRemotePath(),
+                            offshoot.getLocalPath(),
+                            revision,
+                            new ISVNEventHandler() {
+                                private final Function<BinarySource, SVNInfo> getInfo = source -> {
+                                    try {
+                                        return source.getRepository().getInfo(source.getLocalPath().toFile());
+                                    } catch (SVNException e) {
+                                        return null;
+                                    }
+                                };
+                                private final Function<SVNInfo, String> fmtInfo = svnInfo ->
+                                        svnInfo == null || SVNRevision.UNDEFINED.equals(svnInfo.getRevision()) ?
+                                                "<none>" :
+                                                MessageFormat.format(
+                                                    "{0} / {1}",
+                                                    svnInfo.getRevision().getNumber(),
+                                                    Offshoot.DATE_FORMAT.format(svnInfo.getCommittedDate())
+                                                );
+                                private SVNInfo r1, r2;
+
+                                @Override
+                                public void handleEvent(SVNEvent svnEvent, double v) throws SVNException {
+                                    if (svnEvent.getAction() == SVNEventAction.UPDATE_STARTED) {
+                                        r1 = getInfo.apply(offshoot);
+
+                                        Logger.getLogger().info(
+                                                "Update [{0}/{1}] started ({2} files to be updated)",
+                                                offshoot.getRepository().getPID(),
+                                                offshoot.getPID(),
+                                                changes.size()
+                                        );
+                                    } else if (svnEvent.getAction() == SVNEventAction.UPDATE_COMPLETED) {
+                                        r2 = getInfo.apply(offshoot);
+
+                                        List<String> details = events.stream().collect(Collectors.groupingBy(event -> {
+                                            if (event.getAction() == SVNEventAction.UPDATE_ADD || event.getAction() == SVNEventAction.UPDATE_SHADOWED_ADD)
+                                                return "Added";
+                                            else if (event.getAction() == SVNEventAction.UPDATE_DELETE || event.getAction() == SVNEventAction.UPDATE_SHADOWED_DELETE)
+                                                return "Deleted";
+                                            else if (event.getAction() == SVNEventAction.UPDATE_UPDATE || event.getAction() == SVNEventAction.UPDATE_SHADOWED_UPDATE)
+                                                return "Changed";
+                                            else if (event.getAction() == SVNEventAction.RESTORE)
+                                                return "Restored";
+                                            else
+                                                return "Skipped";
+                                        })).entrySet().stream()
+                                                .sorted(Comparator.comparingInt(o -> o.getKey().charAt(0)))
+                                                .filter(entry -> !entry.getValue().isEmpty())
+                                                .map(entry -> String.format(" * %s: %d", StringUtils.rightPad(entry.getKey(), 10), entry.getValue().size()))
+                                                .collect(Collectors.toList());
+                                        Logger.getLogger().info(
+                                                "Update [{0}/{1}] finished (revision: {2} -> {3})\n{4}",
+                                                offshoot.getRepository().getPID(),
+                                                offshoot.getPID(),
+                                                fmtInfo.apply(r1),
+                                                fmtInfo.apply(r2),
+                                                String.join("\n", details)
+                                        );
+                                    } else {
+                                        if (svnEvent.getNodeKind() == SVNNodeKind.FILE) {
+                                            events.add(svnEvent);
+                                            setProgress(
+                                                    Math.min(events.size() * 100 / changes.size(), 100),
+                                                    MessageFormat.format(
+                                                            Language.get(UpdateWC.class, "command@progress"),
+                                                            offshoot.getRelativePath(svnEvent.getFile().getAbsoluteFile().toPath())
+                                                    )
+                                            );
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void checkCancelled() throws SVNCancelException {
+                                    checkPaused();
+                                    if (UpdateTask.this.isCancelled()) {
+                                        throw new SVNCancelException();
+                                    }
+                                }
+                            }
                     );
                 } else {
                     Logger.getLogger().info(
-                            "Update [{0}/{1}] finished. working copy already actual",
+                            "Update [{0}/{1}] finished. no need to update",
                             offshoot.getRepository().getPID(),
                             offshoot.getPID()
                     );
@@ -229,7 +237,8 @@ public class UpdateWC extends EntityCommand<Offshoot> {
                 if (rootCause.isPresent()) {
                     if (rootCause.get() instanceof SVNCancelException || rootCause.get() instanceof ClosedChannelException) {
                         Logger.getLogger().info(
-                                "Update [{0}/{1}] canceled", offshoot.getRepository().getPID(),
+                                "Update [{0}/{1}] canceled",
+                                offshoot.getRepository().getPID(),
                                 offshoot.getPID()
                         );
                         throw new CancelException();
